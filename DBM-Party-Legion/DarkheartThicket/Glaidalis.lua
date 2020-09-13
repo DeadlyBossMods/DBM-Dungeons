4784a394960596e5ee72826b2c448fbd84135c60
MysticalOS <mysticalosx@gmail.com>
2020-09-13 10:23:22 -0400

    Change this to a no target filter warning

--- DBM-Party-Legion/DarkheartThicket/Glaidalis.lua
+++ DBM-Party-Legion/DarkheartThicket/Glaidalis.lua
@@ -15,7 +15,7 @@ mod:RegisterEventsInCombat(
 	"UNIT_SPELLCAST_SUCCEEDED boss1"
 )
 
-local warnLeap					= mod:NewTargetAnnounce(196346, 2)--0.5 seconds may still be too hard to dodge even if target scanning works.
+local warnLeap					= mod:NewTargetNoFilterAnnounce(196346, 2)--0.5 seconds may still be too hard to dodge even if target scanning works.
 local warnNightFall				= mod:NewSpellAnnounce(198401, 2)
 
 local specWarnNightfall			= mod:NewSpecialWarningMove(198408, nil, nil, nil, 1, 2)
