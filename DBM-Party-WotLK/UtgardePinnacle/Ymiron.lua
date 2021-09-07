6b8e0636a3d4e04ee44b82784e3036dbc5dd759f
MysticalOS <mysticalosx@gmail.com>
2021-09-07 17:13:45 -0400

    Fixed spam on Ymiron (wrath)

--- DBM-Party-WotLK/UtgardePinnacle/Ymiron.lua
+++ DBM-Party-WotLK/UtgardePinnacle/Ymiron.lua
@@ -10,7 +10,8 @@ mod:SetEncounterID(2028)
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
-	"SPELL_AURA_APPLIED 48294 59301 51750",
+	"SPELL_CAST_SUCCESS 51750",
+	"SPELL_AURA_APPLIED 48294 59301",
 	"SPELL_AURA_REMOVED 48294 59301"
 )
 
@@ -20,13 +21,17 @@ local warningScreams	= mod:NewSpellAnnounce(51750, 2)
 local timerBane			= mod:NewBuffActiveTimer(5, 48294, nil, nil, nil, 5, nil, DBM_CORE_L.MAGIC_ICON)
 local timerScreams		= mod:NewBuffActiveTimer(8, 51750, nil, nil, nil, 2)
 
+function mod:SPELL_CAST_SUCCESS(args)
+	if args.spellId == 51750 then
+		warningScreams:Show()
+		timerScreams:Start()
+	end
+end
+
 function mod:SPELL_AURA_APPLIED(args)
 	if args:IsSpellID(48294, 59301) then
 		warningBane:Show()
 		timerBane:Start()
-	elseif args.spellId == 51750 then
-		warningScreams:Show()
-		timerScreams:Start()
 	end
 end
 
