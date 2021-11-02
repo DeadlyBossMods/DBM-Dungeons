252b5c7db660ce43760d0ef7e940d37e1d2904d9
MysticalOS <mysticalosx@gmail.com>
2021-11-02 10:20:04 -0400

    bump tocs

--- DBM-Party-BC/Coil_Slavepens/Quagmirran.lua
+++ DBM-Party-BC/Coil_Slavepens/Quagmirran.lua
@@ -10,4 +10,20 @@ mod:SetEncounterID(1940)
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
+	"SPELL_CAST_SUCCESS 39340"
 )
+
+local WarnPoisonBoltVolley		= mod:NewSpellAnnounce(39340)
+
+local timerPoisonBoltVolleyCD	= mod:NewCDTimer(22.6, 39340, nil, nil, nil, 2)--22.6-67
+
+function mod:OnCombatStart(delay)
+	timerPoisonBoltVolleyCD:Start(17-delay)--iffy
+end
+
+function mod:SPELL_CAST_SUCCESS(args)
+	if args.spellId == 39340 then
+		WarnPoisonBoltVolley:Show()
+		timerPoisonBoltVolleyCD:Start()
+	end
+end
