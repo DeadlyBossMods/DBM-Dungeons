e387301d5f5e049222a6ed6b9dc9dfcdeda92e56
MysticalOS <mysticalosx@gmail.com>
2022-01-01 15:19:28 -0500

    Fixed Musel'ek warning spam do to bad warning event. Closes #28

--- DBM-Party-BC/Coil_Underbog/Muselek.lua
+++ DBM-Party-BC/Coil_Underbog/Muselek.lua
@@ -10,16 +10,21 @@ mod:SetEncounterID(1947)
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
-	"SPELL_AURA_APPLIED 31429 34971"
+	"SPELL_CAST_SUCCESS 31429",
+	"SPELL_AURA_APPLIED 34971"
 )
 
 local warnRoar		= mod:NewSpellAnnounce(31429, 2)
 local warnFrenzy	= mod:NewSpellAnnounce(34971, 4)
 
-function mod:SPELL_AURA_APPLIED(args)
+function mod:SPELL_CAST_SUCCESS(args)
 	if args.spellId == 31429 then
 		warnRoar:Show()
-	elseif args.spellId == 34971 then
+	end
+end
+
+function mod:SPELL_AURA_APPLIED(args)
+	if args.spellId == 34971 then
 		warnFrenzy:Show()
 	end
 end
