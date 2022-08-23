44da20dffc5ddc26b9fc354cc420cf0848595ab7
MysticalOS <mysticalosx@gmail.com>
2022-08-23 18:32:17 -0400

    lastly,, downgrade end warning to non special announce, doesn't need to be accessive for a positive announce

--- DBM-Party-WoD/IronDocks/Skulloc.lua
+++ DBM-Party-WoD/IronDocks/Skulloc.lua
@@ -26,12 +26,12 @@ mod:RegisterEventsInCombat(
 --TODO, verify gron smash numbers and see if it is time based or damage based.
 local warnRapidFire			= mod:NewTargetNoFilterAnnounce(168398, 3)
 local warnBackdraft			= mod:NewCastAnnounce(169129, 4)
+local warnCannonBarrageEnd	= mod:NewEndAnnounce(168929, 1)
 
 local specWarnRapidFire		= mod:NewSpecialWarningMoveAway(168398, nil, nil, nil, 1, 2)
 local yellRapidFire			= mod:NewYell(168398)
 local specWarnGronSmash		= mod:NewSpecialWarningSpell(168227, nil, nil, nil, 2, 2)
 local specWarnCannonBarrage	= mod:NewSpecialWarningSpell(168929, nil, nil, nil, 3, 2)--Use the one time cast trigger instead of drycode when relogging
-local specWarnCannonBarrageE= mod:NewSpecialWarningEnd(168929, nil, nil, nil, 1, 2)
 
 local timerRapidFireCD		= mod:NewCDTimer(11.5, 168398, nil, nil, nil, 3)
 local timerRapidFire		= mod:NewTargetTimer(5, 168398, nil, "-Tank", nil, 5)
