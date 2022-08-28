6ef2f11743d41bb49fbae81b6366f5893fcf8662
MysticalOS <mysticalosx@gmail.com>
2022-08-27 21:21:00 -0400

    Small pass on Nokgar to re-enable some timers off limited data that actually is in combat  log. But much can't be fixed without multiple transcriptor logs (or blizzard actually putting boss abilities into combat log)

--- DBM-Party-WoD/IronDocks/Nokgar.lua
+++ DBM-Party-WoD/IronDocks/Nokgar.lua
@@ -12,32 +12,59 @@ mod:SetBossHPInfoToHighest(false)
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
-	"SPELL_AURA_APPLIED 164426 164835 164632",
+	"SPELL_CAST_START 164426",
+	"SPELL_CAST_SUCCESS 164835",
+	"SPELL_AURA_APPLIED 164426 164632",
 	"SPELL_AURA_REMOVED 164426",
-	"UNIT_SPELLCAST_SUCCEEDED boss1",
+	"UNIT_SPELLCAST_SUCCEEDED boss1 boss2",
 	"UNIT_TARGETABLE_CHANGED"
 )
 
 --TODO, see if boss is missing UnitID in stage 1 in M+ version
+--[[
+ability.id = 164835 and type = "cast"
+ or ability.id = 164426 and type = "begincast"
+ or (source.type = "NPC" and source.firstSeen = timestamp) or (target.type = "NPC" and target.firstSeen = timestamp)
+ or type = "dungeonencounterstart" or type = "dungeonencounterend"
+--]]
+local warnBloodLettingHowl				= mod:NewSpellAnnounce(164835, 3)
 local warnNokgar						= mod:NewSpellAnnounce("ej10433", 3, "134170")
 
 local specWarnBurningArrows				= mod:NewSpecialWarningSpell(164635, nil, nil, nil, 2, 2)
 local specWarnBurningArrowsMove			= mod:NewSpecialWarningMove(164635, nil, nil, nil, 1, 8)
 local specWarnRecklessProvocation		= mod:NewSpecialWarningReflect(164426, nil, nil, nil, 1, 2)
 local specWarnRecklessProvocationEnd	= mod:NewSpecialWarningEnd(164426, nil, nil, nil, 1, 2)
-local specWarnEnrage					= mod:NewSpecialWarningDispel(164835, "RemoveEnrage", nil, nil, 1, 2)
 
+local timerRecklessProvocationCD		= mod:NewCDTimer(42.5, 164426, nil, nil, nil, 5, nil, DBM_COMMON_L.DAMAGE_ICON)
 local timerRecklessProvocation			= mod:NewBuffActiveTimer(5, 164426, nil, nil, nil, 5, nil, DBM_COMMON_L.DAMAGE_ICON)
---local timerBurningArrowsCD			= mod:NewNextTimer(25, 164635, nil, nil, nil, 3)--25~42 variable (patterned?)
+local timerBurningArrowsCD				= mod:NewCDTimer(25, 164635, nil, nil, nil, 3)--25~42 variable
+local timerBloodlettingHowlCD			= mod:NewCDTimer(25, 164835, nil, nil, nil, 5, nil, DBM_COMMON_L.ENRAGE_ICON)
+
+function mod:OnCombatStart(delay)
+	self:SetStage(1)
+	--timerBurningArrowsCD:Start()--Unknown, stupid non logged event
+	--timerBloodlettingHowlCD:Start(33.9-delay)--Iffy
+	--timerRecklessProvocationCD:Start(43.7-delay)--Iffy
+end
+
+function mod:SPELL_CAST_START(args)
+	if args.spellId == 164426 then
+		timerRecklessProvocationCD:Start()
+	end
+end
+
+function mod:SPELL_CAST_SUCCESS(args)
+	if args.spellId == 164835 then
+		warnBloodLettingHowl:Show()
+		timerBloodlettingHowlCD:Start()
+	end
+end
 
 function mod:SPELL_AURA_APPLIED(args)
 	if args.spellId == 164426 then
 		specWarnRecklessProvocation:Show(args.destName)
 		specWarnRecklessProvocation:Play("stopattack")
 		timerRecklessProvocation:Start()
-	elseif args.spellId == 164835 and self:AntiSpam(2, 1) then
-		specWarnEnrage:Show(args.destName)
-		specWarnEnrage:Play("enrage") --multi sound
 	elseif args.spellId == 164632 and args:IsPlayer() and self:AntiSpam(2, 2) then
 		specWarnBurningArrowsMove:Show()
 		specWarnBurningArrowsMove:Play("watchfeet")
@@ -56,10 +83,15 @@ function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, spellId)
 	if spellId == 164635 then
 		specWarnBurningArrows:Show()
 		specWarnBurningArrows:Play("watchfeet")
-		--timerBurningArrowsCD:Start()
+		timerBurningArrowsCD:Start(self.vb.phase == 1 and 25 or 40)
 	end
 end
 
 function mod:UNIT_TARGETABLE_CHANGED()
+	self:SetStage(2)
 	warnNokgar:Show()
+	timerRecklessProvocationCD:Stop()
+	timerBurningArrowsCD:Stop()
+	timerBloodlettingHowlCD:Stop()
+--	timerBurningArrowsCD:Start()
 end
