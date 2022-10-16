0a1031e49ace7044ccd53ec5b406de5706db76f1
MysticalOS <mysticalosx@gmail.com>
2022-10-16 00:29:08 -0400

    Update Overgorwn Ancient from M+

--- DBM-Party-Dragonflight/AlgetharAcademy/OvergrownAncient.lua
+++ DBM-Party-Dragonflight/AlgetharAcademy/OvergrownAncient.lua
@@ -5,16 +5,16 @@ mod:SetRevision("@file-date-integer@")
 mod:SetCreatureID(186951)
 mod:SetEncounterID(2563)
 --mod:SetUsedIcons(1, 2, 3)
---mod:SetHotfixNoticeRev(20220322000000)
+mod:SetHotfixNoticeRev(20221015000000)
 --mod:SetMinSyncRevision(20211203000000)
 --mod.respawnTime = 29
 
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
-	"SPELL_CAST_START 388923 388623 371453 388544",
+	"SPELL_CAST_START 388923 388623 396640 388544",
 --	"SPELL_CAST_SUCCESS",
-	"SPELL_AURA_APPLIED 388796 389033 371453",
+	"SPELL_AURA_APPLIED 388796 389033",
 	"SPELL_AURA_APPLIED_DOSE 389033",
 	"SPELL_AURA_REMOVED 389033",
 	"SPELL_AURA_REMOVED_DOSE 389033",
@@ -26,22 +26,28 @@ mod:RegisterEventsInCombat(
 
 --TODO, Fix Germinate event.
 --TODO, Branch Out target scan? it says "at a location" not "at a player"
---TODO, review interupt timer for add, if it's spam cast, scrap.
-local warnEntanglingRoots						= mod:NewTargetNoFilterAnnounce(371453, 3, nil, "RemoveMagic")
+--TODO, longer pull with two bursts to fine tune burst timer
+--TODO, longer pull to confirm if bark and germ are alternating timers, or timer reset by burst that may not always alternate
+--[[
+(ability.id = 388923 or ability.id = 388623 or ability.id = 396640 or ability.id = 388544) and type = "begincast"
+ or ability.id = 388796 and type = "applybuff"
+ or type = "dungeonencounterstart" or type = "dungeonencounterend"
+--]]
+--local warnEntanglingRoots						= mod:NewTargetNoFilterAnnounce(371453, 3, nil, "RemoveMagic")
 
 local specWarnGerminate							= mod:NewSpecialWarningDodge(388796, nil, nil, nil, 2, 2)
 local specWarnLasherToxin						= mod:NewSpecialWarningStack(389033, nil, 12, nil, nil, 1, 6)
 local specWarnBurstForth						= mod:NewSpecialWarningSpell(388923, nil, nil, nil, 2, 2)
 local specWarnBranchOut							= mod:NewSpecialWarningDodge(388623, nil, nil, nil, 2, 2)
-local specWarnEntanglingRoots					= mod:NewSpecialWarningInterrupt(371453, "HasInterrupt", nil, nil, 1, 2)
+local specWarnHealingTouch						= mod:NewSpecialWarningInterrupt(396640, "HasInterrupt", nil, nil, 1, 2)
 local specWarnBarkbreaker						= mod:NewSpecialWarningDefensive(388544, nil, nil, nil, 1, 2)
 --local specWarnGTFO							= mod:NewSpecialWarningGTFO(340324, nil, nil, nil, 1, 8)
 
-local timerGerminateCD							= mod:NewAITimer(35, 388796, nil, nil, nil, 3)
-local timerBurstForthCD							= mod:NewAITimer(35, 388923, nil, nil, nil, 2, nil, DBM_COMMON_L.HEALER_ICON)
-local timerBranchOutCD							= mod:NewAITimer(35, 388623, nil, nil, nil, 3)
-local timerEntanglingRootsCD					= mod:NewAITimer(35, 371453, nil, nil, nil, 4, nil, DBM_COMMON_L.INTERRUPT_ICON)
-local timerBarkbreakerCD						= mod:NewAITimer(35, 388544, nil, "Tank|Healer", nil, 5, nil, DBM_COMMON_L.HEALER_ICON)
+local timerGerminateCD							= mod:NewCDCountTimer(29.1, 388796, nil, nil, nil, 3)
+local timerBurstForthCD							= mod:NewCDTimer(49.8, 388923, nil, nil, nil, 2, nil, DBM_COMMON_L.HEALER_ICON)--Assumed it's on same cycle as branch out, CD not confirmed
+local timerBranchOutCD							= mod:NewCDTimer(49.8, 388623, nil, nil, nil, 3)
+local timerHealingTouchCD						= mod:NewCDTimer(5, 396640, nil, nil, nil, 4, nil, DBM_COMMON_L.INTERRUPT_ICON)--First cast only, after that it's iffy
+local timerBarkbreakerCD						= mod:NewCDCountTimer(35, 388544, nil, "Tank|Healer", nil, 5, nil, DBM_COMMON_L.HEALER_ICON)
 
 --local berserkTimer							= mod:NewBerserkTimer(600)
 
@@ -50,14 +56,18 @@ mod:AddInfoFrameOption(389033, "RemovePoison")
 --mod:AddSetIconOption("SetIconOnStaggeringBarrage", 361018, true, false, {1, 2, 3})
 
 local toxinStacks = {}
+mod.vb.germinateCount = 0
+mod.vb.barkCount = 0
 
 function mod:OnCombatStart(delay)
 	table.wipe(toxinStacks)
-	timerGerminateCD(1-delay)
-	timerBurstForthCD:Start(1-delay)
-	timerBranchOutCD:Start(1-delay)
-	timerBarkbreakerCD:Start(1-delay)
-	if self.Options.InfoFrame then
+	self.vb.germinateCount = 0
+	self.vb.barkCount = 0
+	timerBarkbreakerCD:Start(4.6-delay)
+	timerGerminateCD(13.1-delay, 1)
+	timerBranchOutCD:Start(30.1-delay)
+	timerBurstForthCD:Start(47.1-delay)
+	if self.Options.InfoFrame and self:IsMythic() then
 		DBM.InfoFrame:SetHeader(DBM:GetSpellInfo(389033))
 		DBM.InfoFrame:Show(5, "table", toxinStacks, 1)
 	end
@@ -79,18 +89,29 @@ function mod:SPELL_CAST_START(args)
 		specWarnBurstForth:Show()
 		specWarnBurstForth:Play("aesoon")
 		timerBurstForthCD:Start()
+
+		--The other possible timer explanation
+		--timerBarkbreakerCD:Restart(6, self.vb.barkCount+1)
+		--timerGerminateCD:Restart(15.7, self.vb.germinateCount+1)
 	elseif spellId == 388623 then
 		specWarnBranchOut:Show()
 		specWarnBranchOut:Play("watchstep")
 		timerBranchOutCD:Start()
-	elseif spellId == 371453 then
-		timerEntanglingRootsCD:Start(nil, args.sourceGUID)
+		timerHealingTouchCD:Start(5)
+	elseif spellId == 396640 then
+		timerHealingTouchCD:Start()
 		if self:CheckInterruptFilter(args.sourceGUID, false, true) then
-			specWarnEntanglingRoots:Show(args.sourceName)
-			specWarnEntanglingRoots:Play("kickcast")
+			specWarnHealingTouch:Show(args.sourceName)
+			specWarnHealingTouch:Play("kickcast")
 		end
 	elseif spellId == 388544 then
-		timerBarkbreakerCD:Start()
+		self.vb.barkCount = self.vb.barkCount + 1
+		--4.6, 30.3, 18.2, 29.2
+		if self.vb.germinateCount % 2 == 0 then
+			timerBarkbreakerCD:Start(18.2, self.vb.barkCount+1)
+		else
+			timerBarkbreakerCD:Start(29.2, self.vb.barkCount+1)
+		end
 		if self:IsTanking("player", "boss1", nil, true) then
 			specWarnBarkbreaker:Show()
 			specWarnBarkbreaker:Play("defensive")
@@ -110,9 +131,15 @@ end
 function mod:SPELL_AURA_APPLIED(args)
 	local spellId = args.spellId
 	if spellId == 388796 then
+		self.vb.germinateCount = self.vb.germinateCount + 1
 		specWarnGerminate:Show()
 		specWarnGerminate:Play("watchstep")
-		timerGerminateCD:Start()
+		--13.1, 29.1, 20.6, 29.1 (needs larger sample to confirm)
+		if self.vb.germinateCount % 2 == 0 then
+			timerGerminateCD:Start(20.6, self.vb.germinateCount+1)
+		else
+			timerGerminateCD:Start(29.1, self.vb.germinateCount+1)
+		end
 	elseif spellId == 389033 then
 		local amount = args.amount or 1
 		toxinStacks[args.destName] = amount
@@ -124,8 +151,6 @@ function mod:SPELL_AURA_APPLIED(args)
 			specWarnLasherToxin:Schedule(0.2, amount)
 			specWarnLasherToxin:ScheduleVoice(0.2, "stackhigh")
 		end
-	elseif spellId == 371453 then
-		warnEntanglingRoots:Show(args.destName)
 	end
 end
 mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
@@ -153,7 +178,7 @@ end
 function mod:UNIT_DIED(args)
 	local cid = self:GetCIDFromGUID(args.destGUID)
 	if cid == 196548 then--Ancient Branch
-		timerEntanglingRootsCD:Stop(args.destGUID)
+		timerHealingTouchCD:Stop()
 	end
 end
 
