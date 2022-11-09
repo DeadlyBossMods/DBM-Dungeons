9d61045970d33f5635a0b0a61e7c31f31faf3628
Artemis <QartemisT@gmail.com>
2022-11-09 18:39:06 +0000

    Fix sosund setting for specWarnFizzyBubbles
    Fixes https://github.com/DeadlyBossMods/DBM-Retail/issues/825

--- DBM-Party-MoP/StormstoutBrewery/YanZhu.lua
+++ DBM-Party-MoP/StormstoutBrewery/YanZhu.lua
@@ -27,7 +27,7 @@ local warnCarbonation		= mod:NewSpellAnnounce(115003, 4)
 
 local specWarnBloat			= mod:NewSpecialWarningYou(106546, nil, nil, nil, 1, 2)
 local specWarnBlackoutBrew	= mod:NewSpecialWarningKeepMove(106851, nil, nil, nil, 1, 2)--Moving clears this debuff, it should never increase unless you're doing fight wrong (think Hodir)
-local specWarnFizzyBubbles	= mod:NewSpecialWarning("SpecWarnFizzyBubbles", nil, nil, nil, 1)
+local specWarnFizzyBubbles	= mod:NewSpecialWarning("SpecWarnFizzyBubbles", nil, nil, nil, 1, 2)
 
 local timerBloatCD			= mod:NewCDTimer(13.3, 106546, nil, nil, nil, 3)
 local timerBloat			= mod:NewBuffFadesTimer(30, 106546, nil, nil, nil, 5)
