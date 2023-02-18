a2f308d7e571f8c8f2bce8a4b36a4c0f28766140
MysticalOS <mysticalosx@gmail.com>
2023-02-18 16:05:54 -0500

    sync hybrid mekgineer mod to retail, because at some point i want mods unified and pulled from single branches

--- DBM-Party-Vanilla/Gnomeregan/MekgineerThermaplugg.lua
+++ DBM-Party-Vanilla/Gnomeregan/MekgineerThermaplugg.lua
@@ -1,5 +1,9 @@
-local mod	= DBM:NewMod(422, "DBM-Party-Vanilla", 4, 231)
+local mod	= DBM:NewMod(422, "DBM-Party-Vanilla", 7, 231)
 local L		= mod:GetLocalizedStrings()
+
+local isRetail = WOW_PROJECT_ID == (WOW_PROJECT_MAINLINE or 1)
+local isClassic = WOW_PROJECT_ID == (WOW_PROJECT_CLASSIC or 2)
+local isBCC = WOW_PROJECT_ID == (WOW_PROJECT_BURNING_CRUSADE_CLASSIC or 5)
 
 mod:SetRevision("@file-date-integer@")
 mod:SetCreatureID(7800)
@@ -7,22 +11,51 @@ mod:SetEncounterID(382)
 
 mod:RegisterCombat("combat")
 
-mod:RegisterEventsInCombat(
-	"SPELL_CAST_START 93655",
-	"SPELL_CAST_SUCCESS 74720",
-	"SPELL_AURA_APPLIED 74720"
-)
+if isRetail then
+	mod:RegisterEventsInCombat(
+		"SPELL_CAST_START 93655",
+		"SPELL_CAST_SUCCESS 74720",
+		"SPELL_AURA_APPLIED 74720"
+	)
+elseif isClassic or isBCC then
+	mod:RegisterEventsInCombat(
+		"SPELL_CAST_SUCCESS 10101 11130 11518 11521 11798 11524 11526 11527"
+	)
+else
+	mod:RegisterEventsInCombat(
+		"SPELL_CAST_SUCCESS 10101 11130"
+	)
+end
 
-local warningPound				= mod:NewTargetAnnounce(32346, 2)
+--Only retail, he was reworked in cataclysm, so it'll likely also apply to cataclysm classic
+local warningPound, specWarnSteamBlast, timerSteamBlastCD, timerPoundCD
+local warningKnockAway, timerKnockAwayCD
+if isRetail then
+	warningPound				= mod:NewTargetNoFilterAnnounce(32346, 2)
 
-local specWarnSteamBlast		= mod:NewSpecialWarningInterrupt(93655, "HasInterrupt", nil, nil, 1, 2)
+	specWarnSteamBlast			= mod:NewSpecialWarningInterrupt(93655, "HasInterrupt", nil, nil, 1, 2)
 
-local timerSteamBlastCD			= mod:NewAITimer(180, 93655, nil, nil, nil, 4, nil, DBM_COMMON_L.INTERRUPT_ICON)
-local timerPoundCD				= mod:NewAITimer(180, 74720, nil, "Tank|Healer", nil, 5, nil, DBM_COMMON_L.TANK_ICON)
+	timerSteamBlastCD			= mod:NewAITimer(180, 93655, nil, nil, nil, 4, nil, DBM_COMMON_L.INTERRUPT_ICON)
+	timerPoundCD				= mod:NewAITimer(180, 74720, nil, "Tank|Healer", nil, 5, nil, DBM_COMMON_L.TANK_ICON)
+else--All Classic Flavors have this (including wrath and maybe cata?
+	warningKnockAway			= mod:NewSpellAnnounce(10101, 2)
+
+	timerKnockAwayCD			= mod:NewAITimer(180, 10101, nil, nil, nil, 2)
+end
+
+--Only vanilla and tbc have this
+local warningActivateBomb
+if isClassic or isBCC then
+	warningActivateBomb			= mod:NewSpellAnnounce(11518, 2)
+end
 
 function mod:OnCombatStart(delay)
-	timerSteamBlastCD:Start(1-delay)
-	timerPoundCD:Start(1-delay)
+	if isRetail then
+		timerSteamBlastCD:Start(1-delay)
+		timerPoundCD:Start(1-delay)
+	else
+		timerKnockAwayCD:Start(1-delay)
+	end
 end
 
 function mod:SPELL_CAST_START(args)
@@ -36,7 +69,12 @@ function mod:SPELL_CAST_START(args)
 end
 
 function mod:SPELL_CAST_SUCCESS(args)
-	if args.spellId == 74720 then
+	if args:IsSpellID(10101, 11130) then
+		warningKnockAway:Show()
+		timerKnockAwayCD:Start()
+	elseif args:IsSpellID(11518, 11521, 11798, 11524, 11526, 11527) and self:AntiSpam(3, 1) then
+		warningActivateBomb:Show()
+	elseif args.spellId == 74720 then
 		timerSteamBlastCD:Start()
 	end
 end
@@ -44,5 +82,7 @@ end
 function mod:SPELL_AURA_APPLIED(args)
 	if args.spellId == 74720 then
 		warningPound:Show(args.destName)
+		timerPoundCD:Start()
 	end
 end
+
