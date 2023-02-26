7ef9c18f3363f2ef449f6af67bbd1759c13cab90
MysticalOS <mysticalosx@gmail.com>
2023-02-26 00:44:35 -0500

    Fix bug in last jade commit that didn't cancel the new timers on trash deaths Added timers to several nokhud trash abilities

--- DBM-Party-Dragonflight/TheNokhudOffensive/TheNokhudOffensiveTrash.lua
+++ DBM-Party-Dragonflight/TheNokhudOffensive/TheNokhudOffensiveTrash.lua
@@ -6,16 +6,20 @@ mod:SetRevision("@file-date-integer@")
 mod.isTrashMod = true
 
 mod:RegisterEvents(
-	"SPELL_CAST_START 387145 386024 387127 384336 387629 387614 387411 382233 373395 383823 384365 384476 386694 387125 387440",
-	"SPELL_AURA_APPLIED 395035 334610 386223"
+	"SPELL_CAST_START 387145 386024 387127 384336 387629 387614 387411 382233 373395 383823 384365 386694 387125 387440",
+	"SPELL_CAST_SUCCESS 384476",
+	"SPELL_AURA_APPLIED 395035 334610 386223",
 --	"SPELL_AURA_APPLIED_DOSE 339528",
---	"SPELL_AURA_REMOVED 339525"
+--	"SPELL_AURA_REMOVED 339525",
+	"UNIT_DIED"
 )
 
 --TODO, https://www.wowhead.com/beta/spell=381683/swift-stab ?
 --TODO, target scan https://www.wowhead.com/beta/spell=387127/chain-lightning ?
 --Lady's Trash, minus bottled anima, which will need a unit event to detect it looks like
---local warnConcentrateAnima					= mod:NewTargetNoFilterAnnounce(339525, 3)
+--[[
+(ability.id = 373395 or ability.id = 387411 or ability.id = 373395 or ability.id = 383823 or ability.id = 384365 or ability.id = 387440 or ability.id = 384336 or ability.id = 386024) and type = "begincast"
+--]]
 local warnTotemicOverload					= mod:NewCastAnnounce(387145, 3)
 local warnChantoftheDead					= mod:NewCastAnnounce(387614, 3)
 local warnTempest							= mod:NewCastAnnounce(373395, 4)
@@ -43,6 +47,14 @@ local specWarnDeathBoltVolley				= mod:NewSpecialWarningInterrupt(387411, "HasIn
 local specWarnBloodcurdlingShout			= mod:NewSpecialWarningInterrupt(373395, "HasInterrupt", nil, nil, 1, 2)
 local specWarnDisruptiveShout				= mod:NewSpecialWarningInterrupt(384365, "HasInterrupt", nil, nil, 1, 2)
 
+local timerRallytheClanCD					= mod:NewCDTimer(20.6, 383823, nil, nil, nil, 5)--20-23
+local timerWarStompCD						= mod:NewCDTimer(15.7, 384336, nil, nil, nil, 3)
+local timerDisruptingShoutCD				= mod:NewCDTimer(21.8, 384365, nil, "HasInterrupt", nil, 4, nil, DBM_COMMON_L.INTERRUPT_ICON)--20-30ish
+local timerTempestCD						= mod:NewCDTimer(20.6, 386024, nil, "HasInterrupt", nil, 4, nil, DBM_COMMON_L.INTERRUPT_ICON)--20-25
+local timerDesecratingRoarCD				= mod:NewCDTimer(15.8, 387440, nil, "HasInterrupt", nil, 4, nil, DBM_COMMON_L.INTERRUPT_ICON)
+local timerDeathBoltVolleyCD				= mod:NewCDTimer(10.9, 387411, nil, "HasInterrupt", nil, 4, nil, DBM_COMMON_L.INTERRUPT_ICON)
+local timerBloodcurdlingShoutCD				= mod:NewCDTimer(19.1, 373395, nil, "HasInterrupt", nil, 4, nil, DBM_COMMON_L.INTERRUPT_ICON)
+
 --local playerName = UnitName("player")
 
 --Antispam IDs for this mod: 1 run away, 2 dodge, 3 dispel, 4 incoming damage, 5 you/role, 6 misc, 7 off interrupt
@@ -64,6 +76,7 @@ function mod:SPELL_CAST_START(args)
 	if spellId == 387145 and self:AntiSpam(5, 4) then
 		warnTotemicOverload:Show()
 	elseif spellId == 386024 then
+		timerTempestCD:Start(nil, args.sourceGUID)
 		if self.Options.SpecWarn386024interrupt and self:CheckInterruptFilter(args.sourceGUID, false, true) then
 			specWarnTempest:Show(args.sourceName)
 			specWarnTempest:Play("kickcast")
@@ -71,6 +84,7 @@ function mod:SPELL_CAST_START(args)
 			warnTempest:Show()
 		end
 	elseif spellId == 387411 then
+		timerDeathBoltVolleyCD:Start(nil, args.sourceGUID)
 		if self.Options.SpecWarn387411interrupt and self:CheckInterruptFilter(args.sourceGUID, false, true) then
 			specWarnDeathBoltVolley:Show(args.sourceName)
 			specWarnDeathBoltVolley:Play("kickcast")
@@ -78,17 +92,25 @@ function mod:SPELL_CAST_START(args)
 			warnDeathBoltVolley:Show()
 		end
 	elseif spellId == 373395 then
+		timerBloodcurdlingShoutCD:Start(nil, args.sourceGUID)
 		if self.Options.SpecWarn373395interrupt and self:CheckInterruptFilter(args.sourceGUID, false, true) then
 			specWarnBloodcurdlingShout:Show(args.sourceName)
 			specWarnBloodcurdlingShout:Play("kickcast")
 		elseif self:AntiSpam(3, 7) then
 			warnBloodcurdlingShout:Show()
 		end
-	elseif spellId == 383823 and self:AntiSpam(3, 5) then
-		warnRallytheClan:Show()
-	elseif spellId == 387440 and self:AntiSpam(3, 5) then
-		warnDesecratingRoar:Show()
+	elseif spellId == 383823 then
+		timerRallytheClanCD:Start(nil, args.sourceGUID)
+		if self:AntiSpam(3, 5) then
+			warnRallytheClan:Show()
+		end
+	elseif spellId == 387440 then
+		timerDesecratingRoarCD:Start(nil, args.sourceGUID)
+		if self:AntiSpam(3, 5) then
+			warnDesecratingRoar:Show()
+		end
 	elseif spellId == 384365 then
+		timerDisruptingShoutCD:Start(nil, args.sourceGUID)
 		if self.Options.SpecWarn384365interrupt and self:CheckInterruptFilter(args.sourceGUID, false, true) then
 			specWarnDisruptiveShout:Show(args.sourceName)
 			specWarnDisruptiveShout:Play("kickcast")
@@ -97,9 +119,12 @@ function mod:SPELL_CAST_START(args)
 		end
 	elseif spellId == 387127 then
 		self:ScheduleMethod(0.1, "BossTargetScanner", args.sourceGUID, "CLTarget", 0.1, 8)
-	elseif spellId == 384336 and self:AntiSpam(3, 2) then
-		specWarnWarStomp:Show()
-		specWarnWarStomp:Play("watchstep")
+	elseif spellId == 384336 then
+		timerWarStompCD:Start(nil, args.sourceGUID)
+		if self:AntiSpam(3, 2) then
+			specWarnWarStomp:Show()
+			specWarnWarStomp:Play("watchstep")
+		end
 	elseif spellId == 384476 and self:AntiSpam(3, 2) then
 		specWarnRainofArrows:Show()
 		specWarnRainofArrows:Play("watchstep")
@@ -115,6 +140,15 @@ function mod:SPELL_CAST_START(args)
 		warnStormsurge:Show()
 	elseif spellId == 387125 and self:AntiSpam(3, 5) then
 		warnThunderstrike:Show()
+	end
+end
+
+function mod:SPELL_CAST_SUCCESS(args)
+	local spellId = args.spellId
+	if not self:IsValidWarning(args.sourceGUID) then return end
+	if spellId == 384476 and self:AntiSpam(3, 2) then
+		specWarnRainofArrows:Show()
+		specWarnRainofArrows:Play("watchstep")
 	end
 end
 
@@ -134,3 +168,21 @@ function mod:SPELL_AURA_APPLIED(args)
 	end
 end
 mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
+
+function mod:UNIT_DIED(args)
+	local cid = self:GetCIDFromGUID(args.destGUID)
+	if cid == 192796 then--Nokhud Hornsounder
+		timerRallytheClanCD:Stop(args.destGUID)
+	elseif cid == 191847 then--Nokhud Plainstomper
+		timerWarStompCD:Stop(args.destGUID)
+		timerDisruptingShoutCD:Stop(args.destGUID)
+	elseif cid == 194894 then--Primalist Stormspeaker
+		timerTempestCD:Stop(args.destGUID)
+	elseif cid == 195878--Uthel Beastcaller
+		timerDesecratingRoarCD:Stop(args.destGUID)
+	elseif cid == 195928 or cid == 195927 or cid == 195930 or cid == 195929 then--All 4 Soulharvesters
+		timerDeathBoltVolleyCD:Stop(args.destGUID)
+	elseif cid == 193462--Batak
+		timerBloodcurdlingShoutCD:Stop(args.destGUID)
+	end
+end
