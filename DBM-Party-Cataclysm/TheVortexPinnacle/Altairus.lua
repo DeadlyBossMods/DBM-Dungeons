5bddc1b2b914a2ba0449383636efbb74fb79011c
MysticalOS <mysticalosx@gmail.com>
2023-03-08 19:02:44 -0500

    Vortex Pinnacle update  - Update all bosses  with missing timers filled in  - Populated trash module with every notable spell, complete with timers and alerts

--- DBM-Party-Cataclysm/TheVortexPinnacle/Altairus.lua
+++ DBM-Party-Cataclysm/TheVortexPinnacle/Altairus.lua
@@ -12,17 +12,20 @@ mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
 	"SPELL_AURA_APPLIED 88282 88286",
-	"SPELL_CAST_START 88308"
+	"SPELL_CAST_START 88308",
+	"UNIT_SPELLCAST_SUCCEEDED boss1"
 )
 
 local warnBreath			= mod:NewTargetNoFilterAnnounce(88308, 2)
+local warnCalltheWind		= mod:NewSpellAnnounce(88276, 2)
 local warnUpwind			= mod:NewSpellAnnounce(88282, 1)
 
 local specWarnBreath		= mod:NewSpecialWarningYou(88308, "-Tank", nil, 2, 1, 2)
 local specWarnBreathNear	= mod:NewSpecialWarningClose(88308, nil, nil, nil, 1, 2)
-local specWarnDownwind		= mod:NewSpecialWarningLookAway(88286, nil, nil, nil, 1, 2)
+local specWarnDownwind		= mod:NewSpecialWarningSpell(88286, nil, nil, nil, 1, 14)
 
-local timerBreathCD			= mod:NewCDTimer(10.5, 88308, nil, nil, nil, 3)
+local timerCalltheWindCD	= mod:NewCDTimer(21.9, 88276, nil, nil, nil, 6)
+local timerBreathCD			= mod:NewCDTimer(13.4, 88308, nil, nil, nil, 3)--May be 10.5 pre nerf for cata classic
 
 mod:AddSetIconOption("BreathIcon", 88308, true, false, {8})
 
@@ -47,6 +50,8 @@ end
 
 function mod:OnCombatStart(delay)
 	self.vb.activeWind = "none"
+	timerCalltheWindCD:Start(5.8-delay)
+	timerBreathCD:Start(10.7-delay)
 end
 
 function mod:SPELL_AURA_APPLIED(args)
@@ -55,7 +60,7 @@ function mod:SPELL_AURA_APPLIED(args)
 		self.vb.activeWindactiveWind = "up"
 	elseif args.spellId == 88286 and args:IsPlayer() and self.vb.activeWindactiveWind ~= "down" then
 		specWarnDownwind:Show()
-		specWarnDownwind:Play("turnaway")
+		specWarnDownwind:Play("getupwind")
 		self.vb.activeWindactiveWind = "down"
 	end
 end
@@ -64,5 +69,12 @@ function mod:SPELL_CAST_START(args)
 	if args.spellId == 88308 then
 		self:ScheduleMethod(0.2, "BreathTarget")
 		timerBreathCD:Start()
+	end
+end
+
+function mod:UNIT_SPELLCAST_SUCCEEDED(_, _, spellId)
+	if spellId == 88276 then
+		warnCalltheWind:Show()
+		timerCalltheWindCD:Start()
 	end
 end
--- DBM-Party-Cataclysm/TheVortexPinnacle/Asaad.lua
+++ DBM-Party-Cataclysm/TheVortexPinnacle/Asaad.lua
@@ -14,18 +14,26 @@ mod:RegisterEventsInCombat(
 	"SPELL_CAST_START 87618"
 )
 
+--If cataclysm classic is pre nerf, static cling has shorter cast and needs faster alert
 --local warnStaticCling			= mod:NewSpellAnnounce(87618, 3)
 
 local specWarnStaticCling		= mod:NewSpecialWarningJump(87618, nil, nil, nil, 1, 2)
 local specWarnGroundingField	= mod:NewSpecialWarningMoveTo(86911, nil, DBM_CORE_L.AUTO_SPEC_WARN_OPTIONS.run:format(86911), nil, nil, 3)
 
+local timerStaticClingCD		= mod:NewCDTimer(15.8, 87618, nil, nil, nil, 2)
 local timerGroundingField		= mod:NewCastTimer(10, 86911, nil, nil, nil, 2)
-local timerGroundingFieldCD		= mod:NewCDTimer(45, 86911, nil, nil, nil, 2, nil, DBM_COMMON_L.DEADLY_ICON)
+local timerGroundingFieldCD		= mod:NewCDTimer(46.1, 86911, nil, nil, nil, 2, nil, DBM_COMMON_L.DEADLY_ICON)
+
+function mod:OnCombatStart(delay)
+	timerStaticClingCD:Start(10.7-delay)
+	timerGroundingFieldCD:Start(16.9-delay)
+end
 
 function mod:SPELL_AURA_APPLIED(args)
 	if args.spellId == 86911 and self:AntiSpam(5, 1) then
 		specWarnGroundingField:Show(args.spellName)
 		timerGroundingField:Start()
+		timerStaticClingCD:Start(12)
 		timerGroundingFieldCD:Start()
 	end
 end
@@ -33,7 +41,10 @@ end
 function mod:SPELL_CAST_START(args)
 	if args.spellId == 87618 then
 --		warnStaticCling:Show(args.spellName)
-		specWarnStaticCling:Schedule(0.625)--delay message since jumping at start of cast is no longer correct in 4.0.6+
-		specWarnStaticCling:ScheduleVoice(0.625, "jumpnow")
+		specWarnStaticCling:Schedule(0.3)--delay message since jumping at start of cast is no longer correct in 4.0.6+
+		specWarnStaticCling:ScheduleVoice(0.3, "jumpnow")
+		if timerGroundingFieldCD:GetRemaining() < 15.8 then
+			timerStaticClingCD:Start()
+		end
 	end
 end
