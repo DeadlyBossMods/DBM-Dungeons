8405324f317b869e4ad7b0fdcb03ea21245b91a0
Yllelder Bamir <Yllelder@users.noreply.github.com>
2023-03-15 14:14:11 +0100

    Create localization.es.lua

---
+++ DBM-Party-Dragonflight/localization.es.lua
@@ -0,0 +1,286 @@
+if GetLocale() ~= "esES" then return end
+local L
+
+-----------------------
+-- <<<Brackenhide Hollow >>> --
+-----------------------
+-----------------------
+-- Hackclaw's War-Band --
+-----------------------
+--L= DBM:GetModLocalization(2471)
+
+-----------------------
+-- Treemouth  --
+-----------------------
+--L= DBM:GetModLocalization(2473)
+
+-----------------------
+-- Gutshot --
+-----------------------
+--L= DBM:GetModLocalization(2472)
+
+-----------------------
+-- Decatriarch Wratheye --
+-----------------------
+--L= DBM:GetModLocalization(2474)
+
+---------
+--Trash--
+---------
+L = DBM:GetModLocalization("BrackenhideHollowTrash")
+
+L:SetGeneralLocalization({
+	name =	"Pulls de Hondonada Frondacuero"
+})
+
+-----------------------
+-- <<<Uldaman: Legacy of Tyr >>> --
+-----------------------
+-----------------------
+-- The Lost Dwarves --
+-----------------------
+--L= DBM:GetModLocalization(2475)
+
+-----------------------
+-- Bromach --
+-----------------------
+--L= DBM:GetModLocalization(2487)
+
+-----------------------
+-- Sentinel Talondras --
+-----------------------
+--L= DBM:GetModLocalization(2484)
+
+-----------------------
+-- Emberon --
+-----------------------
+--L= DBM:GetModLocalization(2476)
+
+-----------------------
+-- Chrono-Lord Deios --
+-----------------------
+--L= DBM:GetModLocalization(2479)
+
+---------
+--Trash--
+---------
+L = DBM:GetModLocalization("UldamanLegacyofTyrTrash")
+
+L:SetGeneralLocalization({
+	name =	"Pulls de Uldaman: Legado de Tyr"
+})
+
+-----------------------
+-- <<<The Nokhud Offensive >>> --
+-----------------------
+-----------------------
+-- Granyth --
+-----------------------
+--L= DBM:GetModLocalization(2498)
+
+-----------------------
+-- The Raging Tempest --
+-----------------------
+--L= DBM:GetModLocalization(2497)
+
+-----------------------
+-- Teera and Maruuk --
+-----------------------
+--L= DBM:GetModLocalization(2478)
+
+-----------------------
+-- Balakar Khan --
+-----------------------
+--L= DBM:GetModLocalization(2477)
+
+
+---------
+--Trash--
+---------
+L = DBM:GetModLocalization("TheNokhudOffensiveTrash")
+
+L:SetGeneralLocalization({
+	name =	"Pulls de Ofensiva Nokhud"
+})
+
+L:SetMiscLocalization({
+	Soul = "Alma"
+})
+
+-----------------------
+-- <<<Neltharus >>> --
+-----------------------
+-----------------------
+-- Chargath, Bane of Scales --
+-----------------------
+--L= DBM:GetModLocalization(2490)
+
+-----------------------
+-- The Scorching Forge --
+-----------------------
+--L= DBM:GetModLocalization(2489)
+
+-----------------------
+-- Magmatusk --
+-----------------------
+--L= DBM:GetModLocalization(2494)
+
+-----------------------
+-- Warlord Sargha --
+-----------------------
+--L= DBM:GetModLocalization(2501)
+
+---------
+--Trash--
+---------
+L = DBM:GetModLocalization("NeltharusTrash")
+
+L:SetGeneralLocalization({
+	name =	"Pulls de Neltharus"
+})
+
+-----------------------
+-- <<<Algeth'ar Academy >>> --
+-----------------------
+-----------------------
+-- Crawth --
+-----------------------
+--L= DBM:GetModLocalization(2495)
+
+-----------------------
+-- Vexamus --
+-----------------------
+L= DBM:GetModLocalization(2509)
+
+L:SetMiscLocalization({
+	VexRP		= "¡Ah! ¡Vamos allá!... Ejem. Hace mucho tiempo, los miembros del Vuelo Azul sobrecargaron por accidente a un elemental Arcano, lo que creó un poderoso ensamblaje llamado Vexamus que empezó a sembrar el caos."
+})
+
+-----------------------
+-- Overgrown Ancient --
+-----------------------
+L= DBM:GetModLocalization(2512)
+
+L:SetMiscLocalization({
+	TreeRP	= "Perfecto, estamos a punto de... ¡Ichistrasz! ¡Hay demasiada magia vital! ¿Qué estás haciendo?"
+})
+
+-----------------------
+-- Echo of Doragosa --
+-----------------------
+--L= DBM:GetModLocalization(2514)
+
+---------
+--Trash--
+---------
+L = DBM:GetModLocalization("AlgetharAcademyTrash")
+
+L:SetGeneralLocalization({
+	name =	"Pulls de Academia Algeth'ar"
+})
+
+L:SetOptionLocalization({
+	AGBuffs		= "Selección automática de diálogos para activar potenciadores al interactuar con los PNJ dragones."
+})
+
+-----------------------
+-- <<<The Azure Vault>>> --
+-----------------------
+-----------------------
+-- Leymor --
+-----------------------
+--L= DBM:GetModLocalization(2492)
+
+-----------------------
+-- Talash Greywing --
+-----------------------
+--L= DBM:GetModLocalization(2483)
+
+-----------------------
+-- Umbrelskul --
+-----------------------
+--L= DBM:GetModLocalization(2508)
+
+-----------------------
+-- Azureblade --
+-----------------------
+--L= DBM:GetModLocalization(2505)
+
+---------
+--Trash--
+---------
+L = DBM:GetModLocalization("TheAzurevaultTrash")
+
+L:SetGeneralLocalization({
+	name =	"Pulls de Cámara Azur"
+})
+
+L:SetOptionLocalization({
+	AGBook			= "Selección automática del diálogo para el teletransporte al interactuar con libros."
+})
+
+-----------------------
+-- <<<Ruby Life Pools>>> --
+-----------------------
+-----------------------
+-- Melidrussa Chillworn --
+-----------------------
+--L= DBM:GetModLocalization(2488)
+
+-----------------------
+-- Kokia Blazehoof --
+-----------------------
+--L= DBM:GetModLocalization(2485)
+
+-----------------------
+-- Kyrakka and Erkhart Stormvein --
+-----------------------
+L= DBM:GetModLocalization(2503)
+
+L:SetMiscLocalization({
+	North	= "Hacia el norte",
+	West	= "Hacia el oeste",
+	South	= "Hacia el sur",
+	East	= "Hacia el este"
+})
+
+---------
+--Trash--
+---------
+L = DBM:GetModLocalization("RubyLifePoolsTrash")
+
+L:SetGeneralLocalization({
+	name =	"Pulls de Estanques de Vida Rubí"
+})
+
+-----------------------
+-- <<<Halls of Infusion>>> --
+-----------------------
+-----------------------
+-- Watcher Irideus --
+-----------------------
+--L= DBM:GetModLocalization(2504)
+
+-----------------------
+-- Gulping Goliath --
+-----------------------
+--L= DBM:GetModLocalization(2507)
+
+-----------------------
+-- Khajin the Unyielding --
+-----------------------
+--L= DBM:GetModLocalization(2510)
+
+-----------------------
+-- Primal Tsunami --
+-----------------------
+--L= DBM:GetModLocalization(2511)
+
+---------
+--Trash--
+---------
+L = DBM:GetModLocalization("HallsofInfusionTrash")
+
+L:SetGeneralLocalization({
+	name =	"Pulls de Salas de Infusión"
+})
