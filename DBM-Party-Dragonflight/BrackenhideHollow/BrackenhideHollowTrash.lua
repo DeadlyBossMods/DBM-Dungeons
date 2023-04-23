82338982558335395e0b8b778eb1976fe2fb4359
MysticalOS <mysticalosx@gmail.com>
2023-04-22 23:33:36 -0400

    Add lethal current to VP trash mod Added many missing abilities to Brackenhide Hollow trash mod Added first pass on timers to Brackenhide Hollow trash mod as well

--- DBM-Party-Dragonflight/BrackenhideHollow/BrackenhideHollowTrash.lua
+++ DBM-Party-Dragonflight/BrackenhideHollow/BrackenhideHollowTrash.lua
@@ -6,27 +6,39 @@ mod:SetRevision("@file-date-integer@")
 mod.isTrashMod = true
 
 mod:RegisterEvents(
-	"SPELL_CAST_START 382555 367500 388060 388046 382474 382787 374544 385029 383062 367503",
-	"SPELL_CAST_SUCCESS 368287 383385 382435 384930",
-	"SPELL_AURA_APPLIED 382555 383087 383399 385058 385827 384974 367484",
+	"SPELL_CAST_START 382555 367500 388060 388046 382474 382787 374544 385029 383062 367503 373897 382712 373943 374569 385832",
+	"SPELL_CAST_SUCCESS 382555 368287 383385 382435 384930 372711",
+	"SPELL_SUMMON 374057",
+	"SPELL_AURA_APPLIED 383087 383399 385058 385827 384974 367484 368081",
 --	"SPELL_AURA_APPLIED_DOSE 339528",
 --	"SPELL_AURA_REMOVED 339525",
 	"SPELL_PERIODIC_DAMAGE 383399",
-	"SPELL_PERIODIC_MISSED 383399"
+	"SPELL_PERIODIC_MISSED 383399",
+	"UNIT_DIED"
 )
 
 --TODO: can https://www.wowhead.com/ptr/spell=383385/rotting-surge can be stunned (but not interrupted)
 --TODO, can burst of decay be interrupted/stunned or target scanned?
 --TODO, Add https://www.wowhead.com/spell=382593/crushing-smash?
 --TODO, add https://www.wowhead.com/spell=382410/witherbolt interrupt warning?
+--TODO, ragestorm cd might not start til last one is stopped? need more data
+--[[
+(ability.id = 382555 or ability.id = 367500 or ability.id = 388060 or ability.id = 388046 or ability.id = 382474 or ability.id = 382787 or ability.id = 374544 or ability.id = 385029 or ability.id = 383062 or ability.id = 367503 or ability.id = 373897 or ability.id = 382712 or ability.id = 373943 or ability.id = 374569 or ability.id = 385832) and type = "begincast"
+ or (ability.id = 368287 or ability.id = 383385 or ability.id = 382435 or ability.id = 384930 or ability.id = 372711) and type = "cast"
+ or ability.id = 374057
+--]]
 local warnBurstofDecay						= mod:NewCastAnnounce(374544, 4)--Change to target scan?
 local warnHidiousCackle						= mod:NewCastAnnounce(367500, 4)
 local warnScreech							= mod:NewCastAnnounce(385029, 4)
 local warnDecayClaws						= mod:NewCastAnnounce(382787, 4, nil, nil, "Tank|Healer")
 local warnSummonLashers						= mod:NewCastAnnounce(383062, 2, nil, nil, "Tank")
+local warnDecayingRoots						= mod:NewCastAnnounce(373897, 3)
+local warnBurst								= mod:NewCastAnnounce(374569, 4)
 local warnWitheringContagion				= mod:NewTargetAnnounce(383087, 3)
 local warnWitheringBurst					= mod:NewTargetAnnounce(367503, 3)
+local warnInfuseCorruption					= mod:NewTargetNofilterAnnounce(372711, 3)--Used as target warning but is off interrupt too
 local warnStealth							= mod:NewSpellAnnounce(384930, 3)
+--local warnSummontotem						= mod:NewSpellAnnounce(374057, 4)--Despite tooltip showing cast time, only event in log is SPELL_SUMMON
 
 local specWarnViolentWhirlwind				= mod:NewSpecialWarningRun(388046, "Melee", nil, nil, 4, 2)
 local specWarnScentedMeat					= mod:NewSpecialWarningRun(384974, nil, nil, nil, 4, 2)
@@ -35,24 +47,35 @@ local specWarnRagestorm						= mod:NewSpecialWarningRun(382555, "Melee", nil, ni
 local specWarnRagestormDispel				= mod:NewSpecialWarningDispel(382555, "RemoveEnrage", nil, nil, 1, 2)
 local specWarnBloodyRage					= mod:NewSpecialWarningDispel(385827, "RemoveEnrage", nil, nil, 1, 2)
 local specWarnWitheringPoison				= mod:NewSpecialWarningDispel(385058, "RemovePoison", nil, nil, 1, 2)
+local specWarnWithering						= mod:NewSpecialWarningDispel(368081, false, nil, nil, 1, 2)
 local specWarnWitheringContagion			= mod:NewSpecialWarningMoveAway(383087, nil, nil, nil, 1, 2)
 local specWarnStinkBreath					= mod:NewSpecialWarningDodge(388060, nil, nil, nil, 2, 2)
 local specWarnToxicTrap						= mod:NewSpecialWarningDodge(368287, nil, nil, nil, 2, 2)
 local specWarnRottingSurge					= mod:NewSpecialWarningDodge(383385, nil, nil, nil, 2, 2)
+local specWarnStomp							= mod:NewSpecialWarningDodge(373943, nil, nil, nil, 2, 2)
+local specWarnBloodthirstyCharge			= mod:NewSpecialWarningDodge(385832, nil, nil, nil, 2, 2)
+local specWarnSummontotem					= mod:NewSpecialWarningSwitch(374057, "Dps", nil, nil, 1, 2)
 local specWarnRotchantingTotem				= mod:NewSpecialWarningSwitch(382435, "Dps", nil, nil, 1, 2)
 local yellWitheringContagion				= mod:NewYell(383087)
 local specWarnWitheringBurst				= mod:NewSpecialWarningYou(367503, nil, nil, nil, 1, 2)
 local yellWitheringBurst					= mod:NewYell(367503)
+local specWarnBurstofDecay					= mod:NewSpecialWarningInterrupt(374544, "HasInterrupt", nil, nil, 1, 2)
 local specWarnHidiousCackle					= mod:NewSpecialWarningInterrupt(367500, "HasInterrupt", nil, nil, 1, 2)
 local specWarnDecaySurge					= mod:NewSpecialWarningInterrupt(382474, "HasInterrupt", nil, nil, 1, 2)
 local specWarnScreech						= mod:NewSpecialWarningInterrupt(385029, "HasInterrupt", nil, nil, 1, 2)
+local specWarnNecroticBreath				= mod:NewSpecialWarningInterrupt(382712, "HasInterrupt", nil, nil, 1, 2)--26.7-40?
 local specWarnGTFO							= mod:NewSpecialWarningGTFO(383399, nil, nil, nil, 1, 8)
 
---local timerDecayClawsCD						= mod:NewCDTimer(9.7, 382787, nil, "Tank", nil, 5, nil, DBM_COMMON_L.TANK_ICON)
+local timerDecayClawsCD						= mod:NewCDTimer(10.2, 382787, nil, "Tank|Healer", nil, 5, nil, DBM_COMMON_L.TANK_ICON)
+local timerWitheringBurstCD					= mod:NewCDTimer(23, 367503, nil, nil, nil, 3)
+local timerSummonLashersCD					= mod:NewCDTimer(12.2, 383062, nil, nil, nil, 1)
+local timerStinkBreathCD					= mod:NewCDTimer(17, 388060, nil, nil, nil, 3)
+local timerViolentWhirlwindCD				= mod:NewCDTimer(17, 388046, nil, nil, nil, 2)
+local timerStompCD							= mod:NewCDTimer(20.6, 373943, nil, nil, nil, 2)
 
 --local playerName = UnitName("player")
 
---Antispam IDs for this mod: 1 run away, 2 dodge, 3 dispel, 4 incoming damage, 5 you/role, 6 misc, 7 gtfo
+--Antispam IDs for this mod: 1 run away, 2 dodge, 3 dispel, 4 incoming damage, 5 you/role, 6 misc, 7 off interrupt, 8 gtfo
 
 function mod:BurstTarget(targetname)
 	if not targetname then return end
@@ -77,41 +100,75 @@ function mod:SPELL_CAST_START(args)
 		if self.Options.SpecWarn367500interrupt and self:CheckInterruptFilter(args.sourceGUID, false, true) then
 			specWarnHidiousCackle:Show(args.sourceName)
 			specWarnHidiousCackle:Play("kickcast")
-		elseif self:AntiSpam(3, 5) then
+		elseif self:AntiSpam(3, 7) then
 			warnHidiousCackle:Show()
 		end
 	elseif spellId == 382474 then
-		if self.Options.SpecWarn382474interrupt and self:CheckInterruptFilter(args.sourceGUID, false, true) then
+		--if self.Options.SpecWarn382474interrupt and self:CheckInterruptFilter(args.sourceGUID, false, true) then
 			specWarnDecaySurge:Show(args.sourceName)
 			specWarnDecaySurge:Play("kickcast")
-		--elseif self:AntiSpam(3, 5) then
+		--elseif self:AntiSpam(3, 7) then
 		--	warnHidiousCackle:Show()
+		--end
+	elseif spellId == 382712 then
+		--if self.Options.SpecWarn382712interrupt and self:CheckInterruptFilter(args.sourceGUID, false, true) then
+			specWarnNecroticBreath:Show(args.sourceName)
+			specWarnNecroticBreath:Play("kickcast")
+		--elseif self:AntiSpam(3, 7) then
+		--	warnHidiousCackle:Show()
+		--end
+	elseif spellId == 388060 then
+		if self:AntiSpam(3, 2) then
+			specWarnStinkBreath:Show()
+			specWarnStinkBreath:Play("shockwave")
 		end
-	elseif spellId == 388060 and self:AntiSpam(3, 2) then
-		specWarnStinkBreath:Show()
-		specWarnStinkBreath:Play("shockwave")
-	elseif spellId == 388046 and self:AntiSpam(3, 1) then
-		specWarnViolentWhirlwind:Show()
-		specWarnViolentWhirlwind:Play("justrun")
+		timerStinkBreathCD:Start(nil, args.sourceGUID)
+	elseif spellId == 373943 then
+		if self:AntiSpam(3, 2) then
+			specWarnStomp:Show()
+			specWarnStomp:Play("watchstep")
+		end
+		timerStompCD:Start(nil, args.sourceGUID)
+	elseif spellId == 388046 then
+		if self:AntiSpam(3, 1) then
+			specWarnViolentWhirlwind:Show()
+			specWarnViolentWhirlwind:Play("justrun")
+		end
+		timerViolentWhirlwindCD:Start(nil, args.sourceGUID)
+	elseif spellId == 385832 and self:AntiSpam(3, 2) then
+		specWarnBloodthirstyCharge:Show()
+		specWarnBloodthirstyCharge:Play("chargemove")
 	elseif spellId == 382787 then
---		timerDecayClawsCD:Start()
-		if self:AntiSpam(3, 5) then
+		timerDecayClawsCD:Start(nil, args.sourceGUID)
+		if self:AntiSpam(3, 7) then
 			warnDecayClaws:Show()
 		end
-	elseif spellId == 374544 and self:AntiSpam(3, 5) then
-		warnBurstofDecay:Show()
-	elseif spellId == 385029 and self:AntiSpam(3, 5) then
+	elseif spellId == 374544 then
+		if self.Options.SpecWarn374544interrupt and self:CheckInterruptFilter(args.sourceGUID, false, true) then
+			specWarnBurstofDecay:Show(args.sourceName)
+			specWarnBurstofDecay:Play("kickcast")
+		elseif self:AntiSpam(3, 7) then
+			warnBurstofDecay:Show()
+		end
+	elseif spellId == 385029 then
 		if self.Options.SpecWarn385029interrupt and self:CheckInterruptFilter(args.sourceGUID, false, true) then
 			specWarnScreech:Show(args.sourceName)
 			specWarnScreech:Play("kickcast")
-		elseif self:AntiSpam(3, 5) then
+		elseif self:AntiSpam(3, 7) then
 			warnScreech:Show()
 		end
-	elseif spellId == 383062 and self:AntiSpam(3, 5) then
-		warnSummonLashers:Show()
+	elseif spellId == 383062 then
+		if self:AntiSpam(3, 5) then
+			warnSummonLashers:Show()
+		end
+		timerSummonLashersCD:Start(nil, args.sourceGUID)
 	elseif spellId == 367503 then
 		self:ScheduleMethod(0.1, "BossTargetScanner", args.sourceGUID, "BurstTarget", 0.1, 8)
-
+		timerWitheringBurstCD:Start(nil, args.sourceGUID)
+	elseif spellId == 373897 and self:AntiSpam(3, 5) then
+		warnDecayingRoots:Show()
+	elseif spellId == 374569 and self:AntiSpam(3, 6) then
+		warnBurst:Show()
 	end
 end
 
@@ -129,17 +186,27 @@ function mod:SPELL_CAST_SUCCESS(args)
 		specWarnRotchantingTotem:Play("attacktotem")
 	elseif spellId == 384930 and self:AntiSpam(3, 5) then
 		warnStealth:Show()
+	elseif spellId == 372711 and self:AntiSpam(3, 7) then
+		warnInfuseCorruption:Show(args.destName)
+	elseif spellId == 382555 and self:AntiSpam(3, 3) then--Buff not in combat log, so success is used to assume buff went up
+		specWarnRagestormDispel:Show(args.sourceName)
+		specWarnRagestormDispel:Play("enrage")
 	end
 end
 
+function mod:SPELL_SUMMON(args)
+	if not self.Options.Enabled then return end
+	local spellId = args.spellId
+	if spellId == 374057 and self:AntiSpam(3, 5) then
+		specWarnSummontotem:Show()
+		specWarnSummontotem:Play("attacktotem")
+	end
+end
 
 function mod:SPELL_AURA_APPLIED(args)
 	if not self.Options.Enabled then return end
 	local spellId = args.spellId
-	if spellId == 382555 and self:AntiSpam(3, 3) then
-		specWarnRagestormDispel:Show(args.destName)
-		specWarnRagestormDispel:Play("enrage")
-	elseif spellId == 385827 and self:AntiSpam(3, 3) then
+	if spellId == 385827 and self:AntiSpam(3, 3) then
 		specWarnBloodyRage:Show(args.destName)
 		specWarnBloodyRage:Play("enrage")
 	elseif spellId == 383087 then
@@ -149,12 +216,15 @@ function mod:SPELL_AURA_APPLIED(args)
 			specWarnWitheringContagion:Play("range5")
 			yellWitheringContagion:Yell()
 		end
-	elseif spellId == 383399 and args:IsPlayer() and self:AntiSpam(3, 7) then
+	elseif spellId == 383399 and args:IsPlayer() and self:AntiSpam(3, 8) then
 		specWarnGTFO:Show(args.spellName)
 		specWarnGTFO:Play("watchfeet")
 	elseif spellId == 385058 and args:IsDestTypePlayer() and self:CheckDispelFilter("poison") and self:AntiSpam(3, 3) then
 		specWarnWitheringPoison:Show(args.destName)
 		specWarnWitheringPoison:Play("helpdispel")
+	elseif spellId == 368081 and args:IsDestTypePlayer() and self:CheckDispelFilter("disease") and self:AntiSpam(3, 3) then
+		specWarnWithering:Show(args.destName)
+		specWarnWithering:Play("helpdispel")
 	elseif spellId == 384974 and args:IsPlayer() and self:AntiSpam(4, 1) then
 		specWarnScentedMeat:Show()
 		specWarnScentedMeat:Play("justrun")
@@ -175,10 +245,25 @@ end
 --]]
 
 function mod:SPELL_PERIODIC_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId, spellName)
-	if spellId == 383399 and destGUID == UnitGUID("player") and self:AntiSpam(2, 7) then
+	if spellId == 383399 and destGUID == UnitGUID("player") and self:AntiSpam(2, 8) then
 		specWarnGTFO:Show(spellName)
 		specWarnGTFO:Play("watchfeet")
 	end
 end
 mod.SPELL_PERIODIC_MISSED = mod.SPELL_PERIODIC_DAMAGE
 
+function mod:UNIT_DIED(args)
+	local cid = self:GetCIDFromGUID(args.destGUID)
+	if cid == 186191 then--Decay Speaker
+		timerWitheringBurstCD:Stop(args.destGUID)
+	elseif cid == 189531 then--Decay Elder
+		timerSummonLashersCD:Stop(args.destGUID)
+	elseif cid == 187033 then--Stink Breath
+		timerStinkBreathCD:Stop(args.destGUID)
+		timerViolentWhirlwindCD:Stop(args.destGUID)
+	elseif cid == 187315 then--Disease Slasher
+		timerDecayClawsCD:Stop(args.destGUID)
+	elseif cid == 186229 then--Wilted Oak
+		timerStompCD:Stop(args.destGUID)
+	end
+end
