7613bd40a733874a667e756eca4c0982d3533825
MysticalOS <mysticalosx@gmail.com>
2023-04-29 21:19:43 -0400

    Lightning lash can no longer be interrupted, so now warnings will say to get grounding field Also added alerts for overload grounding field as well, and itll be emphasized if you're actually in it. Added timers for both of the above

--- DBM-Party-Cataclysm/TheVortexPinnacle/Asaad.lua
+++ DBM-Party-Cataclysm/TheVortexPinnacle/Asaad.lua
@@ -77,8 +77,8 @@ function mod:SPELL_CAST_START(args)
 	if args.spellId == 87618 then
 		--1.25 post nerf in classic, 1 sec pre nerf
 		--3.5 lol giga nerf in M+
-		specWarnStaticCling:Schedule(2.5)--delay message since jumping at start of cast is no longer correct in 4.0.6+
-		specWarnStaticCling:ScheduleVoice(2.5, "jumpnow")
+		specWarnStaticCling:Schedule(2.4)--delay message since jumping at start of cast is no longer correct in 4.0.6+
+		specWarnStaticCling:ScheduleVoice(2.4, "jumpnow")
 		if not self:IsMythicPlus() and timerGroundingFieldCD:GetRemaining() < 15.8 then
 			timerStaticClingCD:Start()
 		end
