b81c3d0d83253c76c435eef6b8cbf56680bb689d
MysticalOS <mysticalosx@gmail.com>
2023-09-07 14:44:28 -0400

    Also stub in throne of tides trash module for localizers to get started

--- DBM-Party-Legion/DarkheartThicket/DHTTrash.lua
+++ DBM-Party-Legion/DarkheartThicket/DHTTrash.lua
@@ -12,10 +12,13 @@ mod:RegisterEvents(
 
 local specWarnPrimalRampage			= mod:NewSpecialWarningDodge(198379, "Melee", nil, nil, 1, 2)
 
+--Antispam IDs for this mod: 1 run away, 2 dodge, 3 dispel, 4 incoming damage, 5 you/role, 6 misc, 7 off interrupt
+
 function mod:SPELL_CAST_START(args)
 	if not self.Options.Enabled then return end
+	if not self:IsValidWarning(args.sourceGUID) then return end
 	local spellId = args.spellId
-	if spellId == 198379 then
+	if spellId == 198379 and self:AntiSpam(3, 2) then
 		specWarnPrimalRampage:Show()
 		specWarnPrimalRampage:Play("chargemove")
 	end
