8faa67d14c7aab63ce9a6cf1386a4c5ab2f6bb4e
MysticalOS <mysticalosx@gmail.com>
2023-10-02 16:47:55 -0400

    fixes and cleanups, closes https://github.com/DeadlyBossMods/DBM-Dungeons/issues/141

--- DBM-Party-Cataclysm/BlackrockCaverns/Corla.lua
+++ DBM-Party-Cataclysm/BlackrockCaverns/Corla.lua
@@ -13,8 +13,7 @@ mod:RegisterEventsInCombat(
 	"SPELL_AURA_APPLIED 75823 75697",
 	"SPELL_AURA_APPLIED_DOSE 75697",
 	"SPELL_AURA_REMOVED 75608",
-	"SPELL_CAST_START 75823 82362",
-	"SPELL_INTERRUPT"
+	"SPELL_CAST_START 75823 82362"
 )
 
 local warnDarkCommand		= mod:NewTargetNoFilterAnnounce(75823, 4)
@@ -24,10 +23,8 @@ local specWarnShadowStrike	= mod:NewSpecialWarningInterrupt(82362, nil, nil, nil
 local specWarnDarkCommand	= mod:NewSpecialWarningInterrupt(75823, nil, nil, nil, 1, 2)
 local specWarnEvolution		= mod:NewSpecialWarningStack(75697, true, 80, nil, nil, 1, 6)
 
-local timerDarkCommandCast	= mod:NewCastTimer(4, 75823, nil, nil, nil, 4, nil, DBM_COMMON_L.INTERRUPT_ICON)
 local timerDarkCommand		= mod:NewTargetTimer(4, 75823, nil, nil, nil, 5, nil, DBM_COMMON_L.HEALER_ICON..DBM_COMMON_L.MAGIC_ICON)
-local timerDarkCommandCD	= mod:NewCDTimer(25, 75823, nil, nil, nil, 4, nil, DBM_COMMON_L.INTERRUPT_ICON)
-local timerShadowStrike		= mod:NewCastTimer(2, 82362, nil, nil, nil, 4, nil, DBM_COMMON_L.INTERRUPT_ICON)
+local timerDarkCommandCD	= mod:NewCDTimer(23, 75823, nil, nil, nil, 4, nil, DBM_COMMON_L.INTERRUPT_ICON)
 local timerEvolution		= mod:NewBuffFadesTimer(15, 75697, nil, nil, nil, 5)
 
 function mod:OnCombatStart(delay)
@@ -60,25 +57,11 @@ function mod:SPELL_CAST_START(args)
 			specWarnDarkCommand:Show(args.sourceName)
 			specWarnDarkCommand:Play("kickcast")
 		end
-		timerDarkCommandCast:Start()
 		timerDarkCommandCD:Start()
 	elseif args.spellId == 82362 then
 		if self:CheckInterruptFilter(args.sourceGUID, false, true) then
 			specWarnShadowStrike:Show(args.sourceName)
 			specWarnShadowStrike:Play("kickcast")
 		end
-		if self:IsDifficulty("heroic5") then
-			timerShadowStrike:Start()
-		else
-			timerShadowStrike:Start(3)
-		end
-	end
-end
-
-function mod:SPELL_INTERRUPT(args)
-	if type(args.extraSpellId) == "number" and args.extraSpellId == 82362 then
-		timerShadowStrike:Cancel()
-	elseif type(args.extraSpellId) == "number" and args.extraSpellId == 75823 then
-		timerDarkCommandCast:Cancel()
 	end
 end
