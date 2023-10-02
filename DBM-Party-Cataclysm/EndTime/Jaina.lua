74b8ce374b3045fd80b19929ca9113e5d96e3643
MysticalOS <mysticalosx@gmail.com>
2023-10-02 16:41:37 -0400

    Disable flarecore cd timer, it's not accurate (maybe health based?) Closes https://github.com/DeadlyBossMods/DBM-Dungeons/issues/140

--- DBM-Party-Cataclysm/EndTime/Jaina.lua
+++ DBM-Party-Cataclysm/EndTime/Jaina.lua
@@ -19,20 +19,20 @@ local warnFrostBlades			= mod:NewSpellAnnounce(101339, 3)
 
 local specWarnFlarecore			= mod:NewSpecialWarningSpell(101927, nil, nil, nil, 2, 2)
 
-local timerFlarecore			= mod:NewCDTimer(20, 101927, nil, nil, nil, 5)
+--local timerFlarecore			= mod:NewCDTimer(13.4, 101927, nil, nil, nil, 5)
 local timerFlarecoreDetonate	= mod:NewTimer(10, "TimerFlarecoreDetonate", 101927, nil, nil, 2)
-local timerFrostBlades			= mod:NewNextTimer(25, 101339)
+local timerFrostBlades			= mod:NewCDTimer(24.3, 101339)
 
 function mod:OnCombatStart(delay)
-	timerFlarecore:Start(16)
-	timerFrostBlades:Start(21)
+--	timerFlarecore:Start(10.5)
+	timerFrostBlades:Start(19)
 end
 
 function mod:SPELL_CAST_SUCCESS(args)
 	if args.spellId == 101927 then
 		specWarnFlarecore:Show()
 		specWarnFlarecore:Play("specialsoon")
-		timerFlarecore:Start()
+--		timerFlarecore:Start()
 		timerFlarecoreDetonate:Start()
 	elseif args.spellId == 101812 then	-- Frost Blades is cast immediately after Blink (Frost Blades = 3 events, Blink = 1 event)
 		warnFrostBlades:Show()
