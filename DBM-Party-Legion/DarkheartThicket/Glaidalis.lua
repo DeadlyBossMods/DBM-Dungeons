e3c5ee4223df6d3978b3faec1bfcf07c8fde38cd
MysticalOS <mysticalosx@gmail.com>
2023-10-30 03:08:45 -0400

    DHT boss review taking longer tan I'd like, 3/4 done, will do 4th tomorrow. two of these needed extra work due to severe variations and spell queuing problems. think they're mostly sorted by auto updating timer code though but may need adjustments as more data comes in.

--- DBM-Party-Legion/DarkheartThicket/Glaidalis.lua
+++ DBM-Party-Legion/DarkheartThicket/Glaidalis.lua
@@ -4,80 +4,173 @@ local L		= mod:GetLocalizedStrings()
 mod:SetRevision("@file-date-integer@")
 mod:SetCreatureID(96512)
 mod:SetEncounterID(1836)
+mod:SetUsedIcons(8, 7)
+mod:SetHotfixNoticeRev(20231029000000)
+mod:SetMinSyncRevision(20231029000000)
+--mod.respawnTime = 29
 mod.sendMainBossGUID = true
 
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
 	"SPELL_CAST_START 198379",
-	"SPELL_CAST_SUCCESS 198401 212464",
+	"SPELL_CAST_SUCCESS 198401 212464 196354",
+	"SPELL_SUMMON 198432",
+	"SPELL_AURA_APPLIED 198477",
+	"SPELL_AURA_REMOVED 198477",
 	"SPELL_PERIODIC_DAMAGE 198408",
-	"SPELL_PERIODIC_MISSED 198408",
-	"UNIT_SPELLCAST_SUCCEEDED boss1"
+	"SPELL_PERIODIC_MISSED 198408"
 )
 
-local warnLeap					= mod:NewTargetNoFilterAnnounce(196346, 2)--0.5 seconds may still be too hard to dodge even if target scanning works.
+--[[
+ability.id = 198379 and type = "begincast"
+ or (ability.id = 198401 or ability.id = 212464 or ability.id = 196354) and type = "cast"
+ or ability.id = 198432
+ or type = "dungeonencounterstart" or type = "dungeonencounterend"
+--]]
+--NOTE: Leap will be broken until 10.2 but that's fine. in TW or while leveling dungeon is easy
+--TODO, min timers could still possibly need tweaking/lowering. Same with min ICD of each ability
+local warnLeap					= mod:NewCountAnnounce(196346, 2)
 local warnNightFall				= mod:NewSpellAnnounce(198401, 2)
 
 local specWarnNightfall			= mod:NewSpecialWarningMove(198408, nil, nil, nil, 1, 2)
 --local specWarnLeap			= mod:NewSpecialWarningDodge(196346, nil, nil, nil, 1)
 local yellLeap					= mod:NewYell(196346)
 local specWarnRampage			= mod:NewSpecialWarningDefensive(198379, "Tank", nil, nil, 1, 2)
+local specWarnFixate			= mod:NewSpecialWarningYou(198477, nil, nil, nil, 1, 2)
 
-local timerLeapCD				= mod:NewCDTimer(12.9, 196346, nil, nil, nil, 3)
-local timerRampageCD			= mod:NewCDTimer(15.8, 198379, nil, "Tank", nil, 5, nil, DBM_COMMON_L.TANK_ICON)
-local timerNightfallCD			= mod:NewCDTimer(14.5, 198408, nil, nil, nil, 3)
+local timerLeapCD				= mod:NewCDCountTimer(13.2, 196346, nil, nil, nil, 3)--13.2-17 depending on travel time and spell queuing (timer could be even shorter, small sample)
+local timerRampageCD			= mod:NewCDCountTimer(26.7, 198379, nil, "Tank", nil, 5, nil, DBM_COMMON_L.TANK_ICON)--26.7-32.7
+local timerNightfallCD			= mod:NewCDCountTimer(20.6, 198408, nil, nil, nil, 3)--20.6--30.4
 
-function mod:LeapTarget(targetname, uId)
-	if not targetname then
-		warnLeap:Show(DBM_COMMON_L.UNKNOWN)
-		return
+mod:AddSetIconOption("SetIconOnAdd", -13302, true, 5, {8, 7})
+mod:AddNamePlateOption("NPAuraOnFixate", 198477)
+
+mod:GroupSpells(198401, -13302)--Group add with it's parent spell
+
+mod.vb.addIcon = 8
+mod.vb.leapCount = 0
+mod.vb.rampageCount = 0
+mod.vb.nightCount = 0
+
+--Grievous Leap triggers 5.8 ICD (maybe also 5.7 so will use that there too)
+--Primal rampage triggers 5.7 ICD
+--Nightfall triggers 2.6 ICD
+local function updateAllTimers(self, ICD)
+	DBM:Debug("updateAllTimers running", 3)
+	if timerLeapCD:GetRemaining(self.vb.leapCount+1) < ICD then
+		local elapsed, total = timerLeapCD:GetTime(self.vb.leapCount+1)
+		local extend = ICD - (total-elapsed)
+		DBM:Debug("timerLeapCD extended by: "..extend, 2)
+		timerLeapCD:Update(elapsed, total+extend, self.vb.leapCount+1)
 	end
-	warnLeap:Show(targetname)
+	if timerRampageCD:GetRemaining(self.vb.rampageCount+1) < ICD then
+		local elapsed, total = timerRampageCD:GetTime(self.vb.rampageCount+1)
+		local extend = ICD - (total-elapsed)
+		DBM:Debug("timerRampageCD extended by: "..extend, 2)
+		timerRampageCD:Update(elapsed, total+extend, self.vb.rampageCount+1)
+	end
+	if timerNightfallCD:GetRemaining(self.vb.nightCount+1) < ICD then
+		local elapsed, total = timerNightfallCD:GetTime(self.vb.nightCount+1)
+		local extend = ICD - (total-elapsed)
+		DBM:Debug("timerNightfallCD extended by: "..extend, 2)
+		timerNightfallCD:Update(elapsed, total+extend, self.vb.nightCount+1)
+	end
+end
+
+--Not realy dodgable, more or less just a visual of leap target
+function mod:LeapTarget(targetname, uId)
 	if targetname == UnitName("player") then
 		yellLeap:Yell()
 	end
 end
 
 function mod:OnCombatStart(delay)
-	timerLeapCD:Start(5-delay)
-	timerRampageCD:Start(12.2-delay)
-	timerNightfallCD:Start(19-delay)
+	self.vb.leapCount = 0
+	self.vb.rampageCount = 0
+	self.vb.nightCount = 0
+	timerLeapCD:Start(5-delay, 1)
+	timerRampageCD:Start(12.2-delay, 1)
+	timerNightfallCD:Start(24.2-delay, 1)--24.2-25.5
+	if self.Options.NPAuraOnFixate then
+		DBM:FireEvent("BossMod_EnableHostileNameplates")
+	end
 end
 
-function mod:SPELL_CAST_SUCCESS(args)
-	local spellId = args.spellId
-	if (spellId == 198401 or spellId == 212464) and self:AntiSpam(2, 1) then
-		warnNightFall:Show()
-		if spellId == 212464 then
-			timerNightfallCD:Start(28)--Needs more support data. could be health based too
-		else
-			timerNightfallCD:Start()
-		end
+function mod:OnCombatEnd()
+	if self.Options.NPAuraOnFixate then
+		DBM.Nameplate:Hide(true, nil, nil, nil, true, true)
 	end
 end
 
 function mod:SPELL_CAST_START(args)
 	local spellId = args.spellId
 	if spellId == 198379 then
-		specWarnRampage:Show()
+		self.vb.rampageCount = self.vb.rampageCount + 1
+		specWarnRampage:Show(self.vb.rampageCount)
 		specWarnRampage:Play("defensive")
-		timerRampageCD:Start()
+		timerRampageCD:Start(nil, self.vb.rampageCount+1)
+		updateAllTimers(self, 5.7)
+	end
+end
+
+function mod:SPELL_CAST_SUCCESS(args)
+	local spellId = args.spellId
+	if (spellId == 198401 or spellId == 212464) and self:AntiSpam(2, 1) then
+		self.vb.nightCount = self.vb.nightCount + 1
+		self.vb.addIcon = 8
+		warnNightFall:Show(self.vb.nightCount)
+		timerNightfallCD:Start(nil, self.vb.nightCount+1)
+		updateAllTimers(self, 2.6)
+	elseif spellId == 196354 then
+		self.vb.leapCount = self.vb.leapCount + 1
+		warnLeap:Show(self.vb.leapCount)
+		--"<398.10 22:27:23> [UNIT_SPELLCAST_SUCCEEDED] Archdruid Glaidalis(76.9%-100.0%){Target:Lucyz} -Grievous Leap- [[boss1:Cast-3-5770-1466-11160-196354-0007A1BF2D:196354]]", -- [4835]
+		--"<398.12 22:27:23> [UNIT_TARGET] boss1#Archdruid Glaidalis#Target: Fxa#TargetOfTarget: Archdruid Glaidalis", -- [4842]
+		--"<398.11 22:27:23> [CLEU] SPELL_DAMAGE#Creature-0-5770-1466-11160-96512-000021BD9C#Archdruid Glaidalis#Player-5765-0007A043#Lucyz-Raszageth#196354#Grievous Leap", -- [4843]
+		if DBM.Options.DebugMode then
+			self:BossTargetScanner(args.sourceGUID, "LeapTarget", 0.05, 6, true, nil, nil, nil, true)
+		end
+		timerLeapCD:Start(nil, self.vb.leapCount+1)
+		updateAllTimers(self, 5.7)
+	end
+end
+
+function mod:SPELL_SUMMON(args)
+	local spellId = args.spellId
+	if spellId == 198432 then
+		if self.Options.SetIconOnAdd then
+			self:ScanForMobs(args.destGUID, 2, self.vb.addIcon, 1, nil, 12, "SetIconOnAdd")
+		end
+		self.vb.addIcon = self.vb.addIcon - 1
+	end
+end
+
+function mod:SPELL_AURA_APPLIED(args)
+	if spellId == 198477 and args:IsPlayer() then
+		if self:AntiSpam(3, 2) then
+			specWarnFixate:Show()
+			specWarnFixate:Play("targetyou")
+		end
+		if self.Options.NPAuraOnFixate then
+			DBM.Nameplate:Show(true, args.sourceGUID, spellId, nil, 20)
+		end
+	end
+end
+
+function mod:SPELL_AURA_REMOVED(args)
+	local spellId = args.spellId
+	if spellId == 198477 and args:IsPlayer() then
+		if self.Options.NPAuraOnFixate then
+			DBM.Nameplate:Hide(true, args.sourceGUID, spellId)
+		end
 	end
 end
 
 function mod:SPELL_PERIODIC_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId)
-	if spellId == 198408 and destGUID == UnitGUID("player") and self:AntiSpam(2, 2) then
+	if spellId == 198408 and destGUID == UnitGUID("player") and self:AntiSpam(2, 3) then
 		specWarnNightfall:Show()
 		specWarnNightfall:Play("runaway")
 	end
 end
 mod.SPELL_PERIODIC_MISSED = mod.SPELL_PERIODIC_DAMAGE
-
-function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, spellId)
-	--"<13.84 02:50:50> [UNIT_SPELLCAST_SUCCEEDED] Arch-Druid Glaidalis(Omegal) [[boss1:Grievous Leap::3-2084-1466-6383-196346-000018A4DA:196346]]", -- [47]
-	if spellId == 196346 then
-		self:BossTargetScanner(96512, "LeapTarget", 0.05, 12, true, nil, nil, nil, true)
-		timerLeapCD:Start()
-	end
-end
--- DBM-Party-Legion/DarkheartThicket/Oakheart.lua
+++ DBM-Party-Legion/DarkheartThicket/Oakheart.lua
@@ -4,27 +4,84 @@ local L		= mod:GetLocalizedStrings()
 mod:SetRevision("@file-date-integer@")
 mod:SetCreatureID(103344)
 mod:SetEncounterID(1837)
+mod:SetHotfixNoticeRev(20231029000000)
+mod:SetMinSyncRevision(20231029000000)
+--mod.respawnTime = 29
 mod.sendMainBossGUID = true
 
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
-	"SPELL_CAST_START 204666 204646 204574 204667"
+	"SPELL_CAST_START 204666 204646 204574 204667 204611 212786"
 )
 
+--[[
+(ability.id = 204666 or ability.id = 204574 or ability.id = 204667 or ability.id = 204611 or ability.id = 212786) and type = "begincast"
+ or type = "dungeonencounterstart" or type = "dungeonencounterend"
+ or ability.id = 204646 and type = "begincast"
+--]]
+--NOTE: this boss has some serious spell queuing issues that causes wild variation in timers and ability orders
+--As such, it will use aggressive on the fly timer correction that may feel jarring to users that don't recognize it's happening til they realize it is and why
 local warnShatteredEarth			= mod:NewSpellAnnounce(204666, 2)
 local warnThrowTarget				= mod:NewTargetNoFilterAnnounce(204658, 2)--This is target the tank is THROWN at.
+local warnUproot					= mod:NewSpellAnnounce(212786, 2)
 
 local specWarnRoots					= mod:NewSpecialWarningDodge(204574, nil, nil, nil, 2, 2)
 local yellThrow						= mod:NewYell(204658, 2764)--yell so others can avoid splash damage. I don't think target can avoid
-local specWarnBreath				= mod:NewSpecialWarningDefensive(204667, "Tank", nil, nil, 1, 2)
+local specWarnBreath				= mod:NewSpecialWarningDefensive(204667, "Tank", nil, nil, 1, 2)--Can tank side step? based on logs I seen some tanks completely avoiding damage so maybe change to dodge
 
-local timerShatteredEarthCD			= mod:NewCDTimer(35, 204666, nil, nil, nil, 2)--35-62 variation? is this health based?
-local timerThrowCD					= mod:NewCDTimer(28, 204658, nil, nil, nil, 3, nil, DBM_COMMON_L.TANK_ICON, nil, mod:IsTank() and 2, 4)--29-32
-local timerRootsCD					= mod:NewCDTimer(23, 204574, nil, nil, nil, 3)--23-31
-local timerBreathCD					= mod:NewCDTimer(26.5, 204667, nil, "Tank", nil, 5, nil, DBM_COMMON_L.TANK_ICON)--26--35
+local timerShatteredEarthCD			= mod:NewCDCountTimer(35, 204666, nil, nil, nil, 2)--35-60 (basically same as OG)
+local timerCrushingGripCD			= mod:NewCDCountTimer(27.9, 204611, nil, nil, nil, 5, nil, DBM_COMMON_L.TANK_ICON, nil, mod:IsTank() and 2, 4)--27.9-36 (basically same as OG)
+local timerRootsCD					= mod:NewCDCountTimer(23, 204574, nil, nil, nil, 3)--23-35.1 (basically same as OG)
+local timerBreathCD					= mod:NewCDCountTimer(26.5, 204667, nil, nil, nil, 5)--26-35 (basically same as OG)
+local timerUprootCD					= mod:NewCDCountTimer(32.7, 212786, nil, nil, nil, 1, nil, DBM_COMMON_L.DAMAGE_ICON)--32.7-37.6 (Probably also OG timer, but didn't have it in OG mod)
 
---AKA Crushing Grip
+mod.vb.shatteredCount = 0
+mod.vb.crushingCount = 0
+mod.vb.rootsCount = 0
+mod.vb.breathCount = 0
+mod.vb.uprootCount = 0
+
+--Nightmare Breath triggers 9.7 ICD
+--Crushing Grip triggers 6 ICD (well technically it triggers 1, and second Id triggers 5 more)
+--Uproot triggers 3.6 ICD
+--Strangling Roots triggers 2.4 ICD
+--Shattering Earth triggers 7.2 ICD
+local function updateAllTimers(self, ICD)
+	DBM:Debug("updateAllTimers running", 3)
+	if timerShatteredEarthCD:GetRemaining(self.vb.shatteredCount+1) < ICD then
+		local elapsed, total = timerShatteredEarthCD:GetTime(self.vb.shatteredCount+1)
+		local extend = ICD - (total-elapsed)
+		DBM:Debug("timerShatteredEarthCD extended by: "..extend, 2)
+		timerShatteredEarthCD:Update(elapsed, total+extend, self.vb.shatteredCount+1)
+	end
+	if timerCrushingGripCD:GetRemaining(self.vb.crushingCount+1) < ICD then
+		local elapsed, total = timerCrushingGripCD:GetTime(self.vb.crushingCount+1)
+		local extend = ICD - (total-elapsed)
+		DBM:Debug("timerCrushingGripCD extended by: "..extend, 2)
+		timerCrushingGripCD:Update(elapsed, total+extend, self.vb.crushingCount+1)
+	end
+	if timerRootsCD:GetRemaining(self.vb.rootsCount+1) < ICD then
+		local elapsed, total = timerRootsCD:GetTime(self.vb.rootsCount+1)
+		local extend = ICD - (total-elapsed)
+		DBM:Debug("timerRootsCD extended by: "..extend, 2)
+		timerRootsCD:Update(elapsed, total+extend, self.vb.rootsCount+1)
+	end
+	if timerBreathCD:GetRemaining(self.vb.breathCount+1) < ICD then
+		local elapsed, total = timerBreathCD:GetTime(self.vb.breathCount+1)
+		local extend = ICD - (total-elapsed)
+		DBM:Debug("timerBreathCD extended by: "..extend, 2)
+		timerBreathCD:Update(elapsed, total+extend, self.vb.breathCount+1)
+	end
+	if timerUprootCD:GetRemaining(self.vb.uprootCount+1) < ICD then
+		local elapsed, total = timerUprootCD:GetTime(self.vb.uprootCount+1)
+		local extend = ICD - (total-elapsed)
+		DBM:Debug("timerUprootCD extended by: "..extend, 2)
+		timerUprootCD:Update(elapsed, total+extend, self.vb.uprootCount+1)
+	end
+end
+
+--AKA Crushing Grip secondary mechanic
 function mod:ThrowTarget(targetname, uId)
 	if not targetname then
 		return
@@ -37,27 +94,53 @@ function mod:ThrowTarget(targetname, uId)
 end
 
 function mod:OnCombatStart(delay)
-	timerShatteredEarthCD:Start(5.8-delay)
-	timerRootsCD:Start(12-delay)
-	timerBreathCD:Start(18-delay)
-	timerThrowCD:Start(28.5-delay)
+	self.vb.shatteredCount = 0
+	self.vb.crushingCount = 0
+	self.vb.rootsCount = 0
+	self.vb.breathCount = 0
+	self.vb.uprootCount = 0
+	timerShatteredEarthCD:Start(7.3-delay, 1)--7.3, 8.1, 8.5
+	timerRootsCD:Start(12.2-delay, 1)--12.2, 15.8, 12.2, 12.9
+	timerBreathCD:Start(18.2-delay, 1)--21.4, 21.5, 18.2
+	--Uproot and Crushing can swap places pull to pull then stay that way rest of fight
+	timerCrushingGripCD:Start(27.9-delay, 1)--27.9, 34, 34.8 (34 if it's after uproot)
+	timerUprootCD:Start(30.4-delay, 1)--31.1, 30.4, 34 (30-31 if it's first)
 end
 
 function mod:SPELL_CAST_START(args)
 	local spellId = args.spellId
-	if spellId == 204646 then
-		timerThrowCD:Start()
-		self:BossTargetScanner(103344, "ThrowTarget", 0.1, 12, true, nil, nil, nil, true)
+	if spellId == 204646 then--Secondary cast of Crushing Grip
+		self:BossTargetScanner(103344, "ThrowTarget", 0.1, 12, true, nil, nil, nil, true)--Filters tank,
 	elseif spellId == 204666 then
-		warnShatteredEarth:Show()
-		timerShatteredEarthCD:Start()
+		self.vb.shatteredCount = self.vb.shatteredCount + 1
+		warnShatteredEarth:Show(self.vb.shatteredCount)
+		--35.2, 44.9 / 51.8, 36.3 / 52.2, 60.7
+		timerShatteredEarthCD:Start(nil, self.vb.shatteredCount+1)
+		updateAllTimers(self, 7.2)
 	elseif spellId == 204574 then
-		specWarnRoots:Show()
+		self.vb.rootsCount = self.vb.rootsCount + 1
+		specWarnRoots:Show(self.vb.rootsCount)
 		specWarnRoots:Play("watchstep")
-		timerRootsCD:Start()
+		--25.5, 29.1, 35.1 / 28.7, 32.7, 27.9, 23 / 27.9, 32.7, 31.6, 27.9
+		timerRootsCD:Start(nil, self.vb.rootsCount+1)
+		updateAllTimers(self, 2.4)
 	elseif spellId == 204667 then
-		specWarnBreath:Show()
+		self.vb.breathCount = self.vb.breathCount + 1
+		specWarnBreath:Show(self.vb.breathCount)
 		specWarnBreath:Play("defensive")
-		timerBreathCD:Start()
+		--32.7, 27.9 / 29.1, 32.7, 36.4 / 29.1, 32.8, 27.9
+		timerBreathCD:Start(nil, self.vb.breathCount+1)
+		updateAllTimers(self, 9.7)
+	elseif spellId == 204611 then--Primary Crushnig Grip Cast
+		self.vb.crushingCount = self.vb.crushingCount + 1
+		--32.7, 35.1 / 33.5, 36.4 / 32.7, 27.9, 32.7
+		timerCrushingGripCD:Start(nil, self.vb.crushingCount+1)
+		updateAllTimers(self, 6)
+	elseif spellId == 212786 then
+		self.vb.uprootCount = self.vb.uprootCount + 1
+		warnUproot:Show(self.vb.uprootCount)
+		--32.7, 35.1 / 33.5, 34.5 / 32.7, 37.6, 33.9
+		timerUprootCD:Start(nil, self.vb.uprootCount+1)
+		updateAllTimers(self, 3.6)
 	end
 end
