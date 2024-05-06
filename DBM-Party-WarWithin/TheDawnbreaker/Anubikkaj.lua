ea6c65c5cdefa3f0eb74802b0624a513e77f6559
MysticalOS <mysticalosx@gmail.com>
2024-05-06 19:39:11 -0400

    Second boss and preliminary trash module for dawnbreaker

--- DBM-Party-WarWithin/TheDawnbreaker/Anubikkaj.lua
+++ DBM-Party-WarWithin/TheDawnbreaker/Anubikkaj.lua
@@ -12,7 +12,7 @@ mod.sendMainBossGUID = true
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
---	"SPELL_CAST_START",
+	"SPELL_CAST_START 427001 426860 426787 452127 452099"
 --	"SPELL_CAST_SUCCESS",
 --	"SPELL_AURA_APPLIED",
 --	"SPELL_AURA_REMOVED"
@@ -21,32 +21,93 @@ mod:RegisterEventsInCombat(
 --	"UNIT_SPELLCAST_SUCCEEDED boss1"
 )
 
---local warnSomeAbility						= mod:NewSpellAnnounce(373087, 3)
+--[[
+(target.name = "Anub'ikkaj" or target.name = "Ascendant Vis'coxria" or target.name = "Deathscreamer Iken'tak" or target.name = "Ixkreten the Unbreakable") and (type = "applybuff" or type = "removebuff" or type = "death" or type = "removebuffstack" or type = "applybuffstack")
+ or (source.name = "Anub'ikkaj" or source.name = "Ascendant Vis'coxria" or source.name = "Deathscreamer Iken'tak" or source.name = "Ixkreten the Unbreakable") and (type = "cast" or type = "begincast" or type = "applybuff" or type = "removebuff" or type = "removebuffstack" or type = "applybuffstack") and not ability.id = 1
+--]]
+--TODO, auto marking Animate Shadows? need to see if has spell summon event or if they instantly cast congealed for GUID target scanner, else use CID based single scan
+local warnAnimatedShadows					= mod:NewCountAnnounce(452127, 3)--Change to switch alert if they have to die asap
 
---local specWarnSomeAbility					= mod:NewSpecialWarningDefensive(372858, nil, nil, nil, 1, 2)
+local specWarnTerrifyingSlam				= mod:NewSpecialWarningRunCount(427001, nil, nil, nil, 4, 2)
+local specWarnDarkOrb						= mod:NewSpecialWarningDodgeCount(426860, nil, nil, nil, 2, 2)
+local specWarnRadiantDecay					= mod:NewSpecialWarningDodgeCount(426787, nil, nil, nil, 2, 2)
 --local yellSomeAbility						= mod:NewYell(372107)
 --local specWarnGTFO						= mod:NewSpecialWarningGTFO(372820, nil, nil, nil, 1, 8)
+local specWarnCongealedDarkness				= mod:NewSpecialWarningInterruptCount(452099, nil, nil, nil, 1, 2, 4)
 
---local timerSomeAbilityCD					= mod:NewAITimer(33.9, 372863, nil, nil, nil, 3)
+local timerTerrifyingSlamCD					= mod:NewCDCountTimer(24, 427001, nil, nil, nil, 2)
+local timerDarkOrbCD						= mod:NewCDCountTimer(24, 426860, nil, nil, nil, 3)
+local timerRadiantDecayCD					= mod:NewCDCountTimer(24, 426787, nil, nil, nil, 2, nil, DBM_COMMON_L.HEALER_ICON)
+local timerAnimateShadowsCD					= mod:NewAITimer(24, 452127, nil, nil, nil, 1, nil, DBM_COMMON_L.MYTHIC_ICON)
 
---local castsPerGUID = {}
+mod:AddPrivateAuraSoundOption(426865, true, 426860, 1)--Dark Orb target
 
+mod.vb.slamCount = 0
+mod.vb.orbCount = 0
+mod.vb.radiantCount = 0
+mod.vb.addsCount = 0
+local castsPerGUID = {}
 
---function mod:OnCombatStart(delay)
-
---end
+function mod:OnCombatStart(delay)
+	self.vb.slamCount = 0
+	self.vb.orbCount = 0
+	self.vb.radiantCount = 0
+	self.vb.addsCount = 0
+	table.wipe(castsPerGUID)
+	timerDarkOrbCD:Start(6-delay, 1)
+	timerTerrifyingSlamCD:Start(13-delay, 1)
+	timerRadiantDecayCD:Start(20-delay, 1)
+	timerAnimateShadowsCD:Start(1-delay)
+	self:EnablePrivateAuraSound(426865, "targetyou", 2)--Dark Orb
+	self:EnablePrivateAuraSound(450855, "targetyou", 2, 426865)--Register Additional ID
+end
 
 --function mod:OnCombatEnd()
 
 --end
 
---[[
 function mod:SPELL_CAST_START(args)
 	local spellId = args.spellId
-	if spellId == 372107 then
+	if spellId == 427001 then
+		self.vb.slamCount = self.vb.slamCount + 1
+		specWarnTerrifyingSlam:Show(self.vb.slamCount)
+		if DBM:UnitBuff("boss1", 427153, 427154) then--Terrifying Empowerment
+			specWarnTerrifyingSlam:Play("carefly")
+			specWarnTerrifyingSlam:ScheduleVoice(1.2, "fearsoon")
+		else
+			specWarnTerrifyingSlam:Play("justrun")
+		end
+		timerTerrifyingSlamCD:Start(nil, self.vb.slamCount+1)
+	elseif spellId == 426860 then
+		self.vb.orbCount = self.vb.orbCount + 1
+		specWarnDarkOrb:Show()
+		specWarnDarkOrb:Play("watchorb")
+		timerDarkOrbCD:Start(nil, self.vb.orbCount+1)
+	elseif spellId == 426787 then
+		self.vb.radiantCount = self.vb.radiantCount + 1
+		specWarnRadiantDecay:Show(self.vb.radiantCount)
+		specWarnRadiantDecay:Play("aesoon")
+		timerRadiantDecayCD:Start(nil, self.vb.radiantCount+1)
+	elseif spellId == 452127 then
+		self.vb.addsCount = self.vb.addsCount + 1
+		warnAnimatedShadows:Show(self.vb.addsCount)
+		timerAnimateShadowsCD:Start()--nil, self.vb.addsCount+1
+	elseif spellId == 452099 then
+		if not castsPerGUID[args.sourceGUID] then
+			castsPerGUID[args.sourceGUID] = 0
+		end
+		castsPerGUID[args.sourceGUID] = castsPerGUID[args.sourceGUID] + 1
+		local count = castsPerGUID[args.sourceGUID]
+		if self:CheckInterruptFilter(args.sourceGUID, false, false) then--Count interrupt, so cooldown is not checked
+			specWarnCongealedDarkness:Show(args.sourceName, count)
+			if count < 6 then
+				specWarnCongealedDarkness:Play("kick"..count.."r")
+			else
+				specWarnCongealedDarkness:Play("kickcast")
+			end
+		end
 	end
 end
---]]
 
 --[[
 function mod:SPELL_CAST_SUCCESS(args)
--- DBM-Party-WarWithin/TheDawnbreaker/TheDawnbreakerTrash.lua
+++ DBM-Party-WarWithin/TheDawnbreaker/TheDawnbreakerTrash.lua
@@ -5,23 +5,29 @@ mod:SetRevision("@file-date-integer@")
 --mod:SetModelID(47785)
 mod.isTrashMod = true
 
---mod:RegisterEvents(
---	"SPELL_CAST_START",
+mod:RegisterEvents(
+	"SPELL_CAST_START 451102 451119 450854 451117 451097",
 --	"SPELL_CAST_SUCCESS",
---	"SPELL_AURA_APPLIED",
+	"SPELL_AURA_APPLIED 451097",
 --	"SPELL_AURA_APPLIED_DOSE",
 --	"SPELL_AURA_REMOVED",
---	"UNIT_DIED"
---)
+	"UNIT_DIED"
+)
 
---local warnTotemicOverload					= mod:NewCastAnnounce(387145, 3)
+local warnSilkenShell						= mod:NewCastAnnounce(451097, 3)--High prio interrupt
 
---local specWarnChainLightning				= mod:NewSpecialWarningMoveAway(387127, nil, nil, nil, 1, 2)
+local specWarnRadiantDecay					= mod:NewSpecialWarningSpell(451102, nil, nil, nil, 2, 2)
+local specWarnDarkOrb						= mod:NewSpecialWarningSpell(450854, nil, nil, nil, 2, 2)
+local specWarnTerrifyingSlam				= mod:NewSpecialWarningRun(451117, nil, nil, nil, 4, 2)
 --local yellChainLightning					= mod:NewYell(387127)
---local specWarnStormshield					= mod:NewSpecialWarningDispel(386223, "MagicDispeller", nil, nil, 1, 2)
---local specWarnTempest						= mod:NewSpecialWarningInterrupt(386024, "HasInterrupt", nil, nil, 1, 2)
+local specWarnSilkenShell					= mod:NewSpecialWarningInterrupt(451097, "HasInterrupt", nil, nil, 1, 2)--High prio interrupt
+local specWarnSilkenShellDispel				= mod:NewSpecialWarningDispel(451097, "MagicDispeller", nil, nil, 1, 2)
 
---local timerRainofArrowsCD					= mod:NewCDNPTimer(15.7, 384476, nil, nil, nil, 3)
+local timerAbyssalBlastCD					= mod:NewCDNPTimer(18.4, 451119, nil, "Tank|Healer", nil, 5)
+local timerRadiantDecayCD					= mod:NewCDNPTimer(15.7, 451102, nil, nil, nil, 2)
+local timerDarkOrbCD						= mod:NewCDNPTimer(19.4, 450854, nil, nil, nil, 3)--Small sample, needs more data
+local timerTerrifyingSlamCD					= mod:NewCDNPTimer(14.5, 451117, nil, nil, nil, 2)
+local timerSilkenShellCD					= mod:NewCDNPTimer(21.4, 451097, nil, nil, nil, 4)
 --local timerBloodcurdlingShoutCD				= mod:NewCDNPTimer(19.1, 373395, nil, "HasInterrupt", nil, 4, nil, DBM_COMMON_L.INTERRUPT_ICON)
 
 --local playerName = UnitName("player")
@@ -41,12 +47,37 @@ function mod:CLTarget(targetname)
 end
 --]]
 
---[[
 function mod:SPELL_CAST_START(args)
 	local spellId = args.spellId
 	if not self:IsValidWarning(args.sourceGUID) then return end
-	if spellId == 387145 and self:AntiSpam(5, 4) then
-
+	if spellId == 451102 then
+		timerRadiantDecayCD:Start(nil, args.sourceGUID)
+		if self:AntiSpam(3, 4) then
+			specWarnRadiantDecay:Show()
+			specWarnRadiantDecay:Play("aesoon")
+		end
+	elseif spellId == 451119 then
+		timerAbyssalBlastCD:Start(nil, args.sourceGUID)
+	elseif spellId == 450854 then
+		timerDarkOrbCD:Start(nil, args.sourceGUID)
+		if self:AntiSpam(3, 2) then
+			specWarnDarkOrb:Show()
+			specWarnDarkOrb:Play("watchorb")
+		end
+	elseif spellId == 451117 then
+		timerTerrifyingSlamCD:Start(nil, args.sourceGUID)
+		if self:AntiSpam(3, 6) then
+			specWarnTerrifyingSlam:Show()
+			specWarnTerrifyingSlam:Play("justrun")
+		end
+	elseif spellId == 451097 then
+		timerSilkenShellCD:Start(nil, args.sourceGUID)
+		if self.Options.SpecWarn451097interrupt and self:CheckInterruptFilter(args.sourceGUID, false, true) then
+			specWarnSilkenShell:Show(args.sourceName)
+			specWarnSilkenShell:Play("kickcast")
+		elseif self:AntiSpam(3, 7) then
+			warnSilkenShell:Show()
+		end
 	--elseif spellId == 386024 then
 	--	timerTempestCD:Start(nil, args.sourceGUID)
 	--	if self.Options.SpecWarn386024interrupt and self:CheckInterruptFilter(args.sourceGUID, false, true) then
@@ -57,7 +88,6 @@ function mod:SPELL_CAST_START(args)
 	--	end
 	end
 end
---]]
 
 --[[
 function mod:SPELL_CAST_SUCCESS(args)
@@ -69,21 +99,28 @@ function mod:SPELL_CAST_SUCCESS(args)
 end
 --]]
 
---[[
 function mod:SPELL_AURA_APPLIED(args)
 	if not self.Options.Enabled then return end
 	local spellId = args.spellId
-	if spellId == 395035 then
-
+	if spellId == 451097 and self:AntiSpam(4, 3) then
+		specWarnSilkenShellDispel:Show(args.destName)
+		specWarnSilkenShellDispel:Play("helpdispel")
 	end
 end
 --mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
---]]
 
---[[
 function mod:UNIT_DIED(args)
 	local cid = self:GetCIDFromGUID(args.destGUID)
-	if cid == 192796 then
+	if cid == 211261 then--Ascendant Vis'coxria
+		timerAbyssalBlastCD:Stop(args.destGUID)--They all cast this
+		timerRadiantDecayCD:Stop(args.destGUID)
+	elseif cid == 211263 then--Deathscreamer Iken'tak
+		timerAbyssalBlastCD:Stop(args.destGUID)--They all cast this
+		timerDarkOrbCD:Stop(args.destGUID)
+	elseif cid == 211262 then--Ixkreten the Unbreakable
+		timerAbyssalBlastCD:Stop(args.destGUID)--They all cast this
+		timerTerrifyingSlamCD:Stop(args.destGUID)
+	elseif cid == 213932 then--Sureki Militant
+		timerSilkenShellCD:Stop(args.destGUID)
 	end
 end
---]]
