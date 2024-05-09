8263525c0c387bbc790a66689514064379751d6e
Paul Emmerich <tandanu@deadlybossmods.com>
2024-05-09 18:02:52 +0200

    DMF: Add Cannon timer for Alliance Classic (#201)

--- DBM-WorldEvents/Events/DMF/CannonClassic.lua
+++ DBM-WorldEvents/Events/DMF/CannonClassic.lua
@@ -15,7 +15,7 @@ local specWarnCancelNow = mod:NewSpecialWarningSpell(24742)
 function mod:SPELL_AURA_APPLIED(args)
 	if args:IsSpell(24742) and args:IsPlayer() then
 		local timer = C_Map.GetBestMapForUnit("player") == 1456 and 14.8 -- Thunder Bluff
-			or C_Map.GetBestMapForUnit("player") == 1429 and false -- TODO: Alliance
+			or C_Map.GetBestMapForUnit("player") == 1429 and 4.83 -- Elwynn Forest
 		if timer then
 			timerMagicWings:Start(timer)
 			specWarnCancelNow:Schedule(timer)
