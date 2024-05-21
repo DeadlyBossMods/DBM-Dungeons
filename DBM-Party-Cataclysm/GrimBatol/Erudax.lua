4532e6500dec5b88d3921af459480076da59d35d
MysticalOS <mysticalosx@gmail.com>
2024-05-21 04:36:25 -0400

    Oops, that one should be count generic

--- DBM-Party-Cataclysm/GrimBatol/Erudax.lua
+++ DBM-Party-Cataclysm/GrimBatol/Erudax.lua
@@ -21,7 +21,7 @@ local warnUmbralMending	= mod:NewSpellAnnounce(75763, 4)
 
 local specWarnMending	= mod:NewSpecialWarningInterrupt(75763, nil, nil, nil, 1, 2)
 local specWarnGale		= mod:NewSpecialWarningCount(75664, nil, nil, nil, 2, 2)
-local specWarnAdds		= mod:NewSpecialWarningAdds(75704, "Dps", nil, nil, 3, 2)
+local specWarnAdds		= mod:NewSpecialWarningAddsCount(75704, "Dps", nil, nil, 3, 2)
 
 local timerFeebleCD		= mod:NewCDCountTimer(26, 75792, nil, "Tank", nil, 5, nil, DBM_COMMON_L.TANK_ICON)
 local timerFeeble		= mod:NewTargetTimer(3, 75792, nil, "Tank|Healer", 2, 5)
