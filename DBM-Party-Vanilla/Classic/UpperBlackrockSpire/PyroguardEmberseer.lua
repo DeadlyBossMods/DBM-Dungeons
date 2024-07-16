ed0a7931e26e798e016502f63c013770562882fd
Paul Emmerich <tandanu@deadlybossmods.com>
2024-07-16 20:13:21 +0200

    UBRS: Fix Emberseer combat start timer on Era

--- DBM-Party-Vanilla/Classic/UpperBlackrockSpire/PyroguardEmberseer.lua
+++ DBM-Party-Vanilla/Classic/UpperBlackrockSpire/PyroguardEmberseer.lua
@@ -10,7 +10,14 @@ mod:RegisterEvents(
 	"CHAT_MSG_MONSTER_EMOTE"
 )
 
-local timerCombatStart	= mod:NewCombatTimer(64)
+-- Log from SoD:
+-- <224.24 13:39:27> [CHAT_MSG_MONSTER_EMOTE] %s begins to regain its strength!#Pyroguard Emberseer
+-- <255.00 13:39:57> [CHAT_MSG_MONSTER_EMOTE] %s is nearly at full strength!#Pyroguard Emberseer
+-- <285.77 13:40:28> [CHAT_MSG_MONSTER_EMOTE] %s regains its power and breaks free of its bonds!#Pyroguard Emberseer
+-- <287.41 13:40:30> [CHAT_MSG_MONSTER_YELL] Ha! Ha! Ha! Thank you for freeing me, fools. Now let me repay you by charring the flesh from your bones.#Pyroguard Emberseer
+-- <290.44 13:40:33> [CLEU] SWING_DAMAGE#Creature-0-5210-229-17836-9816-000015056F#Pyroguard Emberseer
+-- <290.45 13:40:33> [NAME_PLATE_UNIT_ADDED] Pyroguard Emberseer
+local timerCombatStart	= mod:NewCombatTimer(DBM:IsRetail() and 64 or 66.2)
 
 function mod:CHAT_MSG_MONSTER_EMOTE(msg)
 	if msg == L.Pull or msg:find(L.Pull) then
