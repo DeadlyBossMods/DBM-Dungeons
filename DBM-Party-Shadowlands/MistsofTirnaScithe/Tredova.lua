2310943fc5812e52de37c84af38b987815b3e85b
MysticalOS <mysticalosx@gmail.com>
2024-08-08 23:43:57 -0400

    Reviewed and updated TIrna Scithe trash warnings and timers Reviewed and updated all Tirna Scithe bosses as well

--- DBM-Party-Shadowlands/MistsofTirnaScithe/Tredova.lua
+++ DBM-Party-Shadowlands/MistsofTirnaScithe/Tredova.lua
@@ -4,12 +4,13 @@ local L		= mod:GetLocalizedStrings()
 mod:SetRevision("@file-date-integer@")
 mod:SetCreatureID(164517)
 mod:SetEncounterID(2393)
+mod:SetHotfixNoticeRev(20240808000000)
 mod:SetUsedIcons(1, 2, 3, 4, 5)--Probably doesn't use all 5, unsure number of mind link targets at max inteligence/energy
 
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
-	"SPELL_CAST_START 322550 322614 337235 337249 337255",
+	"SPELL_CAST_START 322550 322614 337235 337249 337255 463602",
 	"SPELL_CAST_SUCCESS 322614 322654",
 	"SPELL_INTERRUPT",
 	"SPELL_AURA_APPLIED 322527 331172 322648 322563",
@@ -21,40 +22,47 @@ mod:RegisterEventsInCombat(
 
 --Timers can't be fixed until numerous transcriptor logs, without boss energy can't start mark prey timer up mid fight or update CD on other abilities cast more often at higher energy
 --[[
-ability.id = 322550 and type = "begincast"
+(ability.id = 463602 or ability.id = 322550) and type = "begincast"
  or (ability.id = 322614 or ability.id = 322654 or ability.id = 322563) and type = "cast"
  or (ability.id = 322527 or ability.id = 322450) and (type = "applybuff" or type = "removebuff" or type = "applydebuff" or type = "removedebuff")
  or (ability.id = 337235 or ability.id = 337249 or ability.id = 337255) and type = "begincast"
+ or blockedAbility.id = 322450
+ or type = "dungeonencounterstart" or type = "dungeonencounterend"
 --]]
-local warnMarkthePrey				= mod:NewTargetNoFilterAnnounce(322563, 3)
-local warnInfestor					= mod:NewAnnounce("warnInfestor", 4, 337235, nil, nil, nil, 337235)
+local warnMarkedPrey				= mod:NewTargetNoFilterAnnounce(322563, 3)
+--local warnInfestor					= mod:NewAnnounce("warnInfestor", 4, 337235, nil, nil, nil, 337235)
 
 local specWarnConsumption			= mod:NewSpecialWarningDodge(322450, nil, nil, nil, 2, 2)
 local specWarnConsumptionKick		= mod:NewSpecialWarningInterrupt(322450, "HasInterrupt", nil, 2, 1, 2)
 local specWarnAcceleratedIncubation	= mod:NewSpecialWarningSwitchCount(322550, "Dps", nil, nil, 1, 2)
 local specWarnMindLink				= mod:NewSpecialWarningMoveAway(322648, nil, nil, nil, 1, 11)
 local yellMindLink					= mod:NewYell(322648)
-local specWarnMarkthePrey			= mod:NewSpecialWarningYou(322563, nil, nil, nil, 1, 2)
+local specWarnMarkedPrey			= mod:NewSpecialWarningYou(322563, nil, nil, nil, 1, 2)
 local specWarnAcidExpulsion			= mod:NewSpecialWarningDodgeCount(322654, nil, nil, nil, 2, 2)
-local specWarnParasiticInfesterKick	= mod:NewSpecialWarning("specWarnParasiticInfesterKick", nil, nil, nil, 1, 2, 4, 337235, 337235)
-local yellParasiticInfester			= mod:NewYell(337235, L.Infester, true, "yellParasiticInfester")
+local specWarnCoalescingPoison		= mod:NewSpecialWarningRunCount(463602, nil, nil, nil, 4, 12, 4)
+--local specWarnParasiticInfesterKick	= mod:NewSpecialWarning("specWarnParasiticInfesterKick", nil, nil, nil, 1, 2, 4, 337235, 337235)
+--ocal yellParasiticInfester			= mod:NewYell(337235, L.Infester, true, "yellParasiticInfester")
 local specWarnGTFO					= mod:NewSpecialWarningGTFO(326309, nil, nil, nil, 1, 8)
 
-local timerAcceleratedIncubationCD	= mod:NewCDCountTimer(30.3, 322550, nil, nil, nil, 1, nil, nil, true)--30-52? Is this health based?
-local timerMindLinkCD				= mod:NewCDCountTimer(15.4, 322648, nil, nil, nil, 3, nil, nil, true)--15-19, still not cast if everyone already affected by it.
-local timerAcidExpulsionCD			= mod:NewCDCountTimer(19.4, 322654, nil, nil, nil, 3, nil, nil, true)--19-26
-local timerParasiticInfesterCD		= mod:NewTimer(23, "timerParasiticInfesterCD", 337235, nil, nil, 4, DBM_COMMON_L.MYTHIC_ICON..DBM_COMMON_L.INTERRUPT_ICON, true)--23-26.3 (mostly 25-26)
+--All timers are now 35 and all reset to pull timers on shield interrupt
+local timerAcceleratedIncubationCD	= mod:NewCDCountTimer(35, 322550, nil, nil, nil, 1)
+local timerMindLinkCD				= mod:NewCDCountTimer(35, 322648, nil, nil, nil, 3)
+local timerAcidExpulsionCD			= mod:NewCDCountTimer(35, 322654, nil, nil, nil, 3)
+local timerCoalescingPoisonCD		= mod:NewCDCountTimer(35, 463602, nil, nil, nil, 1)
+local timerMarkedPreyCD				= mod:NewCDCountTimer(35, 322563, nil, nil, nil, 3)
+--local timerParasiticInfesterCD		= mod:NewTimer(23, "timerParasiticInfesterCD", 337235, nil, nil, 4, DBM_COMMON_L.MYTHIC_ICON..DBM_COMMON_L.INTERRUPT_ICON, true)--23-26.3 (mostly 25-26)
 
 mod:AddInfoFrameOption(322527, true)
 mod:AddSetIconOption("SetIconOnMindLink", 322648, true, 0, {1, 2, 3, 4, 5})
 
 mod.vb.mindLinkIcon = 1
-mod.vb.firstPray = false
 mod.vb.inubationCount = 0
 mod.vb.mindlinkCount = 0
 mod.vb.expulsionCount = 0
-mod.vb.parasiteCount = 0
+mod.vb.poisonCount = 0
+--mod.vb.parasiteCount = 0
 
+--[[
 function mod:InfesterTarget(targetname, uId)
 	if not targetname then return end
 	warnInfestor:Show(targetname)
@@ -62,17 +70,28 @@ function mod:InfesterTarget(targetname, uId)
 		yellParasiticInfester:Yell()
 	end
 end
+--]]
+
+--Sometimes boss skips first accelerated incubation after shield interrupt
+--So we have to detect this and restart timer for 2nd cast
+local function FixBlizzardBug(self)
+	self.vb.inubationCount = self.vb.inubationCount + 1
+	timerAcceleratedIncubationCD:Start(30, self.vb.inubationCount+1)
+end
 
 function mod:OnCombatStart(delay)
 	self.vb.mindLinkIcon = 1
-	self.vb.firstPray = false
 	self.vb.inubationCount = 0
 	self.vb.mindlinkCount = 0
 	self.vb.expulsionCount = 0
-	self.vb.parasiteCount = 0
-	timerAcidExpulsionCD:Start(7.1-delay, 1)
-	timerMindLinkCD:Start(18.1-delay, 1)--21 in TWW?
-	timerAcceleratedIncubationCD:Start(21.4-delay, 1)--Was 45.2
+	self.vb.poisonCount = 0
+	--self.vb.parasiteCount = 0
+	timerAcidExpulsionCD:Start(6.9-delay, 1)
+	timerAcceleratedIncubationCD:Start(10.9-delay, 1)
+	timerMindLinkCD:Start(24.9-delay, 1)
+	if self:IsMythic() then
+		timerCoalescingPoisonCD:Start(26-delay, 1)
+	end
 	--if self:IsMythic() then
 	--	timerParasiticInfesterCD:Start(11.7-delay, 1)
 	--end
@@ -93,14 +112,20 @@ function mod:SPELL_CAST_START(args)
 		timerAcceleratedIncubationCD:Start(nil, self.vb.inubationCount+1)
 	elseif spellId == 322614 then
 		self.vb.mindLinkIcon = 2
-	elseif spellId == 337235 or spellId == 337249 or spellId == 337255 then
-		self.vb.parasiteCount = self.vb.parasiteCount + 1
-		self:ScheduleMethod(0.2, "BossTargetScanner", args.sourceGUID, "InfesterTarget", 0.1, 8)
-		timerParasiticInfesterCD:Start(nil, self.vb.parasiteCount)
-		if self:CheckInterruptFilter(args.sourceGUID, false, true) then
-			specWarnParasiticInfesterKick:Show(args.sourceName, self.vb.parasiteCount)
-			specWarnParasiticInfesterKick:Play("kickcast")
-		end
+	elseif spellId == 463602 then
+		self.vb.poisonCount = self.vb.poisonCount + 1
+		specWarnCoalescingPoison:Show(self.vb.poisonCount)
+		specWarnCoalescingPoison:Play("pullin")
+		specWarnCoalescingPoison:ScheduleVoice(2, "justrun")
+		timerCoalescingPoisonCD:Start(nil, self.vb.poisonCount+1)
+	--elseif spellId == 337235 or spellId == 337249 or spellId == 337255 then
+	--	self.vb.parasiteCount = self.vb.parasiteCount + 1
+	--	self:ScheduleMethod(0.2, "BossTargetScanner", args.sourceGUID, "InfesterTarget", 0.1, 8)
+	--	timerParasiticInfesterCD:Start(nil, self.vb.parasiteCount)
+	--	if self:CheckInterruptFilter(args.sourceGUID, false, true) then
+	--		specWarnParasiticInfesterKick:Show(args.sourceName, self.vb.parasiteCount)
+	--		specWarnParasiticInfesterKick:Play("kickcast")
+	--	end
 	end
 end
 
@@ -127,10 +152,12 @@ end
 function mod:SPELL_AURA_APPLIED(args)
 	local spellId = args.spellId
 	if spellId == 322527 then--Gorging Shield (Consumption starting)
+		self:Unschedule(FixBlizzardBug)
 		timerMindLinkCD:Stop()
 		timerAcidExpulsionCD:Stop()
---		timerMarkthePreyCD:Stop()
+		timerMarkedPreyCD:Stop()
 		timerAcceleratedIncubationCD:Stop()
+		timerCoalescingPoisonCD:Stop()
 		if self:AntiSpam(3, 1) then
 			specWarnConsumption:Show()
 			specWarnConsumption:Play("watchstep")
@@ -157,10 +184,10 @@ function mod:SPELL_AURA_APPLIED(args)
 		end
 	elseif spellId == 322563 then
 		if args:IsPlayer() then
-			specWarnMarkthePrey:Show()
-			specWarnMarkthePrey:Play("targetyou")
+			specWarnMarkedPrey:Show()
+			specWarnMarkedPrey:Play("targetyou")
 		else
-			warnMarkthePrey:Show(args.destName)
+			warnMarkedPrey:Show(args.destName)
 		end
 	end
 end
@@ -169,6 +196,15 @@ function mod:SPELL_AURA_REMOVED(args)
 	local spellId = args.spellId
 	if spellId == 322450 then--Consumption ended
 		--TODO, maybe update timers that are all spell queued at this point, which will
+		timerAcidExpulsionCD:Start(6.9, self.vb.expulsionCount+1)
+		timerAcceleratedIncubationCD:Start(10.9, self.vb.inubationCount+1)
+		self:Unschedule(FixBlizzardBug)
+		self:Schedule(15.9, FixBlizzardBug, self)
+		timerMarkedPreyCD:Start(14.5, self.vb.mindlinkCount+1)
+		timerMindLinkCD:Start(24.9, self.vb.mindlinkCount+1)
+		if self:IsMythic() then
+			timerCoalescingPoisonCD:Start(26, self.vb.poisonCount+1)
+		end
 	elseif spellId == 322527 then--Gorging Shield
 		specWarnConsumptionKick:Show(args.destName)
 		specWarnConsumptionKick:Play("kickcast")
