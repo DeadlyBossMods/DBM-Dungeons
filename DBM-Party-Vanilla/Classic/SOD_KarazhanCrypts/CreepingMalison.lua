a9ad0ad04abff1bc451bcccad66a64061ca2b800
Paul Emmerich <tandanu@deadlybossmods.com>
2024-12-19 20:38:01 +0100

    Add placeholder mods for new SoD dungeon Karazhan Crypts

---
+++ DBM-Party-Vanilla/Classic/SOD_KarazhanCrypts/CreepingMalison.lua
@@ -0,0 +1,17 @@
+local mod	= DBM:NewMod("CreepingMalison", "DBM-Party-Vanilla", 22)
+local L		= mod:GetLocalizedStrings()
+
+mod:SetRevision("@file-date-integer@")
+mod:SetEncounterID(3146)
+--mod:SetCreatureID()
+mod:SetZone(2875)
+
+mod:RegisterCombat("combat")
+
+mod:RegisterEventsInCombat(
+)
+
+function mod:OnCombatStart()
+	self:AddMsg("This DBM mod is a placeholder for new content, there are no timers or warnings yet.")
+	self:AddMsg("If you see this message well after the new content release consider updating the DBM Dungeon module to the latest version.")
+end
