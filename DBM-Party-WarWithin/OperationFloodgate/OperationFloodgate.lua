1fe3e27f6b2da96d06d4018dfcf7b74e7fd4da5a
MysticalOS <mysticalosx@gmail.com>
2024-12-19 21:33:02 -0500

    Further improve templating for operation floodgate

--- DBM-Party-WarWithin/OperationFloodgate/OperationFloodgate.lua
+++ DBM-Party-WarWithin/OperationFloodgate/OperationFloodgate.lua
@@ -1,4 +1,4 @@
-local mod	= DBM:NewMod("111DungeonTrash", "DBM-Party-WarWithin", 9)
+local mod	= DBM:NewMod("OperationFloodgateTrash", "DBM-Party-WarWithin", 9)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision("@file-date-integer@")
