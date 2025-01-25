d8e32497dc2bfe3290f8c31dfea3ae6307182ba6
anon1231823 <67269448+anon1231823@users.noreply.github.com>
2025-01-25 15:53:32 -0500

    Update localization.es.lua (#355)

--- DBM-Party-Vanilla/Classic/localization.es.lua
+++ DBM-Party-Vanilla/Classic/localization.es.lua
@@ -1532,3 +1532,62 @@ L = DBM:GetModLocalization(489)
 L:SetGeneralLocalization{
 	name 		= "Jefe Ukorz Cabellarena"
 }
+-----------------------------
+--Demon Fall Canyon
+-----------------------------
+--  Grimroot  --
+-----------------------------
+L = DBM:GetModLocalization("Grimroot")
+
+L:SetGeneralLocalization{
+	name 		= "Sombrirraíz"
+}
+
+-----------------------------
+--  Diathorus the Seeker  --
+-----------------------------
+L = DBM:GetModLocalization("Diathorus")
+
+L:SetGeneralLocalization{
+	name 		= "Diathorus el Buscador"
+}
+-----------------------------
+--  Destructor's Wraith  --
+-----------------------------
+L = DBM:GetModLocalization("DestructorsWraith")
+
+L:SetGeneralLocalization{
+	name 		= "Ánima del Destructor"
+}
+-----------------------------
+--  Zilbagob  --
+-----------------------------
+L = DBM:GetModLocalization("Zilbagob")
+
+L:SetGeneralLocalization{
+	name 		= "Zilbagob"
+}
+-----------------------------
+--  Azgaloth  --
+-----------------------------
+L = DBM:GetModLocalization("Azgaloth")
+
+L:SetGeneralLocalization{
+	name 		= "Azgaloth"
+}
+-----------------------------
+--  Pyranis  --
+-----------------------------
+L = DBM:GetModLocalization("Pyranis")
+
+L:SetGeneralLocalization{
+	name 		= "Pyranis"
+}
+-----------------------------
+--  Hellscream's Phantom  --
+-----------------------------
+L = DBM:GetModLocalization("HellscreamsPhantom")
+
+L:SetGeneralLocalization{
+	name 		= "Espectro de Grito Infernal"
+}
