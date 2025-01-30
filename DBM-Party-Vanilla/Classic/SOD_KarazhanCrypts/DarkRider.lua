3b8c97542452965b69c27936e24508e54a6a3b58
Paul Emmerich <tandanu@deadlybossmods.com>
2025-01-31 00:30:43 +0100

    KarazhanCryptos: Add support for Dark Rider

--- DBM-Party-Vanilla/Classic/SOD_KarazhanCrypts/DarkRider.lua
+++ DBM-Party-Vanilla/Classic/SOD_KarazhanCrypts/DarkRider.lua
@@ -10,11 +10,53 @@ mod:SetZone(2875)
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
+	"UNIT_HEALTH",
+	"SWING_DAMAGE"
 )
 
--- My pug failed at Kharon, so I don't have logs yet for a good initial version. Might make some guesses based on warcraftlogs and videos later.
+-- Grab Torch
+-- Two spell IDs, 1220904 and 1220905, casts on torch bearer first then on no target. Only has UCS.
+-- Trigger seems aligned with phase changes, so no warning.
+
+-- I didn't understand the Torment's Illusion mechanic, it's some kind of mirror image that only you can see that you need to kill.
+-- But it spawning doesn't really show up in the log, and nothing really happens if you just ignore it?
+-- The only way to detect it seems to be getting hit by it?
+
+-- There is likely a way to get a combat start timer, but the triggering yell seems inconsistent between first pull and later pulls.
+-- And the fight doesn't really start, it's just him spawning. Might revisit.
+
+local warnPhase2Soon = mod:NewPrePhaseAnnounce(2)
+
+local specWarnIllusion	= mod:NewSpecialWarningTargetChange(1220912, nil, nil, nil, 1, 2)
+
+local warnedPhase1, warnedPhase2, warnedPhase3
 
 function mod:OnCombatStart()
-	self:AddMsg("This DBM mod is a placeholder for new content, there are no timers or warnings yet.")
-	self:AddMsg("If you see this message well after the new content release consider updating the DBM Dungeon module to the latest version.")
+	warnedPhase1, warnedPhase2, warnedPhase3 = false, false, false
+end
+
+function mod:UNIT_HEALTH(uId)
+	if self:GetUnitCreatureId(uId) ~= 238055 then
+		return
+	end
+	local hp = UnitHealth(uId) / UnitHealthMax(uId)
+	if not warnedPhase1 and hp >= 0.76 and hp <= 0.80 then
+		warnedPhase1 = true
+		warnPhase2Soon:Show()
+	end
+	if not warnedPhase2 and hp >= 0.51 and hp <= 0.55 then
+		warnedPhase2 = true
+		warnPhase2Soon:Show()
+	end
+	if not warnedPhase3 and hp >= 0.26 and hp <= 0.30 then
+		warnedPhase2 = true
+		warnPhase2Soon:Show()
+	end
+end
+
+function mod:SWING_DAMAGE(srcGuid, _, _, _, destGuid)
+	if srcGuid == UnitGUID("player") and self:GetCIDFromGUID(destGuid) == 238443 and self:AntiSpam(15, "AttackGhost") then
+		specWarnIllusion:Show(L.Ghost)
+		specWarnIllusion:Play("targetchange")
+	end
 end
