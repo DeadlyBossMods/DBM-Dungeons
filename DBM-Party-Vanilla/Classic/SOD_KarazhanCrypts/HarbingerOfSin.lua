9a6c2f88fc0aef7d13f6adc2d1d821c2c0531c26
Paul Emmerich <tandanu@deadlybossmods.com>
2025-01-31 00:31:44 +0100

    KarazhanCrypts/Harbinger: Add Warning for Flame thingy to dodge
    Note sure if this is useful, might remove again after my next run

--- DBM-Party-Vanilla/Classic/SOD_KarazhanCrypts/HarbingerOfSin.lua
+++ DBM-Party-Vanilla/Classic/SOD_KarazhanCrypts/HarbingerOfSin.lua
@@ -10,7 +10,7 @@ mod:SetZone(2875)
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
-	"SPELL_CAST_START 1219420"
+	"SPELL_CAST_START 1219420 1219387"
 )
 
 -- Pull of the damned --> run out until he casts inferno
@@ -26,6 +26,9 @@ local pullTimer		= mod:NewVarTimer("v45-48.6", 1219420)
 
 local specWarnInferno = mod:NewSpecialWarningRun(1220927, nil, nil, nil, 4, 2)
 
+-- Flame thingy to dodge, generic "soon" announce because the actual damaging spell triggers way after the cast of this is done
+local warnFlameWhirl = mod:NewSoonAnnounce(1219387)
+
 function mod:OnCombatStart(delay)
 	enrageTimer:Start()
 	pullTimer:Start(33.5) -- TODO: validate this
@@ -36,6 +39,9 @@ function mod:SPELL_CAST_START(args)
 		infernoTimer:Start()
 		specWarnInferno:Show()
 		specWarnInferno:Play("justrun")
+		specWarnInferno:ScheduleVoice(18.1, "safenow") -- 100ms before it's actually safe, should be fine with time to play the audio
 		pullTimer:Start()
+	elseif args:IsSpell(1219387) then
+		warnFlameWhirl:Show()
 	end
 end
