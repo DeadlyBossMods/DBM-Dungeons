675ebb459077ac5a0c78f4ddc1f0d1e64ca9ea6b
Paul Emmerich <tandanu@deadlybossmods.com>
2025-01-31 00:30:08 +0100

    KarazhanCryptos/CreepingMalison: Add GTFO warning for burning webs

--- DBM-Party-Vanilla/Classic/SOD_KarazhanCrypts/CreepingMalison.lua
+++ DBM-Party-Vanilla/Classic/SOD_KarazhanCrypts/CreepingMalison.lua
@@ -9,7 +9,31 @@ mod:SetZone(2875)
 
 mod:RegisterCombat("combat")
 
+-- The GTFO warning uses in combat events on purpose despite the fire staying around after the fight.
+-- The warning is just annoying if you are looking for the Relics after the fight.
 mod:RegisterEventsInCombat(
+	"SPELL_PERIODIC_DAMAGE 1222097",
+	"SPELL_PERIODIC_MISSED 1222097",
+	"SPELL_AURA_APPLIED 1222097"
 )
 
--- This fight was completely free, just tank and spank. Adds might be annoying if you don't have a Shaman tank?
+-- This fight was completely free, just tank and spank.
+-- Without a Shaman tank you should probably burn the eggs to avoid adds, that creates some fire on the floor to watch out for.
+
+local specWarnGTFO	= mod:NewSpecialWarningGTFO(1222097, nil, nil, nil, 1, 8)
+
+function mod:SPELL_PERIODIC_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId, spellName)
+	if spellId == 1222097 and destGUID == UnitGUID("player") and self:AntiSpam(3.5, "gtfo") then
+		specWarnGTFO:Show(spellName)
+		specWarnGTFO:Play("watchfeet")
+	end
+end
+mod.SPELL_PERIODIC_MISSED = mod.SPELL_PERIODIC_DAMAGE
+
+function mod:SPELL_AURA_APPLIED(args)
+	if args:IsSpellID(1222097) and args:IsPlayer() and self:AntiSpam(3.5, "gtfo") then
+		specWarnGTFO:Show(args.spellName)
+		specWarnGTFO:Play("watchfeet")
+	end
+end
+
