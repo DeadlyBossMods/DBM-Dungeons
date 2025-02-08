93b386a1b7c05747bf1a48186cea5f3918e6135c
Paul Emmerich <tandanu@deadlybossmods.com>
2025-02-08 15:23:08 +0100

    KarazhanCrypts/Unkomon: Add interrupt warning for Shadow Bolt Volley
    Bad pugs have shown me that this warning is good to have

--- DBM-Party-Vanilla/Classic/SOD_KarazhanCrypts/Unkomon.lua
+++ DBM-Party-Vanilla/Classic/SOD_KarazhanCrypts/Unkomon.lua
@@ -12,7 +12,8 @@ mod:RegisterCombat("combat")
 mod:RegisterEventsInCombat(
 	"SPELL_AURA_APPLIED 1221576",
 	"SPELL_AURA_APPLIED_DOSE 1221576",
-	"SPELL_CAST_SUCCESS 1221577"
+	"SPELL_CAST_SUCCESS 1221577",
+	"SPELL_CAST_START 1220515"
 )
 
 -- Shadow Bolt Volley
@@ -31,17 +32,25 @@ mod:RegisterEventsInCombat(
 local warnFrenzy	= mod:NewStackAnnounce(1221576, 2, nil, "Tank|Healer|RemoveEnrage")
 local warnDoom		= mod:NewAnnounce("WarnDoom", 1, 1221577, "RemoveCurse|Healer")
 
+local specWarnInterrupt = mod:NewSpecialWarningInterrupt(1220515, "HasInterrupt", nil, nil, 1, 2)
+
 function mod:SPELL_AURA_APPLIED(args)
 	if args:IsSpell(1221576) then
 		local amount = args.amount or 1
 		warnFrenzy:Show(args.destName, amount)
 	end
 end
-
 mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
 
 function mod:SPELL_CAST_SUCCESS(args)
 	if args:IsSpell(1221577) then
 		warnDoom:Show()
+	end
+end
+
+function mod:SPELL_CAST_START(args)
+	if args:IsSpell(1220515) and self:CheckInterruptFilter(args.sourceGUID, true, false) then
+		specWarnInterrupt:Show(args.sourceName)
+		specWarnInterrupt:Play("kickcast")
 	end
 end
