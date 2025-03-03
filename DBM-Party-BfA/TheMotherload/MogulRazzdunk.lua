6ffef7da76791bf36e594411e3ce626ef14537a4
MysticalOS <mysticalosx@gmail.com>
2025-03-02 20:39:48 -0500

    lock down motherload trash with more strict validation checks to avoid false dispel alerts from RP mobs Improve mogul razzdunks timers further.

--- DBM-Party-BfA/TheMotherload/MogulRazzdunk.lua
+++ DBM-Party-BfA/TheMotherload/MogulRazzdunk.lua
@@ -28,10 +28,11 @@ local warnSummonBooma				= mod:NewSpellAnnounce(276212, 2)
 --Stage One: Big Guns
 local specWarnGatlingGun			= mod:NewSpecialWarningDodgeCount(260280, nil, nil, nil, 3, 8)
 local specWarnHomingMissile			= mod:NewSpecialWarningMoveAway(260811, nil, nil, nil, 1, 2)
-local yellHomingMissile				= mod:NewYell(260811)
+local yellHomingMissile				= mod:NewShortYell(260811)
 --Stage Two: Drill
 local specWarnDrillSmash			= mod:NewSpecialWarningMoveTo(271456, nil, nil, nil, 1, 2)
-local yellDrillSmash				= mod:NewYell(271456)
+local yellDrillSmash				= mod:NewShortYell(271456)
+local yellDrillSmashFades			= mod:NewShortFadesYell(271456)
 local specWarnHeartseeker			= mod:NewSpecialWarningYou(262515, nil, nil, nil, 1, 2)
 local specWarnHeartseekerOther		= mod:NewSpecialWarningTarget(262515, "Tank", nil, nil, 1, 2)
 local yellHeartseeker				= mod:NewYell(262515)
@@ -48,13 +49,14 @@ mod.vb.gatCount = 0
 mod.vb.homingCount = 0
 mod.vb.drillCount = 0
 
-function mod:DrillTarget(targetname)
+function mod:DrillTarget(targetname, _, scanTime)
 	if not targetname then return end
 	if self:AntiSpam(4, targetname) then--Antispam to lock out redundant later warning from firing if this one succeeds
 		if targetname == UnitName("player") then
 			specWarnDrillSmash:Show(rocket)
 			specWarnDrillSmash:Play("targetyou")
 			yellDrillSmash:Yell()
+			yellDrillSmashFades:Countdown(5 - scanTime)
 		else
 			warnDrillSmash:Show(targetname)
 		end
@@ -78,8 +80,16 @@ function mod:SPELL_CAST_START(args)
 		specWarnGatlingGun:Play("behindboss")
 		specWarnGatlingGun:ScheduleVoice(1.5, "keepmove")
 		--"Gatling Gun-260280-npc:129232-000034EF89 = pull:20.0, 30.0, 92.7, 20.0, 25.0, 20.0",
-		local timer = self.vb.stageTotality == 1 and 30 or "v20-25"
-		timerGatlingGunCD:Start(timer, self.vb.gatCount+1)
+		--"Gatling Gun-260280-npc:129232-000044832B = pull:20.0, 30.0, 30.0, 30.0, 79.6, 20.0, 25.0, 20.0, 25.0, 20.0, 25.0, 20.0, 25.0, 20.0, 25.0, 20.0"
+		if self:GetStage(3) then
+			if self.vb.gatCount % 2 == 0 then
+				timerGatlingGunCD:Start(25, self.vb.gatCount+1)
+			else
+				timerGatlingGunCD:Start(20, self.vb.gatCount+1)
+			end
+		else
+			timerGatlingGunCD:Start(30, self.vb.gatCount+1)
+		end
 	elseif spellId == 271456 then
 		self.vb.drillCount = self.vb.drillCount + 1
 		self:ScheduleMethod(0.5, "BossTargetScanner", args.sourceGUID, "DrillTarget", 0.1, 12, true)
@@ -88,8 +98,16 @@ function mod:SPELL_CAST_START(args)
 	elseif spellId == 260813 then
 		self.vb.homingCount = self.vb.homingCount + 1
 		--"Homing Missile-260813-npc:129232-000034EF89 = pull:5.0, 30.0, 30.0, 67.7, 21.0, 24.0, 21.0",
-		local timer = self.vb.stageTotality == 1 and 30 or "v21-24"
-		timerHomingMissileCD:Start(timer, self.vb.homingCount+1)
+		--"Homing Missile-260813-npc:129232-000044832B = pull:5.0, 30.0, 30.0, 30.0, 84.6, 21.0, 24.0, 21.1, 24.0, 21.1, 23.9, 21.1, 23.9, 21.1, 24.0, 21.0, 24.0",
+		if self:GetStage(3) then
+			if self.vb.homingCount % 2 == 0 then
+				timerHomingMissileCD:Start(24, self.vb.homingCount+1)
+			else
+				timerHomingMissileCD:Start(21, self.vb.homingCount+1)
+			end
+		else
+			timerHomingMissileCD:Start(30, self.vb.homingCount+1)
+		end
 	end
 end
 
@@ -110,7 +128,7 @@ function mod:SPELL_AURA_APPLIED(args)
 		timerHomingMissileCD:Stop()
 		timerDrillSmashCD:Start(17, self.vb.drillCount+1)
 	elseif spellId == 260190 and self:IsInCombat() then--Configuration: Combat
-		self:SetStage(1)
+		self:SetStage(3)
 		timerDrillSmashCD:Stop()
 		timerHomingMissileCD:Start("v7-8.3", self.vb.homingCount+1)
 		timerGatlingGunCD:Start("v17-18.3", self.vb.gatCount+1)
