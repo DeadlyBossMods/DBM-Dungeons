31a4866172b8ce9ee58fa0b0498aca8344968b27
MysticalOS <mysticalosx@gmail.com>
2025-03-03 08:26:08 -0500

    updated all priory of flame bosses for S2 Also changed how captain dailcry's minions work by making timers hybrid timers (both timers AND nameplate timers) instead of just nameplate timers. this helps them behave more as expected when pulled with boss.

--- DBM-Party-WarWithin/PrioryoftheSacredFlame/BaronBraunpyke.lua
+++ DBM-Party-WarWithin/PrioryoftheSacredFlame/BaronBraunpyke.lua
@@ -4,8 +4,8 @@ local L		= mod:GetLocalizedStrings()
 mod:SetRevision("@file-date-integer@")
 mod:SetCreatureID(207939)
 mod:SetEncounterID(2835)
-mod:SetHotfixNoticeRev(20240608000000)
---mod:SetMinSyncRevision(20211203000000)
+mod:SetHotfixNoticeRev(20250303000000)
+mod:SetMinSyncRevision(20250303000000)
 mod:SetZone(2649)
 --mod.respawnTime = 29
 mod.sendMainBossGUID = true
@@ -13,16 +13,19 @@ mod.sendMainBossGUID = true
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
-	"SPELL_CAST_START 422969 423051 446657 446368",
+	"SPELL_CAST_START 422969 423051 446657 446368 423062 446598",
 --	"SPELL_CAST_SUCCESS",
---	"SPELL_AURA_APPLIED 423015 446649",
---	"SPELL_AURA_REMOVED"
+	"SPELL_AURA_APPLIED 446403",
+	"SPELL_AURA_APPLIED_DOSE 446403",
+	"SPELL_AURA_REMOVED 422969",
+	"SPELL_MISSED 446403",
 --	"SPELL_PERIODIC_DAMAGE",
 --	"SPELL_PERIODIC_MISSED"
 	"UNIT_SPELLCAST_SUCCEEDED boss1"
 )
 
 --TODO, Pyre tracking on infoframe?
+--TODO, timers only vetted for mythic+ . mythic 0 and and heroic and normal and follower not vetted or tested at all
 --[[
 (ability.id = 422969 or ability.id = 423051 or ability.id = 446657 or ability.id = 446368) and type = "begincast"
  or (ability.id = 423015 or ability.id = 446649) and type = "applybuff"
@@ -30,18 +33,20 @@ mod:RegisterEventsInCombat(
 --]]
 --Announce fade of wrath too?
 local warnVindictiveWrath					= mod:NewCountAnnounce(422969, 3)
+local warnVindictiveWrathFades				= mod:NewFadesAnnounce(422969, 1)
 local warnCastigatorsShield					= mod:NewCountAnnounce(423015, 3)
 local warnSacredPyre						= mod:NewCountAnnounce(446368, 3)
+local warnFlamesLeft						= mod:NewAddsLeftAnnounce(446368, 2)
 
 local specWarnBurningLight					= mod:NewSpecialWarningInterruptCount(423051, "HasInterrupt", nil, nil, 1, 2)
 local specWarnHammerofPurity				= mod:NewSpecialWarningDodgeCount(423062, nil, nil, nil, 2, 2)
 --local specWarnGTFO						= mod:NewSpecialWarningGTFO(372820, nil, nil, nil, 1, 8)
 
 local timerVindictiveWrathCD				= mod:NewCDCountTimer(48.5, 422969, nil, nil, nil, 6)
-local timerCastigatorsShieldCD				= mod:NewVarCountTimer("v23-27", 423015, nil, nil, nil, 3)--23-27
-local timerBurningLightCD					= mod:NewVarCountTimer("v34-36.4", 423051, nil, nil, nil, 4, nil, DBM_COMMON_L.INTERRUPT_ICON)
-local timerHammerofPurityCD					= mod:NewCDCountTimer(18.2, 423062, nil, nil, nil, 3)
-local timerSacredPyreCD						= mod:NewAITimer(33.9, 446368, nil, nil, nil, 5, nil, DBM_COMMON_L.MYTHIC_ICON)
+local timerCastigatorsShieldCD				= mod:NewCDCountTimer(30.3, 423015, nil, nil, nil, 3)
+local timerBurningLightCD					= mod:NewCDCountTimer(40.0, 423051, nil, nil, nil, 4, nil, DBM_COMMON_L.INTERRUPT_ICON)
+local timerHammerofPurityCD					= mod:NewCDCountTimer(30.3, 423062, nil, nil, nil, 3)
+local timerSacredPyreCD						= mod:NewCDCountTimer(38.8, 446368, nil, nil, nil, 5, nil, DBM_COMMON_L.MYTHIC_ICON)
 
 --local castsPerGUID = {}
 
@@ -50,6 +55,8 @@ mod.vb.shieldCount = 0
 mod.vb.burningCount = 0
 mod.vb.hammerCount = 0
 mod.vb.pyreCount = 0
+mod.vb.flamesRemaining = 3
+mod.vb.wrathActive = false
 
 function mod:OnCombatStart(delay)
 	self.vb.wrathCount = 0
@@ -57,12 +64,14 @@ function mod:OnCombatStart(delay)
 	self.vb.burningCount = 0
 	self.vb.hammerCount = 0
 	self.vb.pyreCount = 0
+	self.vb.flamesRemaining = 3
+	self.vb.wrathActive = false
 	timerHammerofPurityCD:Start(7.4-delay, 1)
-	timerBurningLightCD:Start(16.7-delay, 1)
+	timerBurningLightCD:Start(21.5-delay, 1)
 	timerCastigatorsShieldCD:Start(23.0-delay, 1)
-	timerVindictiveWrathCD:Start(45.9-delay, 1)
+	timerVindictiveWrathCD:Start(31.3-delay, 1)
 	if self:IsMythic() then
-		timerSacredPyreCD:Start(1-delay)
+		timerSacredPyreCD:Start(15.4-delay, 1)
 	end
 end
 
@@ -73,19 +82,25 @@ end
 function mod:SPELL_CAST_START(args)
 	local spellId = args.spellId
 	if spellId == 422969 then
+		self.vb.wrathActive = true
 		self.vb.wrathCount = self.vb.wrathCount + 1
 		warnVindictiveWrath:Show(self.vb.wrathCount)
 		timerVindictiveWrathCD:Start(nil, self.vb.wrathCount+1)
 	elseif spellId == 423051 or spellId == 446657 then--regular, empowered
-		--16.7, 35.2, 36.4, 35.2, 34.0
 		self.vb.burningCount = self.vb.burningCount + 1
 		specWarnBurningLight:Show(args.sourceName, self.vb.burningCount)
 		specWarnBurningLight:Play("kickcast")
 		timerBurningLightCD:Start(nil, self.vb.burningCount+1)
 	elseif spellId == 446368 then
+		self.vb.flamesRemaining = self.vb.wrathActive and 5 or 3
 		self.vb.pyreCount = self.vb.pyreCount + 1
 		warnSacredPyre:Show(self.vb.pyreCount)
-		timerSacredPyreCD:Start()
+		timerSacredPyreCD:Start(nil, self.vb.pyreCount+1)
+	elseif spellId == 423062 or spellId == 446598 then--regular, empowered
+		self.vb.hammerCount = self.vb.hammerCount + 1
+		specWarnHammerofPurity:Show(self.vb.hammerCount)
+		specWarnHammerofPurity:Play("watchstep")
+		timerHammerofPurityCD:Start(nil, self.vb.hammerCount+1)
 	end
 end
 
@@ -98,15 +113,32 @@ function mod:SPELL_CAST_SUCCESS(args)
 end
 --]]
 
---[[
 function mod:SPELL_AURA_APPLIED(args)
 	local spellId = args.spellId
-	if spellId == 423015 or spellId == 446649 then--Regular, empowered
-
+	if spellId == 446403 then
+		self.vb.flamesRemaining = self.vb.flamesRemaining - 1
+		warnFlamesLeft:Cancel()
+		warnFlamesLeft:Schedule(1, self.vb.flamesRemaining)
 	end
 end
---mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
---]]
+mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
+
+function mod:SPELL_MISSED(_, _, _, _, _, _, _, _, spellId)
+	--"<1397.99 19:10:52> [CLEU] SPELL_MISSED##nil#Player-5764-0040B100#Impyr#446403#Sacrificial Flame",
+	if spellId == 446403 then
+		self.vb.flamesRemaining = self.vb.flamesRemaining - 1
+		warnFlamesLeft:Cancel()
+		warnFlamesLeft:Schedule(1, self.vb.flamesRemaining)
+	end
+end
+
+function mod:SPELL_AURA_REMOVED(args)
+	local spellId = args.spellId
+	if spellId == 422969 then--Regular, empowered
+		self.vb.wrathActive = false
+		warnVindictiveWrathFades:Show()
+	end
+end
 
 --[[
 function mod:SPELL_PERIODIC_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId, spellName)
@@ -130,13 +162,7 @@ end
 --NOTE, hammer of purity has a CLEU event now but i need to do more data verification first to see if it's usable
 --NOTE, Castigator shield has a CLEU event now, but it isn't always cast, so it has to use this for cast/timer since even if it's not cast, it incurs a CD
 function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, spellId)
-	if spellId == 446587 then
-		--"Hammer of Purity-446587-npc:207939-000065268F = pull:8.2, 18.2, 24.3, 19.4, 20.6, 23.1, 21.8, 19.4, 23.1"
-		self.vb.hammerCount = self.vb.hammerCount + 1
-		specWarnHammerofPurity:Show(self.vb.hammerCount)
-		specWarnHammerofPurity:Play("watchstep")
-		timerHammerofPurityCD:Start(nil, self.vb.hammerCount+1)
-	elseif spellId == 446645 then
+	if spellId == 446645 then
 		--"Castigator's Shield-446645-npc:207939-000065268F = pull:25.2, 24.3, 23.1, 25.5, 26.7, 23.0, 25.5",
 		self.vb.shieldCount = self.vb.shieldCount + 1
 		warnCastigatorsShield:Show(self.vb.shieldCount)
--- DBM-Party-WarWithin/PrioryoftheSacredFlame/CaptainDailcry.lua
+++ DBM-Party-WarWithin/PrioryoftheSacredFlame/CaptainDailcry.lua
@@ -4,7 +4,7 @@ local L		= mod:GetLocalizedStrings()
 mod:SetRevision("@file-date-integer@")
 mod:SetCreatureID(207946)
 mod:SetEncounterID(2847)
-mod:SetHotfixNoticeRev(20240608000000)
+mod:SetHotfixNoticeRev(20250303000000)
 --mod:SetMinSyncRevision(20211203000000)
 mod:SetZone(2649)
 --mod.respawnTime = 29
@@ -14,8 +14,9 @@ mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
 	"SPELL_CAST_START 424419 447270 424414",
-	"SPELL_CAST_SUCCESS 424414",
+	"SPELL_CAST_SUCCESS 424414 447443",
 	"SPELL_AURA_APPLIED 447443 447439 424419",
+	"SPELL_AURA_APPLIED_DOSE 424419",
 	"SPELL_AURA_REMOVED 447443"
 --	"SPELL_PERIODIC_DAMAGE",
 --	"SPELL_PERIODIC_MISSED"
@@ -36,14 +37,14 @@ local warnSavageMauling						= mod:NewTargetNoFilterAnnounce(447439, 3)
 
 local specWarnBattleCry						= mod:NewSpecialWarningInterruptCount(424419, nil, nil, nil, 1, 2)
 local specWarnBattleCryDispel				= mod:NewSpecialWarningDispel(424419, "RemoveEnrage", nil, nil, 1, 2)
-local specWarnHurlSpear						= mod:NewSpecialWarningDodgeCount(447272, nil, nil, nil, 2, 2)
+local specWarnHurlSpear						= mod:NewSpecialWarningDodgeCount(447270, nil, nil, nil, 2, 2)
 local specWarnPierceArmor					= mod:NewSpecialWarningDefensive(424414, nil, nil, nil, 1, 2)
 --local specWarnGTFO						= mod:NewSpecialWarningGTFO(372820, nil, nil, nil, 1, 8)
 
-local timerSavageMaulingCD					= mod:NewCDCountTimer(15.7, 447439, nil, nil, nil, 3)--It's either 25.7 from last ones REMOVAL, or ~30.3 from last ones application
-local timerBattleCryCD						= mod:NewVarCountTimer("v15.8-18.2", 424419, nil, nil, nil, 4, nil, DBM_COMMON_L.INTERRUPT_ICON)--15.8-18.2
-local timerHurlSpearCD						= mod:NewVarCountTimer("v15.8-19.4", 447272, nil, nil, nil, 3)--15.8-19.4
-local timerPierceArmorCD					= mod:NewCDCountTimer(7.3, 424414, nil, nil, nil, 5, nil, DBM_COMMON_L.TANK_ICON)
+local timerSavageMaulingCD					= mod:NewCDCountTimer(25.1, 447439, nil, nil, nil, 3)
+local timerBattleCryCD						= mod:NewVarCountTimer("v26.7-28.4", 424419, nil, nil, nil, 4, nil, DBM_COMMON_L.INTERRUPT_ICON)--15.8-18.2
+local timerHurlSpearCD						= mod:NewVarCountTimer("v30.3-34.9", 447270, nil, nil, nil, 3)
+local timerPierceArmorCD					= mod:NewVarCountTimer("v13.3-23", 424414, nil, nil, nil, 5, nil, DBM_COMMON_L.TANK_ICON)
 
 mod:AddInfoFrameOption(447443)
 
@@ -58,9 +59,9 @@ function mod:OnCombatStart(delay)
 	self.vb.spearCount = 0
 	self.vb.pierceCount = 0
 	timerPierceArmorCD:Start(6-delay, 1)
-	timerHurlSpearCD:Start(8.4-delay, 1)
-	timerBattleCryCD:Start(12-delay, 1)
-	timerSavageMaulingCD:Start(14.4-delay, 1)
+	timerHurlSpearCD:Start(9.7-delay, 1)
+	timerBattleCryCD:Start(14.9-delay, 1)
+	timerSavageMaulingCD:Start(13.3-delay, 1)
 end
 
 function mod:OnCombatEnd()
@@ -93,15 +94,16 @@ function mod:SPELL_CAST_SUCCESS(args)
 	local spellId = args.spellId
 	if spellId == 424414 then
 		self.vb.pierceCount = self.vb.pierceCount + 1
-		timerPierceArmorCD:Start(5.8, self.vb.pierceCount+1)--Doesn't go on CD unless it finishes, can stutter cast (7.3 - 1.5)
+		timerPierceArmorCD:Start("v10.8-21.5", self.vb.pierceCount+1)--("v13.3-23") - 2.5
+	elseif spellId == 447443 then
+		self.vb.savageCount = self.vb.savageCount + 1
+		--timerSavageMaulingCD:Start(nil, self.vb.savageCount+1)
 	end
 end
 
 function mod:SPELL_AURA_APPLIED(args)
 	local spellId = args.spellId
 	if spellId == 447443 then
-		self.vb.savageCount = self.vb.savageCount + 1
-		--timerSavageMaulingCD:Start(nil, self.vb.savageCount+1)
 		if self.Options.InfoFrame then
 			DBM.InfoFrame:SetHeader(args.spellName)
 			DBM.InfoFrame:Show(2, "enemyabsorb", nil, args.amount, "boss1")
@@ -111,14 +113,18 @@ function mod:SPELL_AURA_APPLIED(args)
 	elseif spellId == 424419 then
 		specWarnBattleCryDispel:CombinedShow(0.3, args.destName)
 		specWarnBattleCryDispel:ScheduleVoice(0.3, "enrage")
+		local cid = self:GetCIDFromGUID(args.destGUID)
+		if cid == 207946 then--Captain Dailcry
+			timerSavageMaulingCD:RemoveTime(12.5, self.vb.savageCount+1)
+		end
 	end
 end
---mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
+mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
 
 function mod:SPELL_AURA_REMOVED(args)
 	local spellId = args.spellId
 	if spellId == 447443 then
-		timerSavageMaulingCD:Start(nil, self.vb.savageCount+1)--15 after last one now?
+		timerSavageMaulingCD:Start(nil, self.vb.savageCount+1)
 		if self.Options.InfoFrame then
 			DBM.InfoFrame:Hide()
 		end
@@ -133,25 +139,4 @@ function mod:SPELL_PERIODIC_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId, spell
 	end
 end
 mod.SPELL_PERIODIC_MISSED = mod.SPELL_PERIODIC_DAMAGE
---]]
-
---[[
-function mod:UNIT_DIED(args)
-	local cid = self:GetCIDFromGUID(args.destGUID)
-	if cid == 211291 then--sergeant-shaynemail
-
-	elseif cid == 211289 then--taener-duelmal
-
-	elseif cid == 211290 then--elaena-emberlanz
-
-	end
-end
---]]
-
---[[
-function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, spellId)
-	if spellId == 74859 then
-
-	end
-end
 --]]
