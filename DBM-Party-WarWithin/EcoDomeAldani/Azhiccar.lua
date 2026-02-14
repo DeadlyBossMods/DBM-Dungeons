c1584f9666b4aa7570eae848e5411ff2a97d8ee3
MysticalOS <mysticalosx@gmail.com>
2026-02-13 23:30:04 -0500

    Update Eco Dome for midnight apis

--- DBM-Party-WarWithin/EcoDomeAldani/Azhiccar.lua
+++ DBM-Party-WarWithin/EcoDomeAldani/Azhiccar.lua
@@ -12,120 +12,26 @@ mod.sendMainBossGUID = true
 
 mod:RegisterCombat("combat")
 
+--Custom Sounds on cast/cooldown expiring
+mod:AddCustomAlertSoundOption(1217327, "Dps", 1)--Invading Shriek
+mod:AddCustomAlertSoundOption(1217232, true, 2)--Devour
+--custom timer colors, countdowns, and disables
+mod:AddCustomTimerOptions(1217327, nil, 1, 0)--Invading Shriek
+mod:AddCustomTimerOptions(1227748, nil, 3, 0)--Toxic Regurgitation
+mod:AddCustomTimerOptions(1217232, nil, 2, 0)--Devour
 --Midnight private aura replacements
-mod:AddPrivateAuraSoundOption(1217446, true, 1217446, 1)--GTFO
+mod:AddPrivateAuraSoundOption(1227748, true, 1227748, 1)--Toxic Regurgitation Target
+mod:AddPrivateAuraSoundOption(1217439, true, 1217446, 1)--GTFO
 
 function mod:OnLimitedCombatStart()
-	self:EnablePrivateAuraSound(1217446, "watchfeet", 8)
+	self:DisableSpecialWarningSounds()
+	self:EnableAlertOptions(1217327, 2, "killmob", 1)
+	self:EnableAlertOptions(1217232, 461, "pullin", 2)
+
+	self:EnableTimelineOptions(1217327, 2)
+	self:EnableTimelineOptions(1227748, 460)
+	self:EnableTimelineOptions(1217232, 461)
+
+	self:EnablePrivateAuraSound(1227748, "runout", 2)
+	self:EnablePrivateAuraSound(1217439, "watchfeet", 8)
 end
-
---[[
-mod:RegisterEventsInCombat(
-	"SPELL_CAST_START 1217232 1217327 1227745",
-	"SPELL_CAST_SUCCESS 1217232",
-	"SPELL_AURA_APPLIED 1217247",
-	"SPELL_AURA_APPLIED_DOSE 1217247",
-	"SPELL_PERIODIC_DAMAGE 1217446",
-	"SPELL_PERIODIC_MISSED 1217446",
-	"RAID_BOSS_WHISPER"
-)
---]]
-
---[[
-
- or type = "dungeonencounterstart" or type = "dungeonencounterend"
---]]
---[[
-local warnToxicRegurgitation			= mod:NewTargetNoFilterAnnounce(1227745, 3)
-local warnFeastStack					= mod:NewStackAnnounce(1217247, 4)
-
-local specWarnDevour					= mod:NewSpecialWarningCount(1217232, nil, nil, nil, 2, 12)
-local specWarnInvadingShriek			= mod:NewSpecialWarningSwitchCount(1217327, "Dps", nil, nil, 1, 2)
-local specWarnToxicRegurgitation		= mod:NewSpecialWarningMoveAwayCount(1227745, nil, nil, nil, 1, 2)
-local yellToxicRegurgitation			= mod:NewShortYell(1227745)
-local yellToxicRegurgitationFades		= mod:NewShortFadesYell(1227745)
-local specWarnGTFO						= mod:NewSpecialWarningGTFO(1217446, nil, nil, nil, 1, 8)
-
-local timerDevourCD						= mod:NewCDCountTimer(86.1, 1217232, nil, nil, nil, 2)
-local timerDevourCast					= mod:NewCastTimer(18, 1217232, nil, nil, nil, 5)
-local timerInvadingShriekCD				= mod:NewCDCountTimer(30.1, 1217327, nil, nil, nil, 1)
-local timerToxicRegurgitationCD			= mod:NewCDCountTimer(30.1, 1227745, nil, nil, nil, 3)
-
-mod.vb.devourCount = 0
-mod.vb.invadingShriekCount = 0
-mod.vb.toxicRegurgitationCount = 0
-
-function mod:OnCombatStart(delay)
-	self.vb.devourCount = 0
-	self.vb.invadingShriekCount = 0
-	self.vb.toxicRegurgitationCount = 0
-	timerInvadingShriekCD:Start(5.1-delay, 1)
-	timerToxicRegurgitationCD:Start(15.4-delay, 1)
-	timerDevourCD:Start(60-delay, 1)
-end
-
-function mod:SPELL_CAST_START(args)
-	local spellId = args.spellId
-	if spellId == 1217232 then
-		self.vb.devourCount = self.vb.devourCount + 1
-		specWarnDevour:Show(self.vb.devourCount)
-		specWarnDevour:Play("pullin")
-		timerDevourCD:Start(nil, self.vb.devourCount+1)
-	elseif spellId == 1217327 then
-		self.vb.invadingShriekCount = self.vb.invadingShriekCount + 1
-		specWarnInvadingShriek:Show(self.vb.invadingShriekCount)
-		specWarnInvadingShriek:Play("killmob")
-		if self.vb.invadingShriekCount % 2 == 0 then
-			timerInvadingShriekCD:Start(47.3, self.vb.invadingShriekCount+1)
-		else
-			timerInvadingShriekCD:Start(37.2, self.vb.invadingShriekCount+1)
-		end
-	elseif spellId == 1227745 then
-		self.vb.toxicRegurgitationCount = self.vb.toxicRegurgitationCount + 1
-		if self.vb.toxicRegurgitationCount % 2 == 0 then
-			timerToxicRegurgitationCD:Start(67.9, self.vb.toxicRegurgitationCount+1)
-		else
-			timerToxicRegurgitationCD:Start(18.2, self.vb.toxicRegurgitationCount+1)
-		end
-	end
-end
-
-function mod:SPELL_CAST_SUCCESS(args)
-	local spellId = args.spellId
-	if spellId == 1217232 then
-		timerDevourCast:Start()
-	end
-end
-
-function mod:SPELL_AURA_APPLIED(args)
-	local spellId = args.spellId
-	if spellId == 1217247 then
-		warnFeastStack:Show(args.destName, args.amount or 1)
-	end
-end
-mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
-
-function mod:SPELL_PERIODIC_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId, spellName)
-	if spellId == 1217446 and destGUID == UnitGUID("player") and self:AntiSpam(3, 1) then
-		specWarnGTFO:Show(spellName)
-		specWarnGTFO:Play("watchfeet")
-	end
-end
-mod.SPELL_PERIODIC_MISSED = mod.SPELL_PERIODIC_DAMAGE
-
-function mod:RAID_BOSS_WHISPER(msg)
-	if msg:find("spell:1227748") then
-		specWarnToxicRegurgitation:Show(self.vb.toxicRegurgitationCount)
-		specWarnToxicRegurgitation:Play("runout")
-		yellToxicRegurgitation:Yell()
-		yellToxicRegurgitationFades:Countdown(6)--6 On Mythic 0, maybe lower on M+? (tooltip says 8 btw, typical blizzard)
-	end
-end
-
-function mod:OnTranscriptorSync(msg, targetName)
-	if msg:find("1227748") and targetName and self:AntiSpam(5, targetName) then
-		targetName = Ambiguate(targetName, "none")
-		warnToxicRegurgitation:CombinedShow(0.5, targetName)
-	end
-end
---]]
--- DBM-Party-WarWithin/EcoDomeAldani/SoulScribe.lua
+++ DBM-Party-WarWithin/EcoDomeAldani/SoulScribe.lua
@@ -12,94 +12,31 @@ mod.sendMainBossGUID = true
 
 mod:RegisterCombat("combat")
 
+--Custom Sounds on cast/cooldown expiring
+mod:AddCustomAlertSoundOption(1224793, true, 1)--Whispers of Fate
+mod:AddCustomAlertSoundOption(1225174, true, 2)--Ceremonial Daggers
+mod:AddCustomAlertSoundOption(1236703, true, 2)--Eternal Weave
+--custom timer colors, countdowns, and disables
+mod:AddCustomTimerOptions(1224793, nil, 5, 0)--Whispers of Fate
+mod:AddCustomTimerOptions(1225174, nil, 3, 0)--Ceremonial Daggers
+mod:AddCustomTimerOptions(1225218, nil, 3, 0)--Dread of the Unknown
+mod:AddCustomTimerOptions(1236703, nil, 6, 0)--Eternal Weave
+--Midnight private aura replacements
 mod:AddPrivateAuraSoundOption(1226444, true, 1226444, 1)
+mod:AddPrivateAuraSoundOption(1225221, true, 1225221, 1)
 
 function mod:OnLimitedCombatStart()
+	self:DisableSpecialWarningSounds()
+
+	self:EnableAlertOptions(1224793, 544, "ghostsoon", 2)
+	self:EnableAlertOptions(1225174, 546, "ghostsoon", 2)
+	self:EnableAlertOptions(1236703, 548, "phasechange", 2)
+
+	self:EnableTimelineOptions(1224793, 544)
+	self:EnableTimelineOptions(1225174, 546)
+	self:EnableTimelineOptions(1225218, 547)
+	self:EnableTimelineOptions(1236703, 548)
+
 	self:EnablePrivateAuraSound(1226444, "targetyou", 2)
+	self:EnablePrivateAuraSound(1225221, "runout", 2)
 end
-
---[[
-mod:RegisterEventsInCombat(
-	"SPELL_CAST_START 1224793 1236703 1225218 1225174",
-	"SPELL_AURA_APPLIED 1224865 1226444",
-	"SPELL_AURA_APPLIED_DOSE 1224865"
-)
---]]
-
---[[
-
- or type = "dungeonencounterstart" or type = "dungeonencounterend"
---]]
---[[
-local warnWhispersofFate				= mod:NewCountAnnounce(1224793, 3)
-local warnFatebound						= mod:NewCountAnnounce(1224865, 3, nil, nil, DBM_CORE_L.AUTO_ANNOUNCE_OPTIONS.stack:format(1224865))
-
-local specWarnWoundedFate				= mod:NewSpecialWarningYou(1226444, nil, nil, nil, 1, 2)
-local specWarnEternalWeave				= mod:NewSpecialWarningCount(1236703, nil, nil, nil, 2, 2)
-local specWarnDreadoftheUnknown			= mod:NewSpecialWarningCount(1225218, nil, nil, nil, 2, 2)--Redo audio when mechanic clear
-local specWarnCeremonialDaggers			= mod:NewSpecialWarningCount(1225174, nil, nil, nil, 2, 2)
-
-local timerWhispersofFateCD				= mod:NewCDCountTimer(30, 1224793, nil, nil, nil, 5)
-local timerEternalWeaveCD				= mod:NewCDCountTimer(87.5, 1236703, nil, nil, nil, 6)
-local timerDreadoftheUnknownCD			= mod:NewCDCountTimer(87.5, 1225218, nil, nil, nil, 2)
-local timerCeremonialDaggersCD			= mod:NewCDCountTimer(87.5, 1225174, nil, nil, nil, 3)
-
-mod.vb.fateCount = 0
-mod.vb.weaveCount = 0
-mod.vb.dreadCount = 0
-mod.vb.daggerCount = 0
-
-function mod:OnCombatStart(delay)
-	self.vb.fateCount = 0
-	self.vb.weaveCount = 0
-	self.vb.dreadCount = 0
-	self.vb.daggerCount = 0
-	timerWhispersofFateCD:Start(6.1-delay, 1)
-	timerCeremonialDaggersCD:Start(9.6-delay, 1)
-	timerDreadoftheUnknownCD:Start(27.8-delay, 1)
-	timerEternalWeaveCD:Start(56.2-delay, 1)
-end
-
-function mod:SPELL_CAST_START(args)
-	local spellId = args.spellId
-	if spellId == 1224793 then
-		self.vb.fateCount = self.vb.fateCount + 1
-		warnWhispersofFate:Show(self.vb.fateCount)
-		if self.vb.fateCount % 3 == 0 then
-			timerWhispersofFateCD:Start(51, self.vb.fateCount+1)
-		else
-			timerWhispersofFateCD:Start(18.2, self.vb.fateCount+1)
-		end
-	elseif spellId == 1236703 then
-		self.vb.weaveCount = self.vb.weaveCount + 1
-		specWarnEternalWeave:Show(self.vb.weaveCount)
-		specWarnEternalWeave:Play("phasechange")
-		timerEternalWeaveCD:Start(nil, self.vb.weaveCount+1)
-	elseif spellId == 1225218 then
-		self.vb.dreadCount = self.vb.dreadCount + 1
-		specWarnDreadoftheUnknown:Show(self.vb.dreadCount)
-		specWarnDreadoftheUnknown:Play("ghostsoon")
-		timerDreadoftheUnknownCD:Start(nil, self.vb.dreadCount+1)
-	elseif spellId == 1225174 then
-		self.vb.daggerCount = self.vb.daggerCount + 1
-		specWarnCeremonialDaggers:Show(self.vb.daggerCount)
-		specWarnCeremonialDaggers:Play("ghostsoon")
-		if self.vb.daggerCount % 2 == 0 then
-			timerCeremonialDaggersCD:Start(51, self.vb.daggerCount+1)
-		else
-			timerCeremonialDaggersCD:Start(36.4, self.vb.daggerCount+1)
-		end
-	end
-end
-
-function mod:SPELL_AURA_APPLIED(args)
-	local spellId = args.spellId
-	if spellId == 1224865 and args:IsPlayer() then
-		warnFatebound:Show(args.amount or 1)
-	elseif spellId == 1226444 and args:IsPlayer() then
-		specWarnWoundedFate:Show()
-		specWarnWoundedFate:Play("targetyou")
-	end
-end
-mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
---]]
