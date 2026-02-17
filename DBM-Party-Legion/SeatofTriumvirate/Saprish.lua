7b2d9620f4cb60e779d16d80f21ec4ea5f3bf7cf
MysticalOS <mysticalosx@gmail.com>
2026-02-17 00:35:53 -0500

    Update Seat for midnight season 1

--- DBM-Party-Legion/SeatofTriumvirate/Saprish.lua
+++ DBM-Party-Legion/SeatofTriumvirate/Saprish.lua
@@ -7,107 +7,141 @@ mod:SetEncounterID(2066)
 
 mod:RegisterCombat("combat")
 
---TODO, some actual custom sounds and timer disables when apis added
-if DBM:IsPostMidnight() then return end
+if DBM:IsPostMidnight() then
+	--Custom Sounds on cast/cooldown expiring
+	mod:AddCustomAlertSoundOption(247175, true, 1)--Void Bomb
+	mod:AddCustomAlertSoundOption(248831, true, 2)--Dread Screech
+	mod:AddCustomAlertSoundOption(1263523, true, 2)--Overload
+	--Custom timer colors, countdowns, and disables
+	mod:AddCustomTimerOptions(247175, true, 3, 0)
+	mod:AddCustomTimerOptions(1280064, true, 3, 0)
+	mod:AddCustomTimerOptions(248831, true, 2, 0)
+	mod:AddCustomTimerOptions(245738, true, 3, 0)
+	mod:AddCustomTimerOptions(1263523, true, 2, 0)
+	--Midnight private aura replacements
+	mod:AddPrivateAuraSoundOption(1280064, true, 1280064, 1)--Phase Dash
+	mod:AddPrivateAuraSoundOption(245742, true, 245742, 2)--Shadow Pounce
 
-mod:RegisterEventsInCombat(
-	"SPELL_CAST_START 245802 248831",
-	"SPELL_CAST_SUCCESS 247245",
-	"SPELL_AURA_APPLIED 247245",
---	"SPELL_AURA_REMOVED 247245",
-	"UNIT_SPELLCAST_SUCCEEDED boss1"
-)
-
---TODO, see if swoop/screech target can be identified
---Void Hunter
-local warnUmbralFlanking				= mod:NewTargetAnnounce(247245, 3)
-local warnVoidTrap						= mod:NewSpellAnnounce(246026, 3, nil, nil, nil, nil, nil, 2)
---local warnDreadScreech					= mod:NewCastAnnounce(248831, 2)
-
---local specWarnHuntersRush				= mod:NewSpecialWarningDefensive(247145, nil, nil, nil, 1, 2)
-local specWarnOverloadTrap				= mod:NewSpecialWarningDodge(247206, nil, nil, nil, 2, 2)
-local specWarnUmbralFlanking			= mod:NewSpecialWarningMoveAway(247245, nil, nil, nil, 1, 2)
-local yellUmbralFlanking				= mod:NewYell(247245)
-local specWarnRavagingDarkness			= mod:NewSpecialWarningDodge(245802, nil, nil, nil, 2, 2)
-local specWarnDreadScreech				= mod:NewSpecialWarningInterrupt(248831, "HasInterrupt", nil, nil, 1, 2)
-
-local timerVoidTrapCD					= mod:NewCDTimer(15.8, 246026, nil, nil, nil, 3)
-local timerOverloadTrapCD				= mod:NewCDTimer(20.6, 247206, nil, nil, nil, 3)
-local timerRavagingDarknessCD			= mod:NewCDTimer(8.8, 245802, nil, nil, nil, 3)
-local timerUmbralFlankingCD				= mod:NewCDTimer(35.2, 247245, nil, nil, nil, 3)
-local timerScreechCD					= mod:NewCDTimer(15.4, 248831, nil, nil, nil, 3, nil, DBM_COMMON_L.HEROIC_ICON)
-
-function mod:OnCombatStart(delay)
-	timerRavagingDarknessCD:Start(5.5-delay)
-	timerVoidTrapCD:Start(8.8-delay)
-	timerOverloadTrapCD:Start(12.5-delay)
-	timerUmbralFlankingCD:Start(20.4-delay)
-	if self:IsHard() then
-		--Stuff
-		timerScreechCD:Start(6.2-delay)
-	end
-end
-
-function mod:SPELL_CAST_START(args)
-	local spellId = args.spellId
-	if spellId == 245802 then
-		specWarnRavagingDarkness:Show()
-		specWarnRavagingDarkness:Play("watchstep")
-		timerRavagingDarknessCD:Start()
-	elseif spellId == 248831 then
-		specWarnDreadScreech:Show(args.sourceName)
-		specWarnDreadScreech:Play("kickcast")
-		timerScreechCD:Start()
-	end
-end
-
-function mod:SPELL_CAST_SUCCESS(args)
-	local spellId = args.spellId
-	if spellId == 247245 then
-		timerUmbralFlankingCD:Start()
-	end
-end
-
-function mod:SPELL_AURA_APPLIED(args)
-	local spellId = args.spellId
-	if spellId == 247245 then
-		warnUmbralFlanking:CombinedShow(0.3, args.destName)
-		if args:IsPlayer() then
-			specWarnUmbralFlanking:Show()
-			specWarnUmbralFlanking:Play("scatter")
-			yellUmbralFlanking:Yell()
+	function mod:OnLimitedCombatStart()
+		self:DisableSpecialWarningSounds()
+		self:EnableAlertOptions(247175, 234, "bombsoon", 2)
+		if self:IsSpellCaster() then
+			self:EnableAlertOptions(248831, 236, "stopcast", 2)
+		else
+			self:EnableAlertOptions(248831, 236, "aesoon", 2)
 		end
---	elseif spellId == 247145 then
---		if self:IsTanking("player", "boss1", nil, true) then
---			specWarnHuntersRush:Show()
---			specWarnHuntersRush:Play("defensive")
---		end
+		self:EnableAlertOptions(1263523, 243, "aesoon", 2)
+
+
+		self:EnableTimelineOptions(247175, 234)
+		self:EnableTimelineOptions(1280064, 235)
+		self:EnableTimelineOptions(248831, 236)
+		self:EnableTimelineOptions(245738, 237)
+		self:EnableTimelineOptions(1263523, 243)
+
+		self:EnablePrivateAuraSound(1280064, "lineyou", 17)
+		self:EnablePrivateAuraSound(245742, "targetyou", 2)
 	end
-end
+else
+	mod:RegisterEventsInCombat(
+		"SPELL_CAST_START 245802 248831",
+		"SPELL_CAST_SUCCESS 247245",
+		"SPELL_AURA_APPLIED 247245",
+	--	"SPELL_AURA_REMOVED 247245",
+		"UNIT_SPELLCAST_SUCCEEDED boss1"
+	)
 
---[[
-function mod:SPELL_AURA_REMOVED(args)
-	local spellId = args.spellId
-	if spellId == 247245 then
+	--TODO, see if swoop/screech target can be identified
+	--Void Hunter
+	local warnUmbralFlanking				= mod:NewTargetAnnounce(247245, 3)
+	local warnVoidTrap						= mod:NewSpellAnnounce(246026, 3, nil, nil, nil, nil, nil, 2)
+	--local warnDreadScreech					= mod:NewCastAnnounce(248831, 2)
 
+	--local specWarnHuntersRush				= mod:NewSpecialWarningDefensive(247145, nil, nil, nil, 1, 2)
+	local specWarnOverloadTrap				= mod:NewSpecialWarningDodge(247206, nil, nil, nil, 2, 2)
+	local specWarnUmbralFlanking			= mod:NewSpecialWarningMoveAway(247245, nil, nil, nil, 1, 2)
+	local yellUmbralFlanking				= mod:NewYell(247245)
+	local specWarnRavagingDarkness			= mod:NewSpecialWarningDodge(245802, nil, nil, nil, 2, 2)
+	local specWarnDreadScreech				= mod:NewSpecialWarningInterrupt(248831, "HasInterrupt", nil, nil, 1, 2)
+
+	local timerVoidTrapCD					= mod:NewCDTimer(15.8, 246026, nil, nil, nil, 3)
+	local timerOverloadTrapCD				= mod:NewCDTimer(20.6, 247206, nil, nil, nil, 3)
+	local timerRavagingDarknessCD			= mod:NewCDTimer(8.8, 245802, nil, nil, nil, 3)
+	local timerUmbralFlankingCD				= mod:NewCDTimer(35.2, 247245, nil, nil, nil, 3)
+	local timerScreechCD					= mod:NewCDTimer(15.4, 248831, nil, nil, nil, 3, nil, DBM_COMMON_L.HEROIC_ICON)
+
+	function mod:OnCombatStart(delay)
+		timerRavagingDarknessCD:Start(5.5-delay)
+		timerVoidTrapCD:Start(8.8-delay)
+		timerOverloadTrapCD:Start(12.5-delay)
+		timerUmbralFlankingCD:Start(20.4-delay)
+		if self:IsHard() then
+			--Stuff
+			timerScreechCD:Start(6.2-delay)
+		end
 	end
-end
 
-function mod:CHAT_MSG_RAID_BOSS_EMOTE(msg)
-	if msg:find("inv_misc_monsterhorn_03") then
-
+	function mod:SPELL_CAST_START(args)
+		local spellId = args.spellId
+		if spellId == 245802 then
+			specWarnRavagingDarkness:Show()
+			specWarnRavagingDarkness:Play("watchstep")
+			timerRavagingDarknessCD:Start()
+		elseif spellId == 248831 then
+			specWarnDreadScreech:Show(args.sourceName)
+			specWarnDreadScreech:Play("kickcast")
+			timerScreechCD:Start()
+		end
 	end
-end
---]]
 
-function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, spellId)
-	if spellId == 247175 then--Void Trap
-		warnVoidTrap:Show()
-		warnVoidTrap:Play("watchstep")
-		timerVoidTrapCD:Start()
-	elseif spellId == 247206 then--Overload Trap
-		specWarnOverloadTrap:Show()
-		specWarnOverloadTrap:Play("watchstep")
-		timerOverloadTrapCD:Start()
+	function mod:SPELL_CAST_SUCCESS(args)
+		local spellId = args.spellId
+		if spellId == 247245 then
+			timerUmbralFlankingCD:Start()
+		end
+	end
+
+	function mod:SPELL_AURA_APPLIED(args)
+		local spellId = args.spellId
+		if spellId == 247245 then
+			warnUmbralFlanking:CombinedShow(0.3, args.destName)
+			if args:IsPlayer() then
+				specWarnUmbralFlanking:Show()
+				specWarnUmbralFlanking:Play("scatter")
+				yellUmbralFlanking:Yell()
+			end
+	--	elseif spellId == 247145 then
+	--		if self:IsTanking("player", "boss1", nil, true) then
+	--			specWarnHuntersRush:Show()
+	--			specWarnHuntersRush:Play("defensive")
+	--		end
+		end
+	end
+
+	--[[
+	function mod:SPELL_AURA_REMOVED(args)
+		local spellId = args.spellId
+		if spellId == 247245 then
+
+		end
+	end
+
+	function mod:CHAT_MSG_RAID_BOSS_EMOTE(msg)
+		if msg:find("inv_misc_monsterhorn_03") then
+
+		end
+	end
+	--]]
+
+	function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, spellId)
+		if spellId == 247175 then--Void Trap
+			warnVoidTrap:Show()
+			warnVoidTrap:Play("watchstep")
+			timerVoidTrapCD:Start()
+		elseif spellId == 247206 then--Overload Trap
+			specWarnOverloadTrap:Show()
+			specWarnOverloadTrap:Play("watchstep")
+			timerOverloadTrapCD:Start()
+		end
 	end
 end
--- DBM-Party-Legion/SeatofTriumvirate/ViceroyNezhar.lua
+++ DBM-Party-Legion/SeatofTriumvirate/ViceroyNezhar.lua
@@ -7,93 +7,122 @@ mod:SetEncounterID(2067)
 
 mod:RegisterCombat("combat")
 
---TODO, some actual custom sounds and timer disables when apis added
-if DBM:IsPostMidnight() then return end
+if DBM:IsPostMidnight() then
+	--NOTE: https://www.wowhead.com/spell=244750/mind-blast is spammed so no sound alert or timer
+	--NOTE: Repulse might be spammy, i think blizzard meant to hook event up to parent ability Collapsing Void
+	--Custom Sounds on cast/cooldown expiring
+	mod:AddCustomAlertSoundOption(1263538, true, 1)--Hentai
+	mod:AddCustomAlertSoundOption(1263528, true, 2)--Repulse
+	mod:AddCustomAlertSoundOption(1277358, true, 2)--Gates of teh Abyss
+	--Custom timer colors, countdowns, and disables
+	mod:AddCustomTimerOptions(1263542, true, 5, 0)
+	mod:AddCustomTimerOptions(1263538, true, 1, 0)
+	mod:AddCustomTimerOptions(1263528, true, 2, 0)
+	mod:AddCustomTimerOptions(1263542, true, 3, 0)
+	--Midnight private aura replacements
+	mod:AddPrivateAuraSoundOption(1263542, false, 1263542, 1)--Mass Void Infusion (just minor rot damage, off by default)
+	mod:AddPrivateAuraSoundOption(1263532, true, 1263532, 1)--Void Storm (GTFO)
 
-mod:RegisterEventsInCombat(
-	"SPELL_CAST_START 244751 248736",
-	"SPELL_CAST_SUCCESS 246324",
-	"SPELL_AURA_APPLIED 248804",
-	"SPELL_AURA_REMOVED 248804",
-	"UNIT_SPELLCAST_SUCCEEDED boss1"
-)
+	function mod:OnLimitedCombatStart()
+		self:DisableSpecialWarningSounds()
+		self:EnableAlertOptions(1263538, 246, "mobsoon", 1)
+		self:EnableAlertOptions(1263528, 247, "carefly", 2)
+		self:EnableAlertOptions(1277358, 376, "watchwave", 2)
 
---TODO, power gain rate consistent?
---TODO, special warning to switch to tentacles once know for sure how to tell empowered apart from non empowered?
---TODO, More work on guard timers, with an english log that's actually captured properly (stared and stopped between pulls)
-local warnEternalTwilight				= mod:NewCastAnnounce(248736, 4)
-local warnAddsLeft						= mod:NewAddsLeftAnnounce(-16424, 2)
-local warnTentacles						= mod:NewSpellAnnounce(244769, 2)
+		self:EnableTimelineOptions(1263542, 245)
+		self:EnableTimelineOptions(1263538, 246)
+		self:EnableTimelineOptions(1263528, 247)
+		self:EnableTimelineOptions(1277358, 376)
 
-local specWarnHowlingDark				= mod:NewSpecialWarningInterrupt(244751, "HasInterrupt", nil, nil, 1, 2)
-local specWarnEntropicForce				= mod:NewSpecialWarningSpell(246324, nil, nil, nil, 1, 2)
-local specWarnAdds						= mod:NewSpecialWarningAdds(249336, "-Healer", nil, nil, 1, 2)
-
-local timerUmbralTentaclesCD			= mod:NewCDTimer(30.4, 244769, nil, nil, nil, 1)
-local timerHowlingDarkCD				= mod:NewCDTimer(28.0, 244751, nil, nil, nil, 4, nil, DBM_COMMON_L.INTERRUPT_ICON)
-local timerEntropicForceCD				= mod:NewCDTimer(28.0, 246324, nil, nil, nil, 2)--28-38
-local timerEternalTwilight				= mod:NewCastTimer(10, 248736, nil, nil, nil, 2, nil, DBM_COMMON_L.DEADLY_ICON, nil, 2, 4)
-local timerAddsCD						= mod:NewAddsTimer(61.9, 249336, nil, "-Healer")
-
-mod.vb.guardsActive = 0
-
-function mod:OnCombatStart(delay)
-	self.vb.guardsActive = 0
-	timerUmbralTentaclesCD:Start(11.8-delay)
-	timerHowlingDarkCD:Start(15.5-delay)
-	timerEntropicForceCD:Start(35-delay)
-	if self:IsHard() then
-		timerAddsCD:Start(53-delay)
+		self:EnablePrivateAuraSound(1263542, "debuffyou", 17)
+		self:EnablePrivateAuraSound(1263532, "watchfeet", 8)
 	end
-end
+else
+	mod:RegisterEventsInCombat(
+		"SPELL_CAST_START 244751 248736",
+		"SPELL_CAST_SUCCESS 246324",
+		"SPELL_AURA_APPLIED 248804",
+		"SPELL_AURA_REMOVED 248804",
+		"UNIT_SPELLCAST_SUCCEEDED boss1"
+	)
 
-function mod:SPELL_CAST_START(args)
-	local spellId = args.spellId
-	if spellId == 244751 then
-		timerHowlingDarkCD:Start()
-		specWarnHowlingDark:Show(args.sourceName)
-		specWarnHowlingDark:Play("kickcast")
-	elseif spellId == 248736 and self:AntiSpam(3, 1) then
-		warnEternalTwilight:Show()
-		timerEternalTwilight:Start()
-	end
-end
+	--TODO, power gain rate consistent?
+	--TODO, special warning to switch to tentacles once know for sure how to tell empowered apart from non empowered?
+	--TODO, More work on guard timers, with an english log that's actually captured properly (stared and stopped between pulls)
+	local warnEternalTwilight				= mod:NewCastAnnounce(248736, 4)
+	local warnAddsLeft						= mod:NewAddsLeftAnnounce(-16424, 2)
+	local warnTentacles						= mod:NewSpellAnnounce(244769, 2)
 
-function mod:SPELL_CAST_SUCCESS(args)
-	local spellId = args.spellId
-	if spellId == 246324 then
-		specWarnEntropicForce:Show()
-		specWarnEntropicForce:Play("keepmove")
-		timerEntropicForceCD:Start()
-	end
-end
+	local specWarnHowlingDark				= mod:NewSpecialWarningInterrupt(244751, "HasInterrupt", nil, nil, 1, 2)
+	local specWarnEntropicForce				= mod:NewSpecialWarningSpell(246324, nil, nil, nil, 1, 2)
+	local specWarnAdds						= mod:NewSpecialWarningAdds(249336, "-Healer", nil, nil, 1, 2)
 
-function mod:SPELL_AURA_APPLIED(args)
-	local spellId = args.spellId
-	if spellId == 248804 then
-		self.vb.guardsActive = self.vb.guardsActive + 1
-	end
-end
+	local timerUmbralTentaclesCD			= mod:NewCDTimer(30.4, 244769, nil, nil, nil, 1)
+	local timerHowlingDarkCD				= mod:NewCDTimer(28.0, 244751, nil, nil, nil, 4, nil, DBM_COMMON_L.INTERRUPT_ICON)
+	local timerEntropicForceCD				= mod:NewCDTimer(28.0, 246324, nil, nil, nil, 2)--28-38
+	local timerEternalTwilight				= mod:NewCastTimer(10, 248736, nil, nil, nil, 2, nil, DBM_COMMON_L.DEADLY_ICON, nil, 2, 4)
+	local timerAddsCD						= mod:NewAddsTimer(61.9, 249336, nil, "-Healer")
 
-function mod:SPELL_AURA_REMOVED(args)
-	local spellId = args.spellId
-	if spellId == 248804 then
-		self.vb.guardsActive = self.vb.guardsActive - 1
-		if self.vb.guardsActive >= 1 then
-			warnAddsLeft:Show(self.vb.guardsActive)
-		--else
-			--Start timer for next guard here if more accurate
+	mod.vb.guardsActive = 0
+
+	function mod:OnCombatStart(delay)
+		self.vb.guardsActive = 0
+		timerUmbralTentaclesCD:Start(11.8-delay)
+		timerHowlingDarkCD:Start(15.5-delay)
+		timerEntropicForceCD:Start(35-delay)
+		if self:IsHard() then
+			timerAddsCD:Start(53-delay)
 		end
 	end
-end
 
-function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, spellId)
-	if spellId == 245038 then
-		warnTentacles:Show()
-		timerUmbralTentaclesCD:Start()
-	elseif spellId == 249336 then--or 249335
-		specWarnAdds:Show()
-		specWarnAdds:Play("killmob")
-		timerAddsCD:Start()
+	function mod:SPELL_CAST_START(args)
+		local spellId = args.spellId
+		if spellId == 244751 then
+			timerHowlingDarkCD:Start()
+			specWarnHowlingDark:Show(args.sourceName)
+			specWarnHowlingDark:Play("kickcast")
+		elseif spellId == 248736 and self:AntiSpam(3, 1) then
+			warnEternalTwilight:Show()
+			timerEternalTwilight:Start()
+		end
+	end
+
+	function mod:SPELL_CAST_SUCCESS(args)
+		local spellId = args.spellId
+		if spellId == 246324 then
+			specWarnEntropicForce:Show()
+			specWarnEntropicForce:Play("keepmove")
+			timerEntropicForceCD:Start()
+		end
+	end
+
+	function mod:SPELL_AURA_APPLIED(args)
+		local spellId = args.spellId
+		if spellId == 248804 then
+			self.vb.guardsActive = self.vb.guardsActive + 1
+		end
+	end
+
+	function mod:SPELL_AURA_REMOVED(args)
+		local spellId = args.spellId
+		if spellId == 248804 then
+			self.vb.guardsActive = self.vb.guardsActive - 1
+			if self.vb.guardsActive >= 1 then
+				warnAddsLeft:Show(self.vb.guardsActive)
+			--else
+				--Start timer for next guard here if more accurate
+			end
+		end
+	end
+
+	function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, spellId)
+		if spellId == 245038 then
+			warnTentacles:Show()
+			timerUmbralTentaclesCD:Start()
+		elseif spellId == 249336 then--or 249335
+			specWarnAdds:Show()
+			specWarnAdds:Play("killmob")
+			timerAddsCD:Start()
+		end
 	end
 end
