fe779a3d1e779280a504f17f8e42535163957f7b
MysticalOS <mysticalosx@gmail.com>
2026-02-23 09:14:32 -0500

    Add midnight support for Maisara Caverns

--- DBM-Party-Midnight/MaisaraCaverns/MurojinandNekraxx.lua
+++ DBM-Party-Midnight/MaisaraCaverns/MurojinandNekraxx.lua
@@ -15,4 +15,39 @@ mod:RegisterCombat("combat")
 
 --)
 
---TODO. Not a damn thing
+--Custom Sounds on cast/cooldown expiring
+mod:AddCustomAlertSoundOption(1266480, true, 1)--Flanking Spear
+mod:AddCustomAlertSoundOption(1243900, true, 2)--Fetid Quillstorm
+mod:AddCustomAlertSoundOption(1243741, true, 2)--Freezing Trap Cast
+mod:AddCustomAlertSoundOption(1246666, "RemoveDisease", 1)--Infected Pinions
+--Custom timer colors, countdowns, and disables
+mod:AddCustomTimerOptions(1266480, true, 5, 0)--Flanking Spear
+mod:AddCustomTimerOptions(1243900, true, 3, 0)--Fetid Quillstorm
+mod:AddCustomTimerOptions(1243741, true, 1, 0)--Freezing Trap Cast
+mod:AddCustomTimerOptions(1260643, true, 3, 0)--Barrage
+mod:AddCustomTimerOptions(1246666, "RemoveDisease", 5, 0)--Infected Pinions
+mod:AddCustomTimerOptions(1249478, true, 3, 0)--Carrion Swoop
+--Midnight private aura replacements
+mod:AddPrivateAuraSoundOption(1243741, true, 1243741, 1, 1)--Freezing Trap Stun
+mod:AddPrivateAuraSoundOption(1260643, true, 1260643, 1, 1)--Barrage
+mod:AddPrivateAuraSoundOption(1249478, true, 1249478, 1, 1)--Carrion Swoop
+
+function mod:OnLimitedCombatStart()
+	if self:IsTank() then
+		self:EnableAlertOptions(1266480, 150, "defensive", 2, 3)
+	end
+	self:EnableAlertOptions(1243900, 151, "watchstep", 2, 2)
+	self:EnableAlertOptions(1243741, 152, "trapsincoming", 19, 2)--has a severity, so type 0 might work
+	self:EnableAlertOptions(1260643, 154, "helpdispel", 2, 3)
+
+	self:EnableTimelineOptions(1266480, 150)
+	self:EnableTimelineOptions(1243900, 151)
+	self:EnableTimelineOptions(1243741, 152)
+	self:EnableTimelineOptions(1260643, 153)
+	self:EnableTimelineOptions(1246666, 154)
+	self:EnableTimelineOptions(1249478, 155)
+
+	self:EnablePrivateAuraSound(1243741, "stunyou", 19)
+	self:EnablePrivateAuraSound(1260643, "frontalyou", 19)
+	self:EnablePrivateAuraSound(1249478, "behindice", 19)
+end
--- DBM-Party-Midnight/MaisaraCaverns/Raktul.lua
+++ DBM-Party-Midnight/MaisaraCaverns/Raktul.lua
@@ -15,4 +15,28 @@ mod:RegisterCombat("combat")
 
 --)
 
---TODO. Not a damn thing
+--NOTE, Deathgorged Vessel lacks an eventID (or it's one of 0's and needs to be located)
+--Custom Sounds on cast/cooldown expiring
+mod:AddCustomAlertSoundOption(1251023, true, 1)--Spiritbreaker
+mod:AddCustomAlertSoundOption(1253788, true, 2)--Soulrending roar
+--Custom timer colors, countdowns, and disables
+mod:AddCustomTimerOptions(1251023, true, 5, 0)--Spiritbreaker
+mod:AddCustomTimerOptions(1252675, true, 3, 0)--Crush Souls
+mod:AddCustomTimerOptions(1253788, true, 6, 0)--Soulrending roar
+--Midnight private aura replacements
+mod:AddPrivateAuraSoundOption(1252675, true, 1252675, 1, 1)--Crush Souls
+mod:AddPrivateAuraSoundOption(1253779, true, 1253779, 1, 2)--Spectral Decay
+
+function mod:OnLimitedCombatStart()
+	if self:IsTank() then
+		self:EnableAlertOptions(1251023, 156, "defensive", 2, 3)
+	end
+	self:EnableAlertOptions(1253788, 158, "phasechange", 2, 2)--change when things are much clearer
+
+	self:EnableTimelineOptions(1251023, 156)
+	self:EnableTimelineOptions(1252675, 157)
+	self:EnableTimelineOptions(1253788, 158)
+
+	self:EnablePrivateAuraSound(1252675, "leapyou", 19)
+	self:EnablePrivateAuraSound(1253779, "watchfeet", 8)
+end
