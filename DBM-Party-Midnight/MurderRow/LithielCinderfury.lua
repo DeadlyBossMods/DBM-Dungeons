83e029ba467436df67690ee85b9b465813e15af7
MysticalOS <mysticalosx@gmail.com>
2026-03-03 20:14:47 -0500

    audio improvements on Cinderfury

--- DBM-Party-Midnight/MurderRow/LithielCinderfury.lua
+++ DBM-Party-Midnight/MurderRow/LithielCinderfury.lua
@@ -29,9 +29,9 @@ mod:AddCustomTimerOptions(1224478, true, 6, 0)--Malefic Wave
 --Midnight private aura replacements
 
 function mod:OnLimitedCombatStart()
-	self:EnableAlertOptions(1218203, 37, "watchstep", 2, 2)--Or change to mobssoon?
+	self:EnableAlertOptions(1218203, 37, "scatter", 2, 2)--Or change to mobssoon?
 	self:EnableAlertOptions(474408, 38, "bigmob", 1, 2)
-	self:EnableAlertOptions(1224478, 207, "specialsoon", 2, 2)--Has no timer or text warning?
+	self:EnableAlertOptions(1224478, 207, "usegate", 19, 2)--Has no timer or text warning?
 
 	self:EnableTimelineOptions(1218203, 37)
 	self:EnableTimelineOptions(474408, 38)
