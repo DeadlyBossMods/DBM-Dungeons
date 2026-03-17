f41a12bbb36a0f96746f109f428613b7814411c0
MysticalOS <mysticalosx@gmail.com>
2026-03-16 23:38:53 -0400

    Change Zaen to use timer trigger instead of private aura trigger for barrel LOS alert. Also fixed tank alert sound firing for all players on same fight. Closes https://github.com/DeadlyBossMods/DeadlyBossMods/issues/1973

--- DBM-Party-Midnight/MurderRow/ZaenBladesorrow.lua
+++ DBM-Party-Midnight/MurderRow/ZaenBladesorrow.lua
@@ -19,6 +19,7 @@ mod:RegisterCombat("combat")
 mod:AddCustomAlertSoundOption(474765, true, 2)--Same Day Delivery
 mod:AddCustomAlertSoundOption(474478, true, 2)--Killing Spree
 mod:AddCustomAlertSoundOption(1222795, true, 2)--Envenom
+mod:AddCustomAlertSoundOption(474545, true, 2)--Murder in a Row
 --Custom timer colors, countdowns, and disables
 mod:AddCustomTimerOptions(1214352, true, 3, 0)--Fire Bomb
 mod:AddCustomTimerOptions(474765, true, 3, 0)--Same Day Delivery
@@ -26,13 +27,16 @@ mod:AddCustomTimerOptions(474545, true, 2, 0)--Murder in a Row
 mod:AddCustomTimerOptions(474478, true, 2, 0)--Killing Spree
 mod:AddCustomTimerOptions(1222795, true, 5, 0)--Envenom
 ----Midnight private aura replacements
-mod:AddPrivateAuraSoundOption(474545, true, 474545, 1, 1)--Murder in a Row
+--mod:AddPrivateAuraSoundOption(474545, true, 474545, 1, 1)--Murder in a Row
 mod:AddPrivateAuraSoundOption(1214352, true, 1214352, 1, 1)--Fire Bomb
 
 function mod:OnLimitedCombatStart()
 	self:EnableAlertOptions(474765, 124, "watchstep", 2, 2)
 	self:EnableAlertOptions(474478, 127, "aesoon", 2, 2)
-	self:EnableAlertOptions(1222795, 193, "defensive", 2, 3)
+	if self:IsTank() then
+		self:EnableAlertOptions(1222795, 193, "defensive", 2, 3)
+	end
+	self:EnableAlertOptions(474545, 125, "breaklos", 2, 4)
 
 	self:EnableTimelineOptions(1214352, 123)
 	self:EnableTimelineOptions(474765, 124)
@@ -40,6 +44,6 @@ function mod:OnLimitedCombatStart()
 	self:EnableTimelineOptions(474478, 127)
 	self:EnableTimelineOptions(1222795, 193)
 
-	self:EnablePrivateAuraSound(474545, "breaklos", 12)
+--	self:EnablePrivateAuraSound(474545, "breaklos", 12)
 	self:EnablePrivateAuraSound(1214352, "bombyou", 12)
 end
