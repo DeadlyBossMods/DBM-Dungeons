17b74ac344e6952c357a88b6a1fca75219e994fd
MysticalOS <mysticalosx@gmail.com>
2026-07-19 16:58:06 -0500

    upgrade ravi to hardcode

--- DBM-Party-Midnight/AltarofFangs/Ravi.lua
+++ DBM-Party-Midnight/AltarofFangs/Ravi.lua
@@ -14,22 +14,26 @@ mod:RegisterCombat("combat")
 --https://www.wowhead.com/ptr/spell=1296219/fetid-roar isn't in journal but has encounter event
 --use https://www.wowhead.com/ptr/spell=1297876/triple-shot for spread debuff/aura sound?
 --901 is used for "Fresh Meat" but not sure for what. Announcing it's spawn?
-local warnTripleShot				= mod:NewIncomingAnnounce(1296220, 2)
+--local warnTripleShot				= mod:NewIncomingAnnounce(1296220, 2)
 
 local specWarnSsscavenging			= mod:NewSpecialWarningCount(1309522, nil, nil, nil, 1, 2, nil, nil, "attackshield")
 local specWarnFeedingFrenzy			= mod:NewSpecialWarningCount(1307765, nil, nil, nil, 3, 2, nil, nil, "attackshield")--Empowered version of Ssscavenging (from fresh meat)
 local specWarnFetidRoar				= mod:NewSpecialWarningCount(1296219, nil, nil, nil, 2, 2, nil, nil, "aesoon")--Possibly not needed
 local specWarnRegurgitate			= mod:NewSpecialWarningCount(1296050, nil, nil, nil, 2, 2, nil, nil, "watchwave")
 local specWarnRavenousStomp			= mod:NewSpecialWarningCount(1307894, nil, nil, nil, 2, 2, nil, nil, "watchstep")
+local specWarnTripleShot			= mod:NewSpecialWarningBlizzYou(1296220, nil, nil, nil, 1, 17, nil, nil, "debuffyou")
 
 local timerSsscavengingCD			= mod:NewCDCountTimer(8, 1309522, nil, nil, nil, 5, nil, DBM_COMMON_L.DAMAGE_ICON)
 local timerTripleShotCD				= mod:NewCDCountTimer(8, 1296220, nil, nil, nil, 3)
 local timerRegurgitateCD			= mod:NewCDCountTimer(8, 1296050, nil, nil, nil, 3)
 local timerRavenousStompCD			= mod:NewCDCountTimer(8, 1307894, nil, nil, nil, 3)
 
+mod:AddCustomAlertSoundOption(1296219, true, 2)--Fetid Roar
+
 --mod:AddAuraSoundOption(470966, true, 470966, 4, 1, "justrun", 2)
 
 local badStateDetected = false
+local nextTwentyFourIsStomp = false
 mod.vb.SsscavengingCount = 0
 mod.vb.TripleShotCount = 0
 mod.vb.RegurgitateCount = 0
@@ -44,7 +48,7 @@ local function setFallback(self, dontSetAlerts)
 --		end
 		specWarnSsscavenging:SetAlert(795, "attackshield", 2, 3)
 		specWarnFetidRoar:SetAlert(796, "aesoon", 2, 2, 0)
-		warnTripleShot:SetAlert(797, "incomingdebuff", 15, 2)
+		specWarnTripleShot:SetAlert(797, "incomingdebuff", 17, 2, 0)
 		specWarnRegurgitate:SetAlert(798, "watchwave", 2, 2, 0)
 		specWarnRavenousStomp:SetAlert(899, "watchstep", 2, 2, 0)
 		specWarnFeedingFrenzy:SetAlert(902, "attackshield", 2, 3, 0)
@@ -65,33 +69,52 @@ function mod:OnLimitedCombatStart()
 	self.vb.TripleShotCount = 1
 	self.vb.RegurgitateCount = 1
 	self.vb.RavenousStompCount = 1
-	--if DBM.Options.HardcodedTimer and not badStateDetected then
-	--	self:IgnoreBlizzardAPI()
-	--	self:RegisterShortTermEvents(
-	--		"ENCOUNTER_TIMELINE_EVENT_ADDED",
-	--		"ENCOUNTER_TIMELINE_EVENT_STATE_CHANGED"
-	--	)
-	--	setFallback(self, true)
-	--else
+	nextTwentyFourIsStomp = false
+	self:EnableAlertOptions(1296219, 796, "aesoon", 2, 2, 0)
+	if DBM.Options.HardcodedTimer and not badStateDetected then
+		self:IgnoreBlizzardAPI()
+		self:RegisterShortTermEvents(
+			"ENCOUNTER_TIMELINE_EVENT_ADDED",
+			"ENCOUNTER_TIMELINE_EVENT_STATE_CHANGED"
+		)
+		setFallback(self, true)
+	else
 		setFallback(self)
-	--end
+	end
 end
 
 function mod:OnCombatEnd()
 	self:TLCountReset()
+	nextTwentyFourIsStomp = false
 	self:UnregisterShortTermEvents()
 end
 
---[[
 do
 	---@param self DBMMod
 	---@param timer number
 	---@param timerExact number
 	---@param eventID number
 	local function timersAll(self, timer, timerExact, eventID)
-		if timer == 3 or timer == 30 then
-	--		timerRampageCD:TLStart(timerExact, eventID, self:TLCountStart(eventID, "rampage", "rampageCount"))
-		else
+		local handled = false
+		if timer == 8 then--Triple Shot opener and post-Ssscavenging batch
+			timerTripleShotCD:TLStart(timerExact, eventID, self:TLCountStart(eventID, "tripleShot", "TripleShotCount"))
+			handled = true
+		elseif timer == 13 then--Regurgitate
+			timerRegurgitateCD:TLStart(timerExact, eventID, self:TLCountStart(eventID, "regurgitate", "RegurgitateCount"))
+			handled = true
+		elseif timer == 25 or timer == 45 then--Ssscavenging opener and repeat
+			timerSsscavengingCD:TLStart(timerExact, eventID, self:TLCountStart(eventID, "ssscavenging", "SsscavengingCount"))
+			handled = true
+		elseif timer == 24 then--Ssscavenging completion starts the ordered Stomp -> Triple Shot 24s pair
+			if nextTwentyFourIsStomp then
+				nextTwentyFourIsStomp = false
+				timerRavenousStompCD:TLStart(timerExact, eventID, self:TLCountStart(eventID, "ravenousStomp", "RavenousStompCount"))
+			else
+				timerTripleShotCD:TLStart(timerExact, eventID, self:TLCountStart(eventID, "tripleShot", "TripleShotCount"))
+			end
+			handled = true
+		end
+		if not handled then
 			badStateDetected = true
 			self:ResumeBlizzardAPI()
 			self:UnregisterShortTermEvents()
@@ -103,6 +126,8 @@ do
 	function mod:ENCOUNTER_TIMELINE_EVENT_ADDED(eventInfo)
 		if eventInfo.source ~= 0 then return end
 		local eventID = eventInfo.id
+		local eventState = C_EncounterTimeline.GetEventState(eventID)
+		if eventState ~= 0 then return end
 		local timerExact = eventInfo.duration
 		local timer = math.floor(timerExact + 0.5)
 		if not badStateDetected then
@@ -114,13 +139,22 @@ do
 		local eventState = C_EncounterTimeline.GetEventState(eventID)
 		if not eventID or not eventState then return end
 		if eventState == 2 then
-			local finishedEventType, eventCount = self:TLCountFinish(eventID)
-			if finishedEventType and eventCount then
-				if finishedEventType == "rampage" then
-					if self:IsTank() then
---						specWarnRampage:Show(eventCount)
---						specWarnRampage:Play("defensive")
-					end
+			local eventType, eventCount = self:TLCountFinish(eventID)
+			if eventType == "ssscavenging" then
+				nextTwentyFourIsStomp = true
+			end
+			if eventType and eventCount then
+				if eventType == "tripleShot" then
+					specWarnTripleShot:Show(eventCount, "debuffyou")
+				elseif eventType == "ssscavenging" then
+					specWarnSsscavenging:Show(eventCount)
+					specWarnSsscavenging:Play("attackshield")
+				elseif eventType == "regurgitate" then
+					specWarnRegurgitate:Show(eventCount)
+					specWarnRegurgitate:Play("watchwave")
+				elseif eventType == "ravenousStomp" then
+					specWarnRavenousStomp:Show(eventCount)
+					specWarnRavenousStomp:Play("watchstep")
 				end
 			end
 		elseif eventState == 3 then
@@ -128,4 +162,3 @@ do
 		end
 	end
 end
---]]
