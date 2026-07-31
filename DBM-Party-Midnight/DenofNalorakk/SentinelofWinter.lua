d1f6f6fe25fc447d6ef826f79e1fe1864826b903
MysticalOS <mysticalosx@gmail.com>
2026-07-31 15:50:21 -0500

    Upgrade Den of Nalorakk to hardcode for 12.1

--- DBM-Party-Midnight/DenofNalorakk/SentinelofWinter.lua
+++ DBM-Party-Midnight/DenofNalorakk/SentinelofWinter.lua
@@ -15,29 +15,126 @@ mod:RegisterCombat("combat")
 
 --)
 
---TODO, recheck if rimeshatter has an event?
--- Custon Sounds on cast/cooldown expiring
-mod:AddCustomAlertSoundOption(1235548, "Healer", 2)--Glacial Torment
-mod:AddCustomAlertSoundOption(1235623, true, 2)--Raging Squall
-mod:AddCustomAlertSoundOption(1235783, true, 1)--Shattering Frostspike
-mod:AddCustomAlertSoundOption(1235656, true, 2)--Eternal Winter
---Custom timer colors, countdowns, and disables
-mod:AddCustomTimerOptions(1235548, true, 5, 0)--Glacial Torment
-mod:AddCustomTimerOptions(1235623, true, 3, 0)--Raging Squall
-mod:AddCustomTimerOptions(1235783, true, 1, 0)--Shattering Frostspike
-mod:AddCustomTimerOptions(1235656, true, 2, 0)--Eternal Winter
+local specWarnGlacialTorment			= mod:NewSpecialWarningCount(1235548, "Healer", nil, nil, 2, 3, nil, nil, "helpdispel")
+local specWarnRagingSquall				= mod:NewSpecialWarningCount(1235623, nil, nil, nil, 2, 2, nil, nil, "watchstep")
+local specWarnShatteringFrostspike		= mod:NewSpecialWarningCount(1235783, nil, nil, nil, 2, 1, nil, nil, "mobsoon")
+local specWarnFrozenTempest				= mod:NewSpecialWarningCount(1235656, nil, nil, nil, 2, 3, nil, nil, "pushbackincoming")
+
+local timerGlacialTormentCD				= mod:NewCDCountTimer(20.5, 1235548, nil, nil, nil, 5)
+local timerRagingSquallCD				= mod:NewCDCountTimer(20.5, 1235623, nil, nil, nil, 3)
+local timerShatteringFrostspikeCD		= mod:NewCDCountTimer(20.5, 1235783, nil, nil, nil, 1)
+local timerFrozenTempestCD				= mod:NewCDCountTimer(20.5, 1235656, nil, nil, nil, 2)
+
 --Custom Aura Sounds
 mod:AddAuraSoundOption(1235641, true, 1235641, 1, 2, "watchfeet", 8)--Raging Squall
 
+mod.vb.tormentCount = 0
+mod.vb.squallCount = 0
+mod.vb.frostspikeCount = 0
+mod.vb.tempestCount = 0
+local badStateDetected = false
+
+---@param self DBMMod
+---@param dontSetAlerts boolean? Called on engage when we only want to set timeline parameters and not touch encounter alerts
+local function setFallback(self, dontSetAlerts)
+	if not dontSetAlerts then
+		if self:IsHealer() then
+			specWarnGlacialTorment:SetAlert(67, "helpdispel", 2, 3)
+		end
+		specWarnRagingSquall:SetAlert(68, "watchstep", 2, 2)
+		specWarnShatteringFrostspike:SetAlert(69, "mobsoon", 2, 1)
+		specWarnFrozenTempest:SetAlert(70, "pushbackincoming", 13, 3)
+	end
+	local onlyColor = not DBM.Options.HideDBMBars and not badStateDetected
+	timerGlacialTormentCD:SetTimeline(67, onlyColor)
+	timerRagingSquallCD:SetTimeline(68, onlyColor)
+	timerShatteringFrostspikeCD:SetTimeline(69, onlyColor)
+	timerFrozenTempestCD:SetTimeline(70, onlyColor)
+end
+
 function mod:OnLimitedCombatStart()
-	self:EnableAlertOptions(1235548, 67, "helpdispel", 2, 3)
-	self:EnableAlertOptions(1235623, 68, "watchstep", 2, 2)
-	self:EnableAlertOptions(1235783, 69, "mobsoon", 2, 1)
-	self:EnableAlertOptions(1235656, 70, "pushbackincoming", 13, 3)
+	self:TLCountReset()
+	self.vb.tormentCount = 1
+	self.vb.squallCount = 1
+	self.vb.frostspikeCount = 1
+	self.vb.tempestCount = 1
+	if DBM.Options.HardcodedTimer and not badStateDetected then
+		self:IgnoreBlizzardAPI()
+		self:RegisterShortTermEvents(
+			"ENCOUNTER_TIMELINE_EVENT_ADDED",
+			"ENCOUNTER_TIMELINE_EVENT_STATE_CHANGED"
+		)
+		setFallback(self, true)
+	else
+		setFallback(self)
+	end
+end
 
-	self:EnableTimelineOptions(1235548, 67)
-	self:EnableTimelineOptions(1235623, 68)
-	self:EnableTimelineOptions(1235783, 69)
-	self:EnableTimelineOptions(1235656, 70)
+function mod:OnCombatEnd()
+	self:TLCountReset()
+	self:UnregisterShortTermEvents()
+end
 
+do
+	---@param self DBMMod
+	---@param timer number
+	---@param timerExact number
+	---@param eventID number
+	local function timersAll(self, timer, timerExact, eventID)
+		if timer == 61 or timer == 63 then--Long placeholder bars are canceled during the recurring reset
+			return true
+		elseif timer == 7 then
+			timerGlacialTormentCD:TLStart(timerExact, eventID, self:TLCountStart(eventID, "torment", "tormentCount"))
+			return true
+		elseif timer == 13 then
+			timerRagingSquallCD:TLStart(timerExact, eventID, self:TLCountStart(eventID, "squall", "squallCount"))
+			return true
+		elseif timer == 25 then
+			timerShatteringFrostspikeCD:TLStart(timerExact, eventID, self:TLCountStart(eventID, "frostspike", "frostspikeCount"))
+			return true
+		elseif timer == 50 then
+			timerFrozenTempestCD:TLStart(timerExact, eventID, self:TLCountStart(eventID, "tempest", "tempestCount"))
+			return true
+		end
+	end
+
+	function mod:ENCOUNTER_TIMELINE_EVENT_ADDED(eventInfo)
+		if eventInfo.source ~= 0 then return end
+		local eventID = eventInfo.id
+		if C_EncounterTimeline.GetEventState(eventID) ~= 0 then return end
+		local timerExact = eventInfo.duration
+		local handled = timersAll(self, math.floor(timerExact + 0.5), timerExact, eventID)
+		if not handled and not badStateDetected then
+			badStateDetected = true
+			self:ResumeBlizzardAPI()
+			self:UnregisterShortTermEvents()
+			setFallback(self)
+			DBM:Debug("|cffff0000Failed to match encounter timeline events to expected timers, falling back to Blizzard API|r", nil, nil, nil, true)
+		end
+	end
+
+	function mod:ENCOUNTER_TIMELINE_EVENT_STATE_CHANGED(eventID)
+		local eventState = C_EncounterTimeline.GetEventState(eventID)
+		if not eventID or not eventState then return end
+		if eventState == 2 then
+			local eventType, eventCount = self:TLCountFinish(eventID)
+			if eventType and eventCount then
+				if eventType == "torment" and self:IsHealer() then
+					specWarnGlacialTorment:Show(eventCount)
+					specWarnGlacialTorment:Play("helpdispel")
+				elseif eventType == "squall" then
+					specWarnRagingSquall:Show(eventCount)
+					specWarnRagingSquall:Play("watchstep")
+				elseif eventType == "frostspike" then
+					specWarnShatteringFrostspike:Show(eventCount)
+					specWarnShatteringFrostspike:Play("mobsoon")
+				elseif eventType == "tempest" then
+					specWarnFrozenTempest:Show(eventCount)
+					specWarnFrozenTempest:Play("pushbackincoming")
+				end
+			end
+		elseif eventState == 3 then
+			self:TLCountCancel(eventID)
+		end
+	end
 end
--- DBM-Party-Midnight/DenofNalorakk/TheHoardmonger.lua
+++ DBM-Party-Midnight/DenofNalorakk/TheHoardmonger.lua
@@ -15,27 +15,113 @@ mod:RegisterCombat("combat")
 
 --)
 
---NOTE, resourceful measures is missing eventID, all sub events are too but likely use same IDs as unempowered versions
---but no timer for empowerment change sadly
--- Custon Sounds on cast/cooldown expiring
-mod:AddCustomAlertSoundOption(1234233, true, 2)--Spoiled Supplies
-mod:AddCustomAlertSoundOption(1253268, true, 2)--Earthshatter Slam
-mod:AddCustomAlertSoundOption(1235118, true, 2)--Ravenous Bellow
---Custom timer colors, countdowns, and disables
-mod:AddCustomTimerOptions(1234233, true, 5, 0)--Spoiled Supplies
-mod:AddCustomTimerOptions(1253268, true, 3, 0)--Earthshatter Slam
-mod:AddCustomTimerOptions(1235118, true, 2, 0)--Ravenous Bellow
+local specWarnSpoiledSupplies				= mod:NewSpecialWarningCount(1234233, nil, nil, nil, 2, 2, nil, nil, "greenmushroomcoming")
+local specWarnEarthshatterSlam				= mod:NewSpecialWarningCount(1253268, nil, nil, nil, 2, 2, nil, nil, "frontal")
+local specWarnRavenousBellow				= mod:NewSpecialWarningCount(1235118, nil, nil, nil, 2, 2, nil, nil, "aesoon")
+
+local timerSpoiledSuppliesCD				= mod:NewCDCountTimer(20.5, 1234233, nil, nil, nil, 5)
+local timerEarthshatterSlamCD				= mod:NewCDCountTimer(20.5, 1253268, nil, nil, nil, 3)
+local timerRavenousBellowCD				= mod:NewCDCountTimer(20.5, 1235118, nil, nil, nil, 2)
+
 --Custom Aura Sounds
 mod:AddAuraSoundOption(1235405, true, 1235405, 1, 2, "watchfeet", 8)--Bonespiked
 --mod:AddAuraSoundOption(1234846, false, 1234846, 1, 1, "toxic", 2)--Toxic Spores (off by default, i don't think it needs a sound, since we can't alert stacks, the PA anchor will handle it
 
+mod.vb.suppliesCount = 0
+mod.vb.slamCount = 0
+mod.vb.bellowCount = 0
+local badStateDetected = false
+
+---@param self DBMMod
+---@param dontSetAlerts boolean? Called on engage when we only want to set timeline parameters and not touch encounter alerts
+local function setFallback(self, dontSetAlerts)
+	if not dontSetAlerts then
+		specWarnSpoiledSupplies:SetAlert(86, "greenmushroomcoming", 12, 2)
+		specWarnEarthshatterSlam:SetAlert(87, "frontal", 15, 2)
+		specWarnRavenousBellow:SetAlert(88, "aesoon", 2, 2)
+	end
+	local onlyColor = not DBM.Options.HideDBMBars and not badStateDetected
+	timerSpoiledSuppliesCD:SetTimeline(86, onlyColor)
+	timerEarthshatterSlamCD:SetTimeline(87, onlyColor)
+	timerRavenousBellowCD:SetTimeline(88, onlyColor)
+end
+
 function mod:OnLimitedCombatStart()
-	self:EnableAlertOptions(1234233, 86, "greenmushroomcoming", 12, 2)
-	self:EnableAlertOptions(1253268, 87, "frontal", 15, 2)
-	self:EnableAlertOptions(1235118, 88, "aesoon", 2, 2)
+	self:TLCountReset()
+	self.vb.suppliesCount = 1
+	self.vb.slamCount = 1
+	self.vb.bellowCount = 1
+	if DBM.Options.HardcodedTimer and not badStateDetected then
+		self:IgnoreBlizzardAPI()
+		self:RegisterShortTermEvents(
+			"ENCOUNTER_TIMELINE_EVENT_ADDED",
+			"ENCOUNTER_TIMELINE_EVENT_STATE_CHANGED"
+		)
+		setFallback(self, true)
+	else
+		setFallback(self)
+	end
+end
 
-	self:EnableTimelineOptions(1234233, 86)
-	self:EnableTimelineOptions(1253268, 87)
-	self:EnableTimelineOptions(1235118, 88)
+function mod:OnCombatEnd()
+	self:TLCountReset()
+	self:UnregisterShortTermEvents()
+end
 
+do
+	---@param self DBMMod
+	---@param timer number
+	---@param timerExact number
+	---@param eventID number
+	local function timersAll(self, timer, timerExact, eventID)
+		if timer == 99 then--Long placeholder bars are canceled during the recurring reset
+			return true
+		elseif timer == 30 then
+			timerSpoiledSuppliesCD:TLStart(timerExact, eventID, self:TLCountStart(eventID, "supplies", "suppliesCount"))
+			return true
+		elseif timer == 16 then
+			timerEarthshatterSlamCD:TLStart(timerExact, eventID, self:TLCountStart(eventID, "slam", "slamCount"))
+			return true
+		elseif timer == 6 then
+			timerRavenousBellowCD:TLStart(timerExact, eventID, self:TLCountStart(eventID, "bellow", "bellowCount"))
+			return true
+		end
+	end
+
+	function mod:ENCOUNTER_TIMELINE_EVENT_ADDED(eventInfo)
+		if eventInfo.source ~= 0 then return end
+		local eventID = eventInfo.id
+		if C_EncounterTimeline.GetEventState(eventID) ~= 0 then return end
+		local timerExact = eventInfo.duration
+		local handled = timersAll(self, math.floor(timerExact + 0.5), timerExact, eventID)
+		if not handled and not badStateDetected then
+			badStateDetected = true
+			self:ResumeBlizzardAPI()
+			self:UnregisterShortTermEvents()
+			setFallback(self)
+			DBM:Debug("|cffff0000Failed to match encounter timeline events to expected timers, falling back to Blizzard API|r", nil, nil, nil, true)
+		end
+	end
+
+	function mod:ENCOUNTER_TIMELINE_EVENT_STATE_CHANGED(eventID)
+		local eventState = C_EncounterTimeline.GetEventState(eventID)
+		if not eventID or not eventState then return end
+		if eventState == 2 then
+			local eventType, eventCount = self:TLCountFinish(eventID)
+			if eventType and eventCount then
+				if eventType == "supplies" then
+					specWarnSpoiledSupplies:Show(eventCount)
+					specWarnSpoiledSupplies:Play("greenmushroomcoming")
+				elseif eventType == "slam" then
+					specWarnEarthshatterSlam:Show(eventCount)
+					specWarnEarthshatterSlam:Play("frontal")
+				elseif eventType == "bellow" then
+					specWarnRavenousBellow:Show(eventCount)
+					specWarnRavenousBellow:Play("aesoon")
+				end
+			end
+		elseif eventState == 3 then
+			self:TLCountCancel(eventID)
+		end
+	end
 end
