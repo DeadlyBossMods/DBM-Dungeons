f3678c3d03222e8cccf1f6e2a81bc91adaee3164
MysticalOS <mysticalosx@gmail.com>
2026-07-31 00:22:12 -0500

    prep mage tower modules for midnight updates

--- DBM-Challenges/Legion/Healer.lua
+++ DBM-Challenges/Legion/Healer.lua
@@ -4,7 +4,6 @@ local L		= mod:GetLocalizedStrings()
 mod.statTypes = "normal,timewalker"
 
 mod:SetRevision("@file-date-integer@")
-mod:DisableHardcodedOptions()
 mod:SetCreatureID(118488)--Lord Erdris Thorn
 mod.soloChallenge = true
 
@@ -18,98 +17,145 @@ mod:RegisterEvents(
 	"LOADING_SCREEN_DISABLED"
 )
 
-mod:RegisterEventsInCombat(
-	"SPELL_CAST_START 235823",
-	"SPELL_AURA_APPLIED 235984 237188",
-	"SPELL_AURA_APPLIED_DOSE 235833",
-	"UNIT_DIED"
-)
---Notes:
---TODO, all. mapids, mob iDs, win event to stop timers (currently only death event stops them)
---TODO, stage 2 detection
---TODO< probably won't need both LSD and ZCNA, just gotta fix core before testing which one is ideal
---Healer
--- Need ignite soul equiv name/ID.
--- Need fear name/Id
+if DBM:IsPostMidnight() then
+	--local warnArcaneBlitz = mod:NewStackAnnounce(235833, 2)
 
-local warnArcaneBlitz		= mod:NewStackAnnounce(235833, 2)
+	--local specWarnManaSting = mod:NewSpecialWarningMoveTo(235984, nil, nil, nil, 1, 2, nil, nil, "findshelter")
+	--local specWarnArcaneBlitz = mod:NewSpecialWarningStack(235833, nil, 4, nil, nil, 1, 6, nil, nil, "stackhigh")
+	--local specWarnIgniteSoul = mod:NewSpecialWarningYou(237188, nil, nil, nil, 3, 2, nil, nil, "targetyou")
+	--local specWarnKnifeDance = mod:NewSpecialWarningInterrupt(235823, nil, nil, nil, 1, 2, nil, nil, "kickcast")
 
-local specWarnManaSting		= mod:NewSpecialWarningMoveTo(235984, nil, nil, nil, 1, 2, nil, nil, "findshelter")
-local specWarnArcaneBlitz	= mod:NewSpecialWarningStack(235833, nil, 4, nil, nil, 1, 6, nil, nil, "stackhigh")--Fine tune the numbers
-local specWarnIgniteSoul	= mod:NewSpecialWarningYou(237188, nil, nil, nil, 3, 2, nil, nil, "targetyou")
-local specWarnKnifeDance	= mod:NewSpecialWarningInterrupt(235823, nil, nil, nil, 1, 2, nil, nil, "kickcast")
+	--local timerIgniteSoulCD = mod:NewCDCountTimer(0, 237188, nil, nil, nil, 3, nil, DBM_COMMON_L.DEADLY_ICON)
+	local badStateDetected = false
+	local function setFallback(self, dontSetAlerts)
+		if not dontSetAlerts then
+			--specWarnManaSting:SetAlert(0, "findshelter", 2)
+			--specWarnArcaneBlitz:SetAlert(0, "stackhigh", 2)
+			--specWarnIgniteSoul:SetAlert(0, "targetyou", 2)
+			--specWarnKnifeDance:SetAlert(0, "kickcast", 2)
+		end
+		local onlyColor = not DBM.Options.HideDBMBars and not badStateDetected
+		--timerIgniteSoulCD:SetTimeline(0, onlyColor)
+	end
 
---local timerEarthquakeCD	= mod:NewNextTimer(60, 237950, nil, nil, nil, 2)
-local timerIgniteSoulCD		= mod:NewAITimer(18, 237188, nil, nil, nil, 3, nil, DBM_COMMON_L.DEADLY_ICON, nil, 3, 4)
+	function mod:OnLimitedCombatStart()
+		self:TLCountReset()
+		badStateDetected = true
+		if DBM.Options.HardcodedTimer and not badStateDetected then
+			self:IgnoreBlizzardAPI()
+			self:RegisterShortTermEvents("ENCOUNTER_TIMELINE_EVENT_ADDED", "ENCOUNTER_TIMELINE_EVENT_STATE_CHANGED")
+			setFallback(self, true)
+		else
+			setFallback(self)
+		end
+	end
 
-function mod:OnCombatStart(delay)
-	self:SetStage(1)
-end
+	function mod:OnCombatEnd()
+		self:TLCountReset()
+		self:UnregisterShortTermEvents()
+	end
 
---Fix for not starting combat on initial mod load
---local currentZoneID = select(8, GetInstanceInfo())
---if currentZoneID == 1710 then
---	DBM:Schedule(1, DBM.StartCombat, DBM, mod, 0, "Hack")
---end
+	function mod:ENCOUNTER_TIMELINE_EVENT_ADDED(eventInfo)
+		--Timeline routing will be added after event durations are verified.
+	end
 
-function mod:SPELL_AURA_APPLIED(args)
-	local spellId = args.spellId
-	if spellId == 235833 then
-		local amount = args.amount or 1
-		if amount % 2 == 0 then
-			if amount >= 4 then
-				specWarnArcaneBlitz:Show(amount)
-				specWarnArcaneBlitz:Play("stackhigh")
-			else
-				warnArcaneBlitz:Show(args.destName, amount)
+	function mod:ENCOUNTER_TIMELINE_EVENT_STATE_CHANGED(eventID)
+		--Timeline completion handling will be added with the event routing.
+	end
+else
+	mod:RegisterEventsInCombat(
+		"SPELL_CAST_START 235823",
+		"SPELL_AURA_APPLIED 235984 237188",
+		"SPELL_AURA_APPLIED_DOSE 235833",
+		"UNIT_DIED"
+	)
+	--Notes:
+	--TODO, all. mapids, mob iDs, win event to stop timers (currently only death event stops them)
+	--TODO, stage 2 detection
+	--TODO< probably won't need both LSD and ZCNA, just gotta fix core before testing which one is ideal
+	--Healer
+	-- Need ignite soul equiv name/ID.
+	-- Need fear name/Id
+
+	local warnArcaneBlitz		= mod:NewStackAnnounce(235833, 2)
+
+	local specWarnManaSting		= mod:NewSpecialWarningMoveTo(235984, nil, nil, nil, 1, 2, nil, nil, "findshelter")
+	local specWarnArcaneBlitz	= mod:NewSpecialWarningStack(235833, nil, 4, nil, nil, 1, 6, nil, nil, "stackhigh")--Fine tune the numbers
+	local specWarnIgniteSoul	= mod:NewSpecialWarningYou(237188, nil, nil, nil, 3, 2, nil, nil, "targetyou")
+	local specWarnKnifeDance	= mod:NewSpecialWarningInterrupt(235823, nil, nil, nil, 1, 2, nil, nil, "kickcast")
+
+	--local timerEarthquakeCD	= mod:NewNextTimer(60, 237950, nil, nil, nil, 2)
+	local timerIgniteSoulCD		= mod:NewAITimer(18, 237188, nil, nil, nil, 3, nil, DBM_COMMON_L.DEADLY_ICON, nil, 3, 4)
+
+	function mod:OnCombatStart(delay)
+		self:SetStage(1)
+	end
+
+	--Fix for not starting combat on initial mod load
+	--local currentZoneID = select(8, GetInstanceInfo())
+	--if currentZoneID == 1710 then
+	--	DBM:Schedule(1, DBM.StartCombat, DBM, mod, 0, "Hack")
+	--end
+
+	function mod:SPELL_AURA_APPLIED(args)
+		local spellId = args.spellId
+		if spellId == 235833 then
+			local amount = args.amount or 1
+			if amount % 2 == 0 then
+				if amount >= 4 then
+					specWarnArcaneBlitz:Show(amount)
+					specWarnArcaneBlitz:Play("stackhigh")
+				else
+					warnArcaneBlitz:Show(args.destName, amount)
+				end
+			end
+		elseif spellId == 235984 and args:IsPlayer() then
+			specWarnManaSting:Show(DBM_COMMON_L.ALLY)
+			specWarnManaSting:Play("findshelter")
+		elseif spellId == 237188 then
+			specWarnIgniteSoul:Show()
+			specWarnIgniteSoul:Play("targetyou")
+			timerIgniteSoulCD:Start()
+		end
+	end
+	mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
+
+	function mod:SPELL_CAST_START(args)
+		if args.spellId == 235823 then
+			specWarnKnifeDance:Show(args.sourceName)
+			specWarnKnifeDance:Play("kickcast")
+		end
+	end
+
+	do
+		local friendlyNPCS = {
+			[118447] = true,--Commander Jarod Shadowsong
+			[118448] = true,--Granny Marl
+			[118451] = true--Callie Carrington
+		}
+
+		function mod:UNIT_DIED(args)
+			local cid = self:GetCIDFromGUID(args.destGUID)
+			if friendlyNPCS[cid] then
+				DBM:EndCombat(self, true)
 			end
 		end
-	elseif spellId == 235984 and args:IsPlayer() then
-		specWarnManaSting:Show(DBM_COMMON_L.ALLY)
-		specWarnManaSting:Play("findshelter")
-	elseif spellId == 237188 then
-		specWarnIgniteSoul:Show()
-		specWarnIgniteSoul:Play("targetyou")
-		timerIgniteSoulCD:Start()
 	end
-end
-mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
 
-function mod:SPELL_CAST_START(args)
-	if args.spellId == 235823 then
-		specWarnKnifeDance:Show(args.sourceName)
-		specWarnKnifeDance:Play("kickcast")
-	end
-end
-
-do
-	local friendlyNPCS = {
-		[118447] = true,--Commander Jarod Shadowsong
-		[118448] = true,--Granny Marl
-		[118451] = true--Callie Carrington
-	}
-
-	function mod:UNIT_DIED(args)
-		local cid = self:GetCIDFromGUID(args.destGUID)
-		if friendlyNPCS[cid] then
-			DBM:EndCombat(self, true)
+	do
+		local function delayedZoneCheck(self)
+			if DBM:GetCurrentArea() == 1710 then
+				DBM:StartCombat(self, 0, "Hack")
+			elseif DBM:GetCurrentArea() ~= 1710 then
+				DBM:EndCombat(self, true)
+			end
 		end
-	end
-end
-
-do
-	local function delayedZoneCheck(self)
-		if DBM:GetCurrentArea() == 1710 then
-			DBM:StartCombat(self, 0, "Hack")
-		elseif DBM:GetCurrentArea() ~= 1710 then
-			DBM:EndCombat(self, true)
+		function mod:LOADING_SCREEN_DISABLED()
+			self:Unschedule(delayedZoneCheck)
+			self:Schedule(1, delayedZoneCheck, self)
+			self:Schedule(3, delayedZoneCheck, self)
 		end
+		mod.OnInitialize = mod.LOADING_SCREEN_DISABLED
+		mod.ZONE_CHANGED_NEW_AREA	= mod.LOADING_SCREEN_DISABLED
 	end
-	function mod:LOADING_SCREEN_DISABLED()
-		self:Unschedule(delayedZoneCheck)
-		self:Schedule(1, delayedZoneCheck, self)
-		self:Schedule(3, delayedZoneCheck, self)
-	end
-	mod.OnInitialize = mod.LOADING_SCREEN_DISABLED
-	mod.ZONE_CHANGED_NEW_AREA	= mod.LOADING_SCREEN_DISABLED
 end
