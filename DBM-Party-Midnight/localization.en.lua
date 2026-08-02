7a15f4c0a4d106c6616fd15bf35fd84b875bbf30
MysticalOS <mysticalosx@gmail.com>
2026-08-01 23:08:30 -0500

    template some trash modules

--- DBM-Party-Midnight/localization.en.lua
+++ DBM-Party-Midnight/localization.en.lua
@@ -8,3 +8,39 @@ L = DBM:GetModLocalization("MurderRowTrash")
 L:SetGeneralLocalization({
 	name =	"Murder Row Trash"
 })
+
+-----------------------
+--The Blinding Vale Trash
+-----------------------
+L = DBM:GetModLocalization("TheBlindingValeTrash")
+
+L:SetGeneralLocalization({
+	name =	"The Blinding Vale Trash"
+})
+
+-----------------------
+--Voidscar Arena Trash
+-----------------------
+L = DBM:GetModLocalization("VoidscarArenaTrash")
+
+L:SetGeneralLocalization({
+	name =	"Voidscar Arena Trash"
+})
+
+-----------------------
+--Altar of Fangs Trash
+-----------------------
+L = DBM:GetModLocalization("AltarofFangsTrash")
+
+L:SetGeneralLocalization({
+	name =	"Altar of Fangs Trash"
+})
+
+-----------------------
+--Den of Nalorakk Trash
+-----------------------
+L = DBM:GetModLocalization("DenofNalorakkTrash")
+
+L:SetGeneralLocalization({
+	name =	"Den of Nalorakk Trash"
+})
--- DBM-Party-Midnight/localization.ru.lua
+++ DBM-Party-Midnight/localization.ru.lua
@@ -9,3 +9,39 @@ L = DBM:GetModLocalization("MurderRowTrash")
 L:SetGeneralLocalization({
 	name =	"Трэш мобы Закоулок душегубов"
 })
+
+-----------------------
+--The Blinding Vale Trash
+-----------------------
+L = DBM:GetModLocalization("TheBlindingValeTrash")
+
+L:SetGeneralLocalization({
+	name =	"Трэш мобы Слепой долины"
+})
+
+-----------------------
+--Voidscar Arena Trash
+-----------------------
+L = DBM:GetModLocalization("VoidscarArenaTrash")
+
+L:SetGeneralLocalization({
+	name =	"Трэш мобы Арены шрамов Бездны"
+})
+
+-----------------------
+--Altar of Fangs Trash
+-----------------------
+L = DBM:GetModLocalization("AltarofFangsTrash")
+
+L:SetGeneralLocalization({
+	name =	"Трэш мобы Алтаря Клыков"
+})
+
+-----------------------
+--Den of Nalorakk Trash
+-----------------------
+L = DBM:GetModLocalization("DenofNalorakkTrash")
+
+L:SetGeneralLocalization({
+	name =	"Трэш мобы Логова Налоракка"
+})
