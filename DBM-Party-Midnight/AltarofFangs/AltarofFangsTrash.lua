69fb53e869dccdfbfdbdde7561ff83c1645c0065
MysticalOS <mysticalosx@gmail.com>
2026-08-12 00:04:54 -0500

    Add damage taken debuff to altar of fangs

--- DBM-Party-Midnight/AltarofFangs/AltarofFangsTrash.lua
+++ DBM-Party-Midnight/AltarofFangs/AltarofFangsTrash.lua
@@ -14,3 +14,4 @@ mod:AddAuraSoundOption(1308865, true, 1308865, 1, 1, "runout", 2, 0)--Infest
 mod:AddAuraSoundOption(1294557, true, 1294557, 1, 1, "debuffyou", 17, 0)--Piercing Hiss (Missed interrupt)
 mod:AddAuraSoundOption(1306550, true, 1306550, 1, 1, "absorbyou", 19, 0)--Blood Sacrifice
 mod:AddAuraSoundOption(1307571, "RemovePoison", 1307571, 1, 3, "helpdispel", 2, 0)--Envenom (Debuff from Mass Envenom)
+mod:AddAuraSoundOption(1294845, "Tank", 1294845, 1, 1, "defensive", 2, 0)--Corrosive Fangs (20% Damage taken Debuff)
